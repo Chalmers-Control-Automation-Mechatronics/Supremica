d6178293313f3b5a2b9a846c40205f8375fc66b3
Knut Akesson <knut@chalmers.se>
2001-09-27 21:00:20 +0000

    *** empty log message ***

--- src/org/supremica/gui/Utility.java
+++ src/org/supremica/gui/Utility.java
@@ -9,46 +9,6 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.table.*;
 
-//** This one is useful for all centered frames
-//** It sets the size and the centers on the screen
-class CenteredFrame extends JFrame
-{
-	public CenteredFrame(int width, int height)
-	{
-		Utility.setupFrame(this, width, height);
-	}
-}
-//** This one sets its own background to white
-class WhitePane extends JScrollPane
-{
-	private void setWhite()
-	{
-		Utility.setupPane(this);;
-	}
-	
-	public WhitePane()
-	{
-		setWhite();
-	}
-	
-	public WhitePane(Component view)
-	{
-		super(view);
-		setWhite();
-	}
-	
-	public WhitePane(Component view, int vsbPolicy, int hsbPolicy)
-	{
-		super(view, vsbPolicy, hsbPolicy);
-		setWhite();
-	}
-	
-	public WhitePane(int vsbPolicy, int hsbPolicy) 
-	{
-		super(vsbPolicy, hsbPolicy);
-		setWhite();
-	}
-}
 //** And the utility class that contains the functions that do the real job
 public class Utility
 {
