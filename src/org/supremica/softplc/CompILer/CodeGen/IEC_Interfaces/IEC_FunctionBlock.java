4944820ba4811436e227eaa6a9107cb151a85cb9
d99andro <unknown@anywhere.com>
2002-04-22 12:58:09 +0000

    *** empty log message ***

--- src/org/supremica/softplc/CompILer/CodeGen/IEC_Interfaces/IEC_FunctionBlock.java
+++ src/org/supremica/softplc/CompILer/CodeGen/IEC_Interfaces/IEC_FunctionBlock.java
@@ -8,8 +8,7 @@ package org.supremica.softplc.CompILer.CodeGen.IEC_Interfaces;
  */
 public interface IEC_FunctionBlock
 {
-
-	/**run is used for il CAL operations of this kind
+	/**run is used for il CAL operations of this kind ie.
 	 *   var
 	 *      neg : fb;
 	 *      a,b : BOOL;
@@ -19,6 +18,7 @@ public interface IEC_FunctionBlock
 	 *   CAL neg
 	 *   LD neg.outputvariable1
 	 *   ST a
+
 	 * and of this kind
 	 *   var
 	 *      neg : fb;
@@ -27,28 +27,6 @@ public interface IEC_FunctionBlock
 	 *   LD TRUE
 	 *   ST a
 	 *   CAL neg(inputvariable1 := a, outputvariable2 => b)
-	 * and run is also called from within runArgs
 	 */
 	void run();
-
-	/*
-	 * runArgs is used for il CAL operations of this kind
-	 *   var
-	 *      neg : fb;
-	 *      a,b : BOOL;
-	 *   end_var
-	 *   LD TRUE
-	 *   ST a
-	 *   CAL neg(a)
-	 *   LD neg.outputvariable1
-	 *   ST a
-	 * where neg's internal name of the inputvariable is unknown
-	 * Call operations using this style must have the appropriate number
-	 * of inputs
-	 */
-
-	// void runArgs(String a, int b)
-	// is an example of a runArgs method for a function block with two inputs
-	// IEC_FunctionBlock(Object owner);
-	// owner should be used for implementing var_external declaration.
 }
--- src/org/supremica/softplc/CompILer/ilc.java
+++ src/org/supremica/softplc/CompILer/ilc.java
@@ -1,3 +1,6 @@
+/** ilc is a compiler for parts of the IEC Instruction List language.
+ * @author Anders Röding
+ */
 package org.supremica.softplc.CompILer;
 
 import org.supremica.softplc.CompILer.CodeGen.*;
@@ -7,25 +10,63 @@ import java.io.*;
 
 public class ilc
 {
-	public static void main(String[] args)
-	{
-		if (args.length < 1)
+    public static void main(String[] args) {
+	if (args.length < 1)
+	    {
+		System.err.println("Usage: ilcompiler file.il");
+		return;
+	    }
+	try {
+	    BufferedReader ilReader = new BufferedReader(new FileReader(new File(args[0])));
+	}
+	catch (Throwable e) {
+	    System.err.println("Error reading file " + args[0]);
+	}
+    }
+
+    public ilc(BufferedReader ilReader) {
+	try {
+	    parser p = new parser(ilReader);
+	    
+	    try {
+		SimpleNode n = p.Start();
+		
+		n.dump("");
+		
+		new VariableChecker(n);
+		
+		n.dump("");
+		
+		JavaBytecodeGenerator jb = new JavaBytecodeGenerator(n, null/*output directory*/);
+		File temp = jb.getTempFile();
+	    }
+	    catch (Exception e)
 		{
-			System.err.println("Usage: ilcompiler file.il");
-			return;
+		    System.out.println("Ooops");
+		    System.out.println(e.getMessage());
+		    e.printStackTrace();
 		}
-
-		try
+	}
+	catch (Throwable e)
+	    {
+		System.out.println("Unable to parse input " + e);
+	    }
+    }
+    
+    /*
+      public static void main(String[] args)
+      {
+      try
+      {
+      parser p = new parser(new BufferedReader(new FileReader(new File(args[0]))));
+      
+      try
 		{
-			parser p = new parser(new BufferedReader(new FileReader(new File(args[0]))));
-
-			try
-			{
-				SimpleNode n = p.Start();
-
-				n.dump("");
-
-				new VariableChecker(n);
+		SimpleNode n = p.Start();
+		
+		n.dump("");
+		
+		new VariableChecker(n);
 
 				n.dump("");
 
@@ -44,4 +85,5 @@ public class ilc
 			System.out.println("Unable to parse input " + e);
 		}
 	}
+    */
 }
