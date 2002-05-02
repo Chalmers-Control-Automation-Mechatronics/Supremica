321b27930ce66ff76b2f8e67ce4b89d47aa128be
d99this <unknown@anywhere.com>
2002-05-02 13:17:54 +0000

    *** empty log message ***

---
+++ tests/ilprograms/caltestRunner.java
@@ -0,0 +1,12 @@
+public class caltestRunner{
+    
+    public static void main(String args[]) {
+		boolean[] in = null;
+		boolean[] out = null;
+		
+		CALTEST caltest = new CALTEST(in, out);
+		
+		caltest.run();
+    }
+ 
+}
