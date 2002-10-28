f9893f705fa938e166a4e103dec13ba1f9e95d40
Knut Akesson <knut@chalmers.se>
2002-10-28 15:44:46 +0000

    *** empty log message ***

--- templates/JGrafChart.cmd
+++ templates/JGrafChart.cmd
@@ -1,2 +1,2 @@
-@java -cp Supremica.jar org.jgrafchart.Basic2GC %1 %2 %3 %4 %5 %6 %7 %8 %9
+@java -cp Supremica.jar;SupremicaLib.jar org.jgrafchart.Basic2GC %1 %2 %3 %4 %5 %6 %7 %8 %9
 
--- templates/SupremicaTeacher.cmd
+++ templates/SupremicaTeacher.cmd
@@ -1,1 +1,1 @@
-@java -cp Supremica.jar org.supremica.apps.SupremicaTeacher %1 %2 %3 %4 %5 %6 %7 %8 %9
+@java -cp Supremica.jar;SupremicaLib.jar org.supremica.apps.SupremicaTeacher %1 %2 %3 %4 %5 %6 %7 %8 %9
