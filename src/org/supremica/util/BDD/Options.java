2c9caa7893d835268616f4da77ab2682e2247ec0
Arash Vahidi <arash@sics.se>
2002-10-28 12:47:51 +0000

    added algo_family and trace_on

--- src/org/supremica/util/BDD/Options.java
+++ src/org/supremica/util/BDD/Options.java
@@ -2,10 +2,19 @@ package org.supremica.util.BDD;
 
 public class Options
 {
-	static final int LINE_WIDTH = 55;    // # of chars per line?, screen width
-	static final boolean use_cudd = false;
-	static final boolean debug_on = false;
-	static final boolean sanity_check_on = false;
-	static final boolean user_alters_PCG = false;
-	static final boolean show_grow = false;
+    public static final int 
+	ALGO_MONOLITHIC = 0,
+	ALGO_CONJUNCTIVE = 1,
+	ALGO_DISJUNCTIVE = 2,
+	ALGO_SMOOTHED = 3
+	;
+
+	public static final int LINE_WIDTH = 55;    // # of chars per line?, screen width
+	public static final boolean use_cudd = false;
+	public static boolean debug_on = false;
+	public static boolean sanity_check_on = false;
+	public static boolean user_alters_PCG = false;
+	public static boolean show_grow = false;
+    public static boolean trace_on = false;
+    public static int algo_family = ALGO_MONOLITHIC;
 }
