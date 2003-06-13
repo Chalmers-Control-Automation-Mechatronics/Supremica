ea35989afa6a5404843d52678d3509fcfe06d975
Knut Akesson <knut@chalmers.se>
2003-06-13 10:22:27 +0000

    *** empty log message ***

--- templates/SupremicaJGrafchartLocal.cmd
+++ templates/SupremicaJGrafchartLocal.cmd
@@ -1,4 +1,4 @@
-@cd ..\resources\jgrafchart
+@cd ..\resources\jgrafchart\bin
 
 @set LIB=../lib
 @set CODE=../code
@@ -7,4 +7,6 @@
 @REM set CP="%CODE%;%LIB%/JGo.jar;%LIB%/crimson.jar;%LIB%/xalan.jar;%LIB%/jaxp.jar;%LIB%/regler.jar;%LIB%/xmlBlaster.jar;%LIB%/jutils.jar;%LIB%/xtdash.jar;%LIB%/jacorb.jar;%LIB%/jh.jar;%LIB%/CCOM.jar"
 @REM java -classpath %CP% grafchart.sfc.Editor  -graphicalSteps 1 -geometry 1024x768
 
-@java -cp ..\..\build;..\..\lib\unjared\;..\images org.supremica.apps.Supremica -p SupremicaProperties.cfg %1 %2 %3 %4 %5 %6 %7 %8 %9
+@java -cp ..\..\..\build;..\..\..\lib\unjared\;..\..\..\images org.supremica.apps.Supremica -p ..\..\..\dist\SupremicaProperties.cfg %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+@cd ..\..\..\dist
