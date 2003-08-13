68ddec44a3bbbda80745c1b39783a320d432ef69
f99arhu <unknown@anywhere.com>
2003-08-13 06:41:00 +0000

    *** empty log message ***

--- src/org/supremica/external/shoefactory/Executor/FactoryExecutor.java
+++ src/org/supremica/external/shoefactory/Executor/FactoryExecutor.java
@@ -4,9 +4,16 @@ import org.supremica.external.shoefactory.plantBuilder.*;
 import org.supremica.external.shoefactory.Animator.*;
 import org.supremica.gui.*;
 import grafchart.sfc.*;
-
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.io.*;
 import java.net.URL;
 import java.util.*;
+import  org.supremica.automata.*;
+import org.supremica.log.*;
+
+
 
 public class FactoryExecutor
 {
@@ -16,6 +23,7 @@ public class FactoryExecutor
 	static Gui gui;
 	static EditorAPI e;
 	static ArrayList shoes = new ArrayList ();
+	static int[] syncAutomata = {45,44,43,42,41,40,39,38,37,36,35,34,33,32,31,30,29,28,27,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0};
 
 	public FactoryExecutor(boolean [] sv, Gui g)
 	{
@@ -27,9 +35,9 @@ public class FactoryExecutor
 		{
 			if(shoeNr==1)
 			{
-				e= new EditorAPI(args);
+				e = new EditorAPI(args);
 				Editor.singleton = e;
-				Editor.removePaletteAction();
+				e.removePaletteAction();
 
 				URL url = Supremica.class.getResource("/shoefactory/ShoeFactory.xml");
 				//GCDocument top = e.openWorkspace(url.getPath());
@@ -43,10 +51,8 @@ public class FactoryExecutor
 				Specification shoeSpec = new Specification(shoeNr,sv);
 				gui.addProject(shoeSpec.getSpec());
 
-				int[] syncAutomata = {45,44,43,42,41,40,39,38,37,36,35,34,33,32,31,30,29,28,27,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0};
 				SyncBuilder syncPlant = new SyncBuilder(gui, shoePlant.getPlant(), syncAutomata);
 				syncPlant.synthesizePlants("theSupervisor");
-				
 
 				GCDocument newShoe = e.newWorkspace();
 				Shoe s = new Shoe(newShoe, stationV, shoeNr);
@@ -74,20 +80,30 @@ public class FactoryExecutor
 			}
 			else
 			{
-				shoePlant.add_shoe(shoeNr);
+				shoePlant.add_shoe(shoeNr,true);
 				Specification shoeSpec = new Specification(shoeNr,sv);
 				gui.addProject(shoeSpec.getSpec());
 
 				GCDocument newShoe = e.newWorkspace();
 				Shoe s = new Shoe(newShoe, stationV, shoeNr);
 				shoes.add(shoeNr-1,newShoe);
+				
+				
+				remove_Aut(44+shoeNr);				
+							
+				
+				syncAutomata = addSyncAutomata(syncAutomata,shoeNr+44);
+				SyncBuilder syncPlant = new SyncBuilder(gui, shoePlant.getPlant(), syncAutomata);
+				syncPlant.synthesizePlants("theSupervisor");
+				
 				shoeNr++;
-
+/*
 				boolean OK = e.compileWorkspace(newShoe);
 				if(OK)
 				{
 					//e.startWorkspace(newShoe);
 				}
+*/
 			}
 		}
 		catch(Exception ex)
@@ -101,23 +117,39 @@ public class FactoryExecutor
 		GCDocument temp = (GCDocument) shoes.get(nr-1);
 		e.stopWorkspace(temp);
 		//e.deleteWorkspace(temp);
+		shoePlant.remove_shoe(nr);
 		return true;
 	}
-
-	public static int fiveRot (int n)
+	
+	public static int[] addSyncAutomata(int[] aut, int nr)
 	{
-		if(n>8 || n<3)
-			return n;
-		else
-			return n+6;
+		int[] newSync = new int[aut.length+1];
+		System.arraycopy(aut,0,newSync,1,aut.length);
+		newSync[0]=nr;	
+		return newSync;
 	}
 
-	public static int fiveRotdown (int n)
+	public static void remove_Aut(int nr)
 	{
-		if(n<9)
-			return n;
-		else
-			return n-6;
-	}
+		int [] sel ={nr};
+		gui.selectAutomata(sel);
+		Automata selectedAutomata = gui.getSelectedAutomata();
+		Iterator autIt = selectedAutomata.iterator();
+			while (autIt.hasNext())
+			{
+			Automaton currAutomaton = (Automaton) autIt.next();
+			String currAutomatonName = currAutomaton.getName();
+
+			try
+			{
+				gui.getVisualProjectContainer().getActiveProject().removeAutomaton(currAutomatonName);
+			}
+			catch (Exception ex)
+				{
+			
+				}	
+			}
+
+	}	
 
 }
