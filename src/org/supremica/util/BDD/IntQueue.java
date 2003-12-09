145e9b4ebc62f9d2f302de63fef102b8d81ec88b
Arash Vahidi <arash@sics.se>
2003-12-09 12:05:46 +0000

    IntQueue

---
+++ src/org/supremica/util/BDD/IntQueue.java
@@ -0,0 +1,46 @@
+package org.supremica.util.BDD;
+
+
+
+/**
+ * A ring queue opf integers
+ *
+ */
+public class IntQueue {
+	private int size, read, write;
+	private int []data;
+	public IntQueue(int size) {
+		this.size = size;
+		this.read = 0;
+		this.write = 0;
+		this.data = new int[size];
+	}
+
+	public void enqueue(int x) {
+		data[write] = x;
+		write = (write + 1) % size;
+	}
+
+	public int dequeue() {
+		int ret = data[read];
+		read = (read + 1) % size;
+		return ret;
+	}
+
+	public boolean empty() { return (read == write); }
+
+/*
+	public static void main(String [] args) {
+		IntQueue q = new IntQueue (5);
+
+		q.enqueue(0);
+		q.enqueue(2);
+		q.enqueue(3);
+		q.enqueue(4);
+
+		while(!q.empty()) {
+			System.out.println("-->" + q.dequeue() );
+		}
+	}
+	*/
+}
