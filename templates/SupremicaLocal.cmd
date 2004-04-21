371397bf9e38b9abb9d5cd040a4610c476f1d0d8
Tord Andersson <unknown@anywhere.com>
2004-04-21 11:26:16 +0000

    Made it possible to open the included examples when running SupremicaLocal

--- templates/SupremicaLocal.cmd
+++ templates/SupremicaLocal.cmd
@@ -1,1 +1,1 @@
-@java -cp ..\build;..\lib\unjared\;..\images org.supremica.apps.Supremica -p SupremicaProperties.cfg %1 %2 %3 %4 %5 %6 %7 %8 %9
+@java -cp ..\build;..\lib\unjared\;..\images;..\examples org.supremica.apps.Supremica -p SupremicaProperties.cfg %1 %2 %3 %4 %5 %6 %7 %8 %9
