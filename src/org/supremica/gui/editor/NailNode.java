b9705c29a4a6bee333e70846ba3fca69d55f0ba2
Knut Akesson <knut@chalmers.se>
2004-04-21 08:47:58 +0000

    *** empty log message ***

---
+++ src/org/supremica/gui/editor/NailNode.java
@@ -0,0 +1,283 @@
+
+/*
+ *  Supremica Software License Agreement
+ *
+ *  The Supremica software is not in the public domain
+ *  However, it is freely available without fee for education,
+ *  research, and non-profit purposes.  By obtaining copies of
+ *  this and other files that comprise the Supremica software,
+ *  you, the Licensee, agree to abide by the following
+ *  conditions and understandings with respect to the
+ *  copyrighted software:
+ *
+ *  The software is copyrighted in the name of Supremica,
+ *  and ownership of the software remains with Supremica.
+ *
+ *  Permission to use, copy, and modify this software and its
+ *  documentation for education, research, and non-profit
+ *  purposes is hereby granted to Licensee, provided that the
+ *  copyright notice, the original author's names and unit
+ *  identification, and this permission notice appear on all
+ *  such copies, and that no charge be made for such copies.
+ *  Any entity desiring permission to incorporate this software
+ *  into commercial products or to use it for commercial
+ *  purposes should contact:
+ *
+ *  Knut Akesson (KA), knut@supremica.org
+ *  Supremica,
+ *  Haradsgatan 26A
+ *  431 42 Molndal
+ *  SWEDEN
+ *
+ *  to discuss license terms. No cost evaluation licenses are
+ *  available.
+ *
+ *  Licensee may not use the name, logo, or any other symbol
+ *  of Supremica nor the names of any of its employees nor
+ *  any adaptation thereof in advertising or publicity
+ *  pertaining to the software without specific prior written
+ *  approval of the Supremica.
+ *
+ *  SUPREMICA AND KA MAKES NO REPRESENTATIONS ABOUT THE
+ *  SUITABILITY OF THE SOFTWARE FOR ANY PURPOSE.
+ *  IT IS PROVIDED "AS IS" WITHOUT EXPRESS OR IMPLIED WARRANTY.
+ *
+ *  Supremica or KA shall not be liable for any damages
+ *  suffered by Licensee from the use of this software.
+ *
+ *  Supremica is owned and represented by KA.
+ */
+package org.supremica.gui.editor;
+
+import com.nwoods.jgo.*;
+import java.awt.*;
+
+public class NailNode
+	extends JGoArea
+{
+	static int myStdSize = 20;
+
+	// Nail
+	protected JGoEllipse myEllipse = null;
+	protected NailPort thePort = null;
+
+	/**
+	 * A newly constructed StateNode is not usable until you've
+	 * called initialize().
+	 *
+	 *@param  theState Description of the Parameter
+	 */
+	public NailNode()
+	{
+		super();
+	}
+
+	public JGoObject copyObject(JGoCopyEnvironment env)
+	{
+		NailNode newobj = (NailNode) super.copyObject(env);
+
+		if (newobj != null)
+		{
+
+			// the JGoObjects that are part of this area are copied
+			// separately by copyChildren()
+			// no other fields to copy
+		}
+
+		return newobj;
+	}
+
+	// Fix how to copy a state !??
+	public void copyChildren(JGoArea newarea, JGoCopyEnvironment env)
+	{
+
+		// don't bother calling JGoArea's default implementation,
+		// so that we can set our fields explicitly
+		NailNode newobj = (NailNode) newarea;
+
+		if (myEllipse != null)
+		{
+			newobj.myEllipse = (JGoEllipse) myEllipse.copyObject(env);
+
+			newobj.addObjectAtHead(newobj.myEllipse);
+		}
+
+		Point c = newobj.myEllipse.getSpotLocation(Center);
+		NailPort op = thePort;
+		NailPort np = (NailPort) op.copyObject(env);
+
+		newobj.thePort = np;
+
+		newobj.addObjectAtTail(np);
+
+		np.myEllipse = newobj.myEllipse;
+	}
+
+	/**
+	 * Keep the parts of a NailNode positioned relative to each other
+	 * by setting their locations using some of the standard spots of
+	 * a JGoObject.
+	 * <p>
+	 * By default the label will be positioned at the bottom of the node,
+	 * above the ellipse.  To change this to be below the ellipse, at
+	 * the bottom of the node, change the myLabel.setSpotLocation() call.
+	 */
+	public void layoutChildren()
+	{
+		if (myEllipse == null)
+		{
+			return;
+		}
+
+		if (thePort != null)
+		{
+			Point c = myEllipse.getSpotLocation(Center);
+			thePort.setSpotLocation(Center, c.x, c.y);
+		}
+	}
+
+	public void initialize()
+	{
+		setSize(myStdSize, myStdSize);
+
+		// the area as a whole is not directly selectable using a mouse,
+		// but the area can be selected by trying to select any of its
+		// children, all of whom are currently !isSelectable().
+		setSelectable(false);
+		setGrabChildSelection(true);
+
+		// the user can move this node around
+		setDraggable(true);
+
+		// the user cannot resize this node
+		setResizable(false);
+
+		// create the bigger circle/ellipse around and behind the port
+		myEllipse = new JGoEllipse(getTopLeft(), getSize());
+
+		myEllipse.setSelectable(false);
+		myEllipse.setDraggable(false);
+
+		// can't setLocation until myEllipse exists
+		//myEllipse.setSpotLocation(JGoObject.Center, theState.getLocation());
+
+		// if there is a string, create a label with a transparent
+		// background that is centered
+		// String labeltext = theState.getName();
+/*
+		if (labeltext == null)
+		{
+			labeltext = theState.getId();
+		}
+*/
+		/*
+		 *  if (labeltext != null)
+		 *  {
+		 *  myLabel = new JGoText(labeltext);
+		 *  myLabel.setSelectable(false);
+		 *  myLabel.setDraggable(false);
+		 *  myLabel.setAlignment(JGoText.ALIGN_CENTER);
+		 *  myLabel.setTransparent(true);
+		 *  }
+		 */
+
+		// create a Port, which knows how to make sure
+		// connected JGoLinks have a reasonable end point
+		thePort = new NailPort();
+		thePort.myEllipse = myEllipse;
+
+		thePort.setSize(8, 8);
+
+		// add all the children to the area
+		addObjectAtHead(myEllipse);
+
+//		if (myLabel != null)
+//		{
+//			addObjectAtTail(myLabel);
+//		}
+
+		addObjectAtTail(thePort);
+
+		// now position the label and port appropriately
+		// relative to the ellipse
+		layoutChildren();
+		setBrush(JGoBrush.white);
+		setColor(Color.blue);
+	}
+
+	public String getToolTipText()
+	{
+		return "Nail";
+	}
+
+
+	// Convenience methods: control the ellipse's pen and brush
+	public JGoPen getPen()
+	{
+		return myEllipse.getPen();
+	}
+
+	public void setPen(JGoPen p)
+	{
+		myEllipse.setPen(p);
+	}
+
+	public JGoBrush getBrush()
+	{
+		return myEllipse.getBrush();
+	}
+
+	public void setBrush(JGoBrush b)
+	{
+		myEllipse.setBrush(b);
+	}
+
+	public Color getColor()
+	{
+		return getPen().getColor();
+	}
+
+	public void setColor(Color c)
+	{
+		setPen(JGoPen.makeStockPen(c));
+	}
+
+	/*
+	 *  public void colorChange()
+	 *  {
+	 *  Color c = getColor();
+	 *  if (c == Color.red)
+	 *  {
+	 *  setColor(Color.green);
+	 *  }
+	 *  else if (c == Color.green)
+	 *  {
+	 *  setColor(Color.blue);
+	 *  }
+	 *  else if (c == Color.blue)
+	 *  {
+	 *  setColor(Color.red);
+	 *  }
+	 *  }
+	 */
+	public JGoPort getPort()
+	{
+		return thePort;
+	}
+
+	static int getStdSize()
+	{
+		return myStdSize;
+	}
+
+	static void setStdSize(int size)
+	{
+		myStdSize = size;
+	}
+
+	public JGoObject getEllipse()
+	{
+		return myEllipse;
+	}
+
+}
---
+++ src/org/supremica/gui/editor/NailPort.java
@@ -0,0 +1,152 @@
+
+/*
+ * Supremica Software License Agreement
+ *
+ * The Supremica software is not in the public domain
+ * However, it is freely available without fee for education,
+ * research, and non-profit purposes.  By obtaining copies of
+ * this and other files that comprise the Supremica software,
+ * you, the Licensee, agree to abide by the following
+ * conditions and understandings with respect to the
+ * copyrighted software:
+ *
+ * The software is copyrighted in the name of Supremica,
+ * and ownership of the software remains with Supremica.
+ *
+ * Permission to use, copy, and modify this software and its
+ * documentation for education, research, and non-profit
+ * purposes is hereby granted to Licensee, provided that the
+ * copyright notice, the original author's names and unit
+ * identification, and this permission notice appear on all
+ * such copies, and that no charge be made for such copies.
+ * Any entity desiring permission to incorporate this software
+ * into commercial products or to use it for commercial
+ * purposes should contact:
+ *
+ * Knut Akesson (KA), knut@supremica.org
+ * Supremica,
+ * Haradsgatan 26A
+ * 431 42 Molndal
+ * SWEDEN
+ *
+ * to discuss license terms. No cost evaluation licenses are
+ * available.
+ *
+ * Licensee may not use the name, logo, or any other symbol
+ * of Supremica nor the names of any of its employees nor
+ * any adaptation thereof in advertising or publicity
+ * pertaining to the software without specific prior written
+ * approval of the Supremica.
+ *
+ * SUPREMICA AND KA MAKES NO REPRESENTATIONS ABOUT THE
+ * SUITABILITY OF THE SOFTWARE FOR ANY PURPOSE.
+ * IT IS PROVIDED "AS IS" WITHOUT EXPRESS OR IMPLIED WARRANTY.
+ *
+ * Supremica or KA shall not be liable for any damages
+ * suffered by Licensee from the use of this software.
+ *
+ * Supremica is owned and represented by KA.
+ */
+package org.supremica.gui.editor;
+
+import com.nwoods.jgo.*;
+import java.awt.*;
+
+/**
+* To get the link arrowheads' point at the edge of the ellipse,
+* we need to override how the link point is computed, rather
+* than depending on the built-in mechanism specifying a spot.
+*/
+public class NailPort
+	extends JGoPort
+{
+	public JGoEllipse myEllipse = null;
+
+	public NailPort()
+	{
+		super();
+
+		setSelectable(false);
+		setDraggable(false);
+		setStyle(StyleEllipse);    // black circle/ellipse
+
+		// use custom link spots for both links coming in and going out
+		setFromSpot(JGoObject.NoSpot);
+		setToSpot(JGoObject.NoSpot);
+	}
+
+	/**
+	* Return a point on the edge of the ellipse
+	*/
+	public Point getLinkPointFromPoint(int x, int y, Point p)
+	{
+		if (p == null)
+		{
+			p = new Point();
+		}
+
+		p.x = x;
+		p.y = y;
+
+		Point center = getSpotLocation(JGoObject.Center);
+		double x1 = (double) x;
+		double y1 = (double) y;
+		double x2 = center.x;
+		double y2 = center.y;
+
+		center = myEllipse.getSpotLocation(JGoObject.Center);
+
+		double U = center.x;
+		double V = center.y;
+		double P = myEllipse.getWidth();
+		double Q = myEllipse.getHeight();
+		double A = (4.0 / (P * P)) * (x2 - x1) * (x2 - x1) + (4.0 / (Q * Q)) * (y2 - y1) * (y2 - y1);
+		double B = (8.0 / (P * P)) * (x2 - x1) * (x1 - U) + (8.0 / (Q * Q)) * (y2 - y1) * (y1 - V);
+		double C = (4.0 / (P * P)) * (x1 - U) * (x1 - U) + (4.0 / (Q * Q)) * (y1 - V) * (y1 - V) - 1.0;
+		double D = B * B - 4.0 * A * C;
+
+		if (D < 0)
+		{
+			return p;
+		}
+
+		double T1 = (-1.0 * B - Math.sqrt(D)) / (2.0 * A);
+		double T2 = (-1.0 * B + Math.sqrt(D)) / (2.0 * A);
+		double t1 = Math.min(T1, T2);
+		double t2 = Math.max(T1, T2);
+
+		if ((0.0 <= t1) && (t1 <= 1.0))
+		{
+			p.x = (int) Math.round(x1 + t1 * (x2 - x1));
+			p.y = (int) Math.round(y1 + t1 * (y2 - y1));
+
+			return p;
+		}
+		else if ((0.0 <= t2) && (t2 <= 1.0))
+		{
+			p.x = (int) Math.round(x1 + t2 * (x2 - x1));
+			p.y = (int) Math.round(y1 + t2 * (y2 - y1));
+
+			return p;
+		}
+
+		return p;
+	}
+
+	/*
+	 *       boolean isValidDestination()
+	 *       {
+	 *               return true;
+	 *       }
+	 *
+	 *       boolean isValidLink()
+	 *       {
+	 *               return true;
+	 *       }
+	 *
+	 *       boolean isValidSource()
+	 *       {
+	 *               return true;
+	 *       }
+	 */
+}
