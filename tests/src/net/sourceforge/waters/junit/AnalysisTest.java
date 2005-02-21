f3501b5d059c7bc1c6c0779a973db73fe72732ce
Knut Akesson <knut@chalmers.se>
2005-02-21 02:51:15 +0000

    *** empty log message ***

---
+++ tests/src/net/sourceforge/waters/junit/AnalysisTest.java
@@ -0,0 +1,101 @@
+//###########################################################################
+//# PROJECT: Waters
+//# PACKAGE: net.sourceforge.waters.junit
+//# CLASS:   AlgorithmsTest
+//###########################################################################
+//# $Id: AnalysisTest.java,v 1.1 2005-02-21 02:51:15 knut Exp $
+//###########################################################################
+
+
+package net.sourceforge.waters.junit;
+
+import java.io.File;
+import java.io.IOException;
+import javax.xml.bind.JAXBException;
+import junit.framework.TestCase;
+
+import net.sourceforge.waters.model.analysis.ProductDESCopier;
+import net.sourceforge.waters.model.analysis.ProductDESResult;
+import net.sourceforge.waters.model.base.DocumentManager;
+import net.sourceforge.waters.model.base.ProxyMarshaller;
+import net.sourceforge.waters.model.base.WatersException;
+import net.sourceforge.waters.model.compiler.ModuleCompiler;
+import net.sourceforge.waters.model.des.ProductDESMarshaller;
+import net.sourceforge.waters.model.des.ProductDESProxy;
+import net.sourceforge.waters.model.module.ModuleMarshaller;
+import net.sourceforge.waters.model.module.ModuleProxy;
+
+
+public class AnalysisTest extends TestCase
+{
+
+  //#########################################################################
+  //# Test Cases
+  public void testCopy_machine()
+    throws IOException, JAXBException, WatersException
+  {
+    testCopy("machine");
+  }
+
+
+  //#########################################################################
+  //# Utilities
+  void testCopy(final String modname)
+    throws IOException, JAXBException, WatersException
+  {
+    final String inextname = modname + mModuleMarshaller.getDefaultExtension();
+    final File infilename = new File(mInputDirectory, inextname);
+    final ProductDESProxy des = compile(infilename);
+    final ProductDESCopier copier = new ProductDESCopier(des);
+    final ProductDESResult result = (ProductDESResult) copier.run();
+    assertTrue("Unexpected result value!", result.getSatisfied() == false);
+    final ProductDESProxy copy = result.getProductDES();
+    final String desname = des.getName();
+    final String copyname = copy.getName();
+    assertTrue("Unexpected result name!", desname.equals(copyname));
+  }
+
+  ProductDESProxy compile(final File infilename)
+    throws JAXBException, WatersException, IOException
+  {
+    final ModuleProxy module =
+      (ModuleProxy) mModuleMarshaller.unmarshal(infilename);
+    final ModuleCompiler compiler =
+      new ModuleCompiler(module, mDocumentManager);
+    return compiler.compile();
+  }
+
+
+  //#########################################################################
+  //# Overrides for junit.framework.TestCase
+  protected void setUp()
+    throws JAXBException
+  {
+    mModuleMarshaller = new ModuleMarshaller();
+    mDESMarshaller = new ProductDESMarshaller();
+    mDocumentManager = new DocumentManager();
+    mDocumentManager.register(mModuleMarshaller);
+    mDocumentManager.register(mDESMarshaller);
+    mInputDirectory = new File("examples", "handwritten");
+    mOutputDirectory = new File("logs", "analysis");
+  }
+
+  protected void tearDown()
+  {
+    mModuleMarshaller = null;
+    mDESMarshaller = null;
+    mDocumentManager = null;
+    mInputDirectory = null;
+    mOutputDirectory = null;
+  }
+
+
+  //#########################################################################
+  //# Data Members
+  private ModuleMarshaller mModuleMarshaller;
+  private ProductDESMarshaller mDESMarshaller;
+  private DocumentManager mDocumentManager;
+  private File mInputDirectory;
+  private File mOutputDirectory;
+
+}
---
+++ tests/src/net/sourceforge/waters/junit/JAXBTestCase.java
@@ -0,0 +1,125 @@
+//###########################################################################
+//# PROJECT: Waters
+//# PACKAGE: waters.junit
+//# CLASS:   JAXBTestCase
+//###########################################################################
+//# $Id: JAXBTestCase.java,v 1.1 2005-02-21 02:51:15 knut Exp $
+//###########################################################################
+
+
+package net.sourceforge.waters.junit;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.xml.bind.JAXBException;
+import junit.framework.TestCase;
+
+import net.sourceforge.waters.model.base.DocumentProxy;
+import net.sourceforge.waters.model.base.ElementProxy;
+import net.sourceforge.waters.model.base.ModelException;
+import net.sourceforge.waters.model.base.ModelPrinter;
+import net.sourceforge.waters.model.base.ProxyMarshaller;
+
+
+abstract class JAXBTestCase
+  extends TestCase
+{
+
+  //#########################################################################
+  //# Utilities
+  DocumentProxy testParse(final String name)
+    throws JAXBException, ModelException, IOException
+  {
+    final ProxyMarshaller marshaller = getProxyMarshaller();
+    final String extname = name + marshaller.getDefaultExtension();
+    final File filename = new File(getInputDirectory(), extname);
+    return testParse(filename);
+  }
+
+  DocumentProxy testParse(final File filename)
+    throws JAXBException, ModelException, IOException
+  {
+    final ProxyMarshaller marshaller = getProxyMarshaller();
+    final DocumentProxy proxy = marshaller.unmarshal(filename);
+    proxy.pprint(mPrinter);
+    mPrinter.flush();
+    return proxy;
+  }
+
+  DocumentProxy testMarshal(final String name)
+    throws JAXBException, ModelException, IOException
+  {
+    final ProxyMarshaller marshaller = getProxyMarshaller();
+    final String extname = name + marshaller.getDefaultExtension();
+    final File infilename = new File(getInputDirectory(), extname);
+    final File outfilename = new File(getOutputDirectory(), extname);
+    return testMarshal(infilename, outfilename);
+  }
+
+  DocumentProxy testMarshal(final File infilename, final File outfilename)
+    throws JAXBException, ModelException, IOException
+  {
+    final ProxyMarshaller marshaller = getProxyMarshaller();
+    final DocumentProxy proxy1 = marshaller.unmarshal(infilename);
+    marshaller.marshal(proxy1, outfilename);
+    final DocumentProxy proxy2 = marshaller.unmarshal(outfilename);
+    assertTrue("Structure changed after marshalling!",
+	       proxy1.equals(proxy2));
+    assertTrue("Structure changed after marshalling!",
+	       proxy2.equals(proxy1));
+    assertTrue("Geometry information changed after marshalling!",
+	       proxy1.equalsWithGeometry(proxy2));
+    assertTrue("Geometry information changed after marshalling!",
+	       proxy2.equalsWithGeometry(proxy1));
+    return proxy1;
+  }
+
+  void testHandcraft(final DocumentProxy handcrafted)
+    throws JAXBException, ModelException, IOException
+  {
+    final ProxyMarshaller marshaller = getProxyMarshaller();
+    final String name = handcrafted.getName();
+    final String extname = name + marshaller.getDefaultExtension();
+    final File infilename = new File(getInputDirectory(), extname);
+    final File outfilename = new File(getOutputDirectory(), extname);
+    marshaller.marshal(handcrafted, outfilename);
+    final DocumentProxy parsed = marshaller.unmarshal(infilename);
+    assertTrue("Constructed structure differs from expected in file!",
+	       handcrafted.equals(parsed));
+    assertTrue("Constructed structure differs from expected in file!",
+	       parsed.equals(handcrafted));
+    assertTrue("Constructed geometry info differs from expected in file!",
+	       handcrafted.equalsWithGeometry(parsed));
+    assertTrue("Constructed geometry info differs from expected in file!",
+	       parsed.equalsWithGeometry(handcrafted));
+  }
+
+
+  //#########################################################################
+  //# Provided by Subclasses
+  abstract ProxyMarshaller getProxyMarshaller();
+  abstract File getInputDirectory();
+  abstract File getOutputDirectory();
+
+
+  //#########################################################################
+  //# Overrides for junit.framework.TestCase
+  protected void setUp()
+    throws JAXBException
+  { 
+    final PrintWriter writer = new PrintWriter(System.out);
+    mPrinter = new ModelPrinter(writer);
+  }
+
+  protected void tearDown()
+  { 
+    mPrinter = null;
+  }
+
+
+  //#########################################################################
+  //# Data Members
+  private ModelPrinter mPrinter;
+
+}
