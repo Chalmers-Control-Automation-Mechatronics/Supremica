45774434a7c966e015045066b426eaad891e811b
Knut Akesson <knut@chalmers.se>
2005-03-04 08:18:39 +0000

    *** empty log message ***

---
+++ src/org/supremica/gui/ide/actions/EditorPrintAction.java
@@ -0,0 +1,33 @@
+package org.supremica.gui.ide.actions;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.ImageIcon;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import org.supremica.gui.ide.ModuleContainer;
+import org.supremica.gui.ide.IDE;
+
+public class EditorPrintAction
+	extends IDEAction
+{
+
+	public EditorPrintAction(IDEActionInterface ide)
+	{
+		super(ide);
+
+		putValue(Action.NAME, "Print...");
+		putValue(Action.SHORT_DESCRIPTION, "Print");
+		putValue(Action.SMALL_ICON, new ImageIcon(IDE.class.getResource("/toolbarButtonGraphics/general/Print16.gif")));
+	}
+
+	public void actionPerformed(ActionEvent e)
+	{
+		doAction();
+	}
+
+	public void doAction()
+	{
+		System.err.println("Print is not implemented yet!");
+	}
+}
---
+++ src/org/supremica/gui/ide/actions/EditorPrintPDFAction.java
@@ -0,0 +1,33 @@
+package org.supremica.gui.ide.actions;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.ImageIcon;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import org.supremica.gui.ide.ModuleContainer;
+import org.supremica.gui.ide.IDE;
+
+public class EditorPrintPDFAction
+	extends IDEAction
+{
+
+	public EditorPrintPDFAction(IDEActionInterface ide)
+	{
+		super(ide);
+
+		putValue(Action.NAME, "Print As PDF...");
+		putValue(Action.SHORT_DESCRIPTION, "Print As PDF");
+		putValue(Action.SMALL_ICON, new ImageIcon(IDE.class.getResource("/toolbarButtonGraphics/general/Print16.gif")));
+	}
+
+	public void actionPerformed(ActionEvent e)
+	{
+		doAction();
+	}
+
+	public void doAction()
+	{
+		System.err.println("Print as PDF is not implemented yet!");
+	}
+}
