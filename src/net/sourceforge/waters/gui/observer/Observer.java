e642f474e7222af4f56c8d07c0737995287c27a5
Simon Ware <siw4@cs.waikato.ac.nz>
2005-08-30 00:26:06 +0000

    :Observer interface

---
+++ src/net/sourceforge/waters/gui/observer/Observer.java
@@ -0,0 +1,19 @@
+package net.sourceforge.waters.gui.observer;
+
+/**
+ * The Observer Interface for the Observer Design Pattern.
+ *
+ * It makes So many it so that many method of viewing and Modifying Concrete data
+ * representation can Operate on the Same data representation at the Same Time and 
+ * can be Updated Simultaneously
+ *
+ * @author Simon Ware
+ */
+
+public interface Observer
+{
+    /**
+     * called to update the observer when its subject is modified
+     */
+    public void update();
+}
---
+++ src/net/sourceforge/waters/gui/observer/Subject.java
@@ -0,0 +1,50 @@
+/* Potato Chip Graph editor
+ * Subject.java - Subject/Observer inteface
+ * Copyright (C) 2005 - Alastair Porter, Gian Perrone and Simon Ware
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * $Id: Subject.java,v 1.1 2005-08-30 00:26:06 siw4 Exp $
+ */
+
+package net.sourceforge.waters.gui.observer;
+
+/**
+ * The Subject Interface for the Observer Design Pattern.
+ *
+ * It makes So many it so that many method of viewing and Modifying Concrete data
+ * representation can Operate on the Same data representation at the Same Time and 
+ * can be Updated Simultaneously
+ *
+ * @author Simon Ware
+ */
+
+public interface Subject
+{
+    /**
+     * attachs an Observer to this Subject.
+     */
+    public void attach(Observer o);
+
+    /**
+     * removes an Observer from this Subject
+     */
+    public void detach(Observer o);
+
+    /**
+     * notifies all observers of this Subject of a modification
+     */
+    public void notifyObservers();
+}
