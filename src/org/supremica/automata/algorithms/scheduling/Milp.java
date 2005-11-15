76396cb26c3bd6a492f22647e676c9ee9af6aef1
Avenir Kobetski <kavenir@hotmail.com>
2005-11-15 16:54:40 +0000

    *** empty log message ***

--- src/org/supremica/automata/algorithms/scheduling/Milp.java
+++ src/org/supremica/automata/algorithms/scheduling/Milp.java
@@ -5,6 +5,7 @@ import java.io.*;
 
 import org.supremica.log.*;
 import org.supremica.automata.*;
+import org.supremica.automata.algorithms.*;
 
 public class Milp 
 	implements Scheduler
@@ -14,16 +15,16 @@ public class Milp
 	private final String MODEL_FILE_NAME = "C:\\Documents and Settings\\avenir\\Desktop\\temp.mod";
 // 	private final String MODEL_FILE_NAME = "C:\\Documents and Settings\\Avenir\\Skrivbord\\temp.mod";
 
-	private Automata theAutomata;
+	private Automata theAutomata, robots, zones;
 
 //  	public native void jniTest();
 	
 	public Milp(Automata theAutomata) 
 		throws Exception
 	{
-		this.theAutomata = theAutomata;
-
 		logger.info("In the Milp.java");
+
+		initAutomata(theAutomata);
 
 		convertXmlToMod();
 
@@ -32,7 +33,7 @@ public class Milp
 
 	public int[] schedule()
 	{
-		return null;
+		return   null;
 	}
 
 	public Automaton buildScheduleAutomaton(int[] markedNode) 
@@ -40,20 +41,128 @@ public class Milp
 		return null;
 	}
 
-	private void convertXmlToMod() 
+	private void initAutomata(Automata theAutomata)
 		throws Exception
 	{
-		Automata robots = theAutomata.getPlantAutomata(); 
-		Automata zones = theAutomata.getSpecificationAutomata(); 
+		this.theAutomata = theAutomata;
 
+		robots = theAutomata.getPlantAutomata(); 
+		zones = theAutomata.getSpecificationAutomata(); 
+
+		// The robots synchronized with all corresponding specifications (except mutex zone specifications)
+		Automata restrictedRobots = new Automata();
+
+		for (int i=0; i<robots.size(); i++)
+		{
+			Automaton currRobot = robots.getAutomatonAt(i);
+			String currRobotName = currRobot.getName();
+			
+			for (int j=0; j<zones.size(); j++)
+			{
+				Automaton currSpec = zones.getAutomatonAt(j);
+				
+				if (currSpec.getName().contains(currRobotName))
+				{
+					Automata toBeSynched = new Automata(currRobot);
+					toBeSynched.addAutomaton(currSpec);
+
+					currRobot = AutomataSynchronizer.synchronizeAutomata(toBeSynched);
+				
+					currRobot.setName(currRobotName);
+					currRobot.setType(AutomatonType.Plant);
+
+					zones.removeAutomaton(currSpec);
+				}
+			}
+			
+			restrictedRobots.addAutomaton(currRobot);
+		}
+
+		robots = restrictedRobots;
+	}
+
+	private void convertXmlToMod() 
+		throws Exception
+	{	
 		int nrOfRobots = robots.size();
 		int nrOfZones = zones.size();
+
+		// Finding maximum number of time variables per robot (i.e. max nr of states)
+		int nrOfTics = 0;
+		for (int i=0; i<nrOfRobots; i++)
+		{
+			int nbrOfStates = robots.getAutomatonAt(i).nbrOfStates();
+			if (nbrOfStates > nrOfTics)
+				nrOfTics = nbrOfStates;
+		}
+		nrOfTics--;
+
+		// The string containing precedence constraints 
+		String precConstraints = "";
+		
+		// Making deltaTime-header
+		String deltaTime = "param deltaTime default 0\n:";
+
+		for (int i=0; i<nrOfTics; i++)
+			deltaTime += "\t" + i;
+
+		deltaTime += " :=\n";
+
+		// Extracting deltaTimes for each robot
+		for (int i=0; i<nrOfRobots; i++) 
+		{
+			int currTic = 0;
+			deltaTime += i;
+			
+			Automaton currRobot = robots.getAutomatonAt(i);
+			StateIterator states = currRobot.stateIterator();
+
+			while (states.hasNext())
+			{
+				State currState = states.nextState();
+				
+				if (!currState.isAccepting())
+				{
+					deltaTime += "\t" + currState.getCost();	
+
+					StateIterator nextStates = currState.nextStateIterator();
+					int nbrOfOutgoingArcs = currState.nbrOfOutgoingArcs();
+
+					if (nbrOfOutgoingArcs == 1) 
+					{
+						State nextState = nextStates.nextState();
+						
+						precConstraints += "prec_" + currRobot.getName() + "_" + currState.getName() + "_" + nextState.getName() + ": ";
+						precConstraints += "time[" + i + ", " + nextState.getIndex() + "] >= time[" + i + ", " + currState.getIndex() + "] + deltaTime[" + i + ", " + currState.getIndex() + "];\n";
+					}
+					else if (nbrOfOutgoingArcs == 2)
+					{
+						// Implement t1 >= t0 + delta_t0 - M*alt0; t1_prim >= t0 + delta_t0 - M*(1 - alt0);
+					}
+					else if (nbrOfOutgoingArcs > 2)
+					{
+						// Implement t1 >= t0 + delta_t0 - M*alt0; t1_prim >= t0 + delta_t0 - M*alt0_prim; 
+						// t1_prim_prim >= t0 + delta_t0 - M*alt0_prim_prim; alt0 + alt0_prim + alt0_prim_prim = 1;
+					}
+
+					currTic++;
+				}
+			}
+
+			// If the number of states of the current automaton is less 
+			// than max_nr_of_states, the deltaTime-matrix is filled with points 
+			// i.e zeros. 
+			for (int j=currTic; j<nrOfTics; j++)
+				deltaTime += "\t.";
+
+			deltaTime += "\n";
+		}
+
+		// The writing part
 
 		File modelFile = new File(MODEL_FILE_NAME);
 		if (!modelFile.exists())
 			modelFile.createNewFile();
-
-		// The writing part
 
 		BufferedWriter w = new BufferedWriter(new FileWriter(modelFile));
 
@@ -90,19 +199,29 @@ public class Milp
 		w.newLine();
 		w.write("minimize makespan: c;");
 		w.newLine();
-		
-		// The precedence constraints
+
+		// The constraints section
 		w.newLine();
-		w.write("s.t. precedence{r in Robots, t in Tics: t>0}:");
-		w.newLine();
-		w.write("\ttime[r,t] >= time[r,t-1] + deltaTime[r,t];");
+		w.write("subject to");
 		w.newLine();
 
 		// The cycle time constraints
 		w.newLine();
-		w.write("s.t. cycle_time{r in Robots}: c >= time[r, maxTic];");
+		w.write("cycle_time{r in Robots}: c >= time[r, maxTic];");
 		w.newLine();
 		
+		// The precedence constraints
+		w.newLine();
+		w.write(precConstraints);
+		w.newLine();
+		
+// 		// The precedence constraints
+// 		w.newLine();
+// 		w.write("s.t. precedence{r in Robots, t in Tics: t>0}:");
+// 		w.newLine();
+// 		w.write("\ttime[r,t] >= time[r,t-1] + deltaTime[r,t];");
+// 		w.newLine();
+	
 		// The end of the model-section and the beginning of the data-section
 		w.newLine();
 		w.write("data;");
@@ -114,18 +233,9 @@ public class Milp
 		w.newLine();
 		w.write("param nrOfZones := " + (nrOfZones - 1) + ";");
 		w.newLine();
-		
-// 	// 	// This part should be automatized
-// 		int nrOfTics = 0;
-// 		for (int i=0; i<nrOfRobots; i++)
-// 		{
-// 			if (nbrOfStates > nrOfTics)
-// 				nrOfTics = nbrOfStates;
-// 		}
-
-// 		// behovs nrOfTics i *.mod-filen?
-// 		w.write("param maxTic := " + (nrOfTics - 1) + ";");
-// 		w.newLine();
+		// Behovs maxTic verkligen???
+		w.write("param maxTic := " + (nrOfTics - 1) + ";");
+		w.newLine();
 
 // 		String altRouteConstraints = "";
 // 		String precedenceConstraints = "";
@@ -141,16 +251,7 @@ public class Milp
 // 		w.newLine();
 // 		w.write(deltaTime);
 // 		w.newLine();
-		
-// 		for (int i=0; i<nrOfRobots; i++) 
-// 		{
-// 			int currTic = 0;
-// 			deltaTime = "" + i + "\t0";
-			
-// 			ArrayList<State> currStates = new ArrayList<State>();
-// 			ArrayList<State> closedStates = new ArrayList<State>();
-					
-// 			Automaton currAuto = robots.getAutomatonAt(i);
+
 // 			currStates.add(currAuto.getInitialState());
 
 // 			while (!currStates.isEmpty())
@@ -183,34 +284,13 @@ public class Milp
 
 // 			}
 			
-// 		}
-
-// 		String[] deltaTimes = new String[nrOfRobots + 1];
-// 		deltaTimes[0] = ":\t0";
-// 		for (int j=0; j<nrOfRobots; j++) 
-// 		{
-// 			deltaTimes[j] += j + "\t0";
-// 		}
-// 		for (int i=1; i<nrOfTics; i++)
-// 		{
-// 			deltaTime[0] += "\t" + i;
-			
-// 			for (int  j=0; j<nrOfRobots; j++) 
-// 			{
-// 				deltaTimes[j] += j + "\t0";
-// 			}
-// 		}
-// 		deltaTimes[0] += " :=";
-// 		w.write(":\t0\t1\t2\t3\t4\t5 :=");
-// 		w.newLine();
-// 		w.write("0\t0\t79\t65\t394\t29\t433");
-// 		w.newLine();
-// 		w.write("1\t0\t198\t411\t389\t0\t2 ;");
-// 		w.newLine();
+		w.newLine();
+		w.write(deltaTime);
+		w.newLine();
 
 		w.newLine();
 		w.write("end;");
 		w.flush();
 	}
-
+	
 }
