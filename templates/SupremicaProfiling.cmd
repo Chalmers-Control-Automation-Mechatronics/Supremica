27cfcd186fc5cd4027617a2aa24c4b7d91c63817
Knut Akesson <knut@chalmers.se>
2005-11-28 16:20:45 +0000

    *** empty log message ***

--- templates/SupremicaProfiling.cmd
+++ templates/SupremicaProfiling.cmd
@@ -1,1 +1,3 @@
-@java -agentlib:yjpagent  -Xmx500M -cp Supremica.jar;SupremicaLib.jar -enableassertions org.supremica.apps.Supremica -p SupremicaProperties.cfg %1 %2 %3 %4 %5 %6 %7 %8 %9
+@java -agentpath:"C:\Program Files\netbeans-5.0beta2\profiler1\lib\deployed\jdk15\windows\profilerinterface.dll=\"C:\Program Files\netbeans-5.0beta2\profiler1\lib\"",5140
+ -Xms500M -Xmx500M -cp Supremica.jar;SupremicaLib.jar -enableassertions org.supremica.apps.Supremica -p SupremicaProperties.cfg %1 %2 %3 %4 %5 %6 %7 %8 %9
+
