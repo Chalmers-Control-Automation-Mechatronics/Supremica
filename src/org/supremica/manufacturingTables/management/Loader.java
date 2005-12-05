169c37ae770c7720c04780690466bf3dffef0170
Oscar Ljungkrantz <oscar.ljungkrantz@gmail.com>
2005-12-05 08:03:50 +0000

    *** empty log message ***

--- src/org/supremica/manufacturingTables/management/Loader.java
+++ src/org/supremica/manufacturingTables/management/Loader.java
@@ -59,14 +59,13 @@
  */
 package org.supremica.manufacturingTables.management;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.util.Iterator;
 import java.util.List;
-import java.io.IOException;
+import java.io.*;
 import javax.xml.bind.*;
 import org.supremica.manufacturingTables.xsd.factory.*;
-
+import org.supremica.functionblocks.xsd.libraryelement.*;
+import org.supremica.functionblocks.xsd.libraryelement.impl.*;
 import org.supremica.properties.SupremicaProperties;
 
 
@@ -75,7 +74,7 @@ public class Loader
     private JAXBContext jaxbContext;
     private Unmarshaller u;
     
-    // variabel för att hålla reda på  tabuleringen, bara för utskriften av XML-koden tills vidare
+    // Variable to keep track of the indentation, just for now for printing the XML code
     protected int nbrOfBlanks;
     protected String blanks;
 
@@ -83,19 +82,18 @@ public class Loader
     {
 	try
 	    {
-		System.err.println("Entered the Loader constructor.");
-		//		jaxbContext = JAXBContext.newInstance("org.supremica.manufacturingTables.xsd.factory", this.getClass().getClassLoader());
+		java.lang.System.err.println("Entered the Loader constructor.");
 		jaxbContext = JAXBContext.newInstance("org.supremica.manufacturingTables.xsd.factory");
-		System.err.println("jaxbcontext skapat");
+		java.lang.System.err.println("jaxbcontext skapat");
 		u = jaxbContext.createUnmarshaller();
-		System.err.println("unmarshaller skapat");
+		java.lang.System.err.println("unmarshaller skapat");
 		// enable validation
 		u.setValidating( true );
-		System.err.println("validation satt till true");
+		java.lang.System.err.println("validation satt till true");
 
 		// We will allow the Unmarshaller's default
 		// ValidationEventHandler to receive notification of warnings
-		// and errors which will be sent to System.out.  The default
+		// and errors which will be sent to java.lang.System.err.  The default
 		// ValidationEventHandler will cause the unmarshal operation
 		// to fail with an UnmarshalException after encountering the
 		// first error or fatal error.
@@ -117,24 +115,24 @@ public class Loader
 		    {
 			// Unmarshall from the file with the fileName
 			Object o = u.unmarshal(theFile);
-			System.err.println("filen unmarshallad");
+			java.lang.System.err.println("filen unmarshallad");
 
-			//return (FactoryType) o;
+			// return (FactoryType) o;
 			buildPLCProgram((FactoryType) o);
 		    }
 		else
 		    {
-			System.err.println("Problems reading the file!");
+			java.lang.System.err.println("Problems reading the file!");
 		    }
 	    }
 	catch(UnmarshalException ue)
 	    {
-		System.out.println("Invalid XML code (UnmarshalException)" );
+		java.lang.System.err.println("Invalid XML code (UnmarshalException)" );
 		ue.printStackTrace();
 	    }
 	catch(JAXBException je)
 	    {
-		System.err.println("JAXBException caucht!");
+		java.lang.System.err.println("JAXBException caught!");
 		je.printStackTrace();
 	    }
 	return;
@@ -142,19 +140,19 @@ public class Loader
 
     private void buildPLCProgram(FactoryType factory)
     {
-	// variabel för att hålla reda på  tabuleringen, bara för utskriften av XML-koden
+	// Variable to keep track of the indentation, just for now for printing the XML code
 	nbrOfBlanks = 0;
 	blanks = "                                                                              ";
 	// Factory
-	System.err.println(blanks.substring(0,nbrOfBlanks) + "<Factory name=\"" + factory.getName() + "\">");
+	java.lang.System.err.println(blanks.substring(0,nbrOfBlanks) + "<Factory name=\"" + factory.getName() + "\">");
 	if(factory.getDescription()!=null)
 	    {
-		System.err.println(blanks.substring(0,nbrOfBlanks) + "<Description>" + factory.getDescription() + "</Description>");
+		java.lang.System.err.println(blanks.substring(0,nbrOfBlanks) + "<Description>" + factory.getDescription() + "</Description>");
 	    }
 	// Areas
 	nbrOfBlanks++;
 	AreasType areas = factory.getAreas();
-	System.err.println(blanks.substring(0,nbrOfBlanks) + "<Areas>");
+	java.lang.System.err.println(blanks.substring(0,nbrOfBlanks) + "<Areas>");
 
 	// Nbr of Areas
 	List areaList = areas.getArea();
@@ -162,15 +160,15 @@ public class Loader
 	for (Iterator areaIter = areaList.iterator();areaIter.hasNext();)
 	    {
 		AreaType currentArea = (AreaType) areaIter.next();
-		System.err.println(blanks.substring(0,nbrOfBlanks) + "<Area name=\"" + currentArea.getName() + "\">");
+		java.lang.System.err.println(blanks.substring(0,nbrOfBlanks) + "<Area name=\"" + currentArea.getName() + "\">");
 		if(currentArea.getDescription()!=null)
 		    {
-			System.err.println(blanks.substring(0,nbrOfBlanks) + "<Description>" + currentArea.getDescription() + "</Description>");
+			java.lang.System.err.println(blanks.substring(0,nbrOfBlanks) + "<Description>" + currentArea.getDescription() + "</Description>");
 		    }
 		// Cells
 		nbrOfBlanks++;
 		CellsType cells = currentArea.getCells();
-		System.err.println(blanks.substring(0,nbrOfBlanks) + "<Cells>");
+		java.lang.System.err.println(blanks.substring(0,nbrOfBlanks) + "<Cells>");
 
 		// Nbr of Cells
 		List cellList = cells.getCell();
@@ -178,13 +176,13 @@ public class Loader
 		for (Iterator cellIter = cellList.iterator();cellIter.hasNext();)
 		    {
 			CellType currentCell = (CellType) cellIter.next();
-			System.err.println(blanks.substring(0,nbrOfBlanks) + "<Cell name=\"" + currentCell.getName() + "\">");
+			java.lang.System.err.println(blanks.substring(0,nbrOfBlanks) + "<Cell name=\"" + currentCell.getName() + "\">");
 			nbrOfBlanks++;
 			
 			// Description
 			if(currentCell.getDescription()!=null)
 			    {
-				System.err.println(blanks.substring(0,nbrOfBlanks) + "<Description>" + currentCell.getDescription() + "</Description>");
+				java.lang.System.err.println(blanks.substring(0,nbrOfBlanks) + "<Description>" + currentCell.getDescription() + "</Description>");
 			    }
 
 			// Equipment
@@ -197,23 +195,23 @@ public class Loader
 			buildMachines(currentCell.getMachines());
 			
 			nbrOfBlanks--;
-			System.err.println(blanks.substring(0,nbrOfBlanks) + "</Cell>");
+			java.lang.System.err.println(blanks.substring(0,nbrOfBlanks) + "</Cell>");
 		    }
 		nbrOfBlanks--;
-		System.err.println(blanks.substring(0,nbrOfBlanks) + "</Cells>");
+		java.lang.System.err.println(blanks.substring(0,nbrOfBlanks) + "</Cells>");
 		nbrOfBlanks--;
-		System.err.println(blanks.substring(0,nbrOfBlanks) + "</Area>");
+		java.lang.System.err.println(blanks.substring(0,nbrOfBlanks) + "</Area>");
 		
 	    }
 	nbrOfBlanks--;
-	System.err.println(blanks.substring(0,nbrOfBlanks) + "</Areas>");
+	java.lang.System.err.println(blanks.substring(0,nbrOfBlanks) + "</Areas>");
 	nbrOfBlanks--;
-	System.err.println(blanks.substring(0,nbrOfBlanks) + "</Factory>");
+	java.lang.System.err.println(blanks.substring(0,nbrOfBlanks) + "</Factory>");
     }
     
     private void buildMachines(MachinesType machines)
     {
-	System.err.println(blanks.substring(0,nbrOfBlanks) + "<Machines>");
+	java.lang.System.err.println(blanks.substring(0,nbrOfBlanks) + "<Machines>");
 
 	// Nbr of Machines
 	List machineList = machines.getMachine();
@@ -223,39 +221,40 @@ public class Loader
 		buildMachine((MachineType) machineIter.next());
 	    }
 	nbrOfBlanks--;
-	System.err.println(blanks.substring(0,nbrOfBlanks) + "</Machines>");
+	java.lang.System.err.println(blanks.substring(0,nbrOfBlanks) + "</Machines>");
     }
 
     private void buildMachine(MachineType machine)
     {
-	System.err.println(blanks.substring(0,nbrOfBlanks) + "<Machine machineType=\"" + machine.getMachineType() + "\" " + "name=\"" + machine.getName() + "\">");
+	java.lang.System.err.println(blanks.substring(0,nbrOfBlanks) + "<Machine machineType=\"" + machine.getMachineType() + "\" " + "name=\"" + machine.getName() + "\">");
 	nbrOfBlanks++;
 
 	// Description
 	if (machine.getDescription()!=null)
 	    {
-		System.err.println(blanks.substring(0,nbrOfBlanks) + "<Description>" + machine.getDescription() + "</Description>");
+		java.lang.System.err.println(blanks.substring(0,nbrOfBlanks) + "<Description>" + machine.getDescription() + "</Description>");
 	    }
 	// Variables
 	if (machine.getVariables()!=null)
 	    {
 		VariablesType variables = machine.getVariables();
-		System.err.println(blanks.substring(0,nbrOfBlanks) + "<Variables>");
+		java.lang.System.err.println(blanks.substring(0,nbrOfBlanks) + "<Variables>");
 		// Nbr of Variables
 		List variableList = variables.getVariable();
 		nbrOfBlanks++;
 		for (Iterator variableIter = variableList.iterator();variableIter.hasNext();)
 		    {
-			// Eftersom det finns en variable-klass borde detta simpletype vara en Variable, men det är en String
-			// I version 2.0 av JAXB kommer man ha möjlighet att sätta mapSimpleTypeDef="true" som förhoppningsvis 
-			// råder bot på detta
+			// Since there is a class Variable this XML simpletype should be represented by a Variable object,
+			// but it is a String.
+			// In JABX version 2.0 there will be an opportunity to set mapSimpleTypeDef="true" and that will 
+			// hopefully sort this out
 			//Variable currentVariable = (Variable) variableIter.next();
 			String currentVariable = (String) variableIter.next();
-			//System.err.println(blanks.substring(0,nbrOfBlanks) + "<Variable>" + currentVariable.getValue() + "</Variable>");
-			System.err.println(blanks.substring(0,nbrOfBlanks) + "<Variable>" + currentVariable + "</Variable>");
+			//java.lang.System.err.println(blanks.substring(0,nbrOfBlanks) + "<Variable>" + currentVariable.getValue() + "</Variable>");
+			java.lang.System.err.println(blanks.substring(0,nbrOfBlanks) + "<Variable>" + currentVariable + "</Variable>");
 		    }
 		nbrOfBlanks--;
-		System.err.println(blanks.substring(0,nbrOfBlanks) + "</Variables>");
+		java.lang.System.err.println(blanks.substring(0,nbrOfBlanks) + "</Variables>");
 	    }
 	
 	// Equipment
@@ -265,14 +264,14 @@ public class Loader
 	    }
 
 	nbrOfBlanks--;
-	System.err.println(blanks.substring(0,nbrOfBlanks) + "</Machine>");
+	java.lang.System.err.println(blanks.substring(0,nbrOfBlanks) + "</Machine>");
     }
 
 
 
     private void buildEquipment(EquipmentType equip)
     {
-	System.err.println(blanks.substring(0,nbrOfBlanks) + "<Equipment>");
+	java.lang.System.err.println(blanks.substring(0,nbrOfBlanks) + "<Equipment>");
 
 	// Nbr of EquipmentEntities
 	List equipList = equip.getEquipmentEntity();
@@ -283,37 +282,38 @@ public class Loader
 		buildEquipmentEntity(currentEquip);
 	    }
 	nbrOfBlanks--;
-	System.err.println(blanks.substring(0,nbrOfBlanks) + "</Equipment>");
+	java.lang.System.err.println(blanks.substring(0,nbrOfBlanks) + "</Equipment>");
     }
 
     private void buildEquipmentEntity(EquipmentEntityType equipEnt)
     {
-	System.err.println(blanks.substring(0,nbrOfBlanks) + "<EquipmentEntity equipmentType=\"" + equipEnt.getEquipmentType() + "\" " + "name=\"" + equipEnt.getName() + "\">");
+	java.lang.System.err.println(blanks.substring(0,nbrOfBlanks) + "<EquipmentEntity equipmentType=\"" + equipEnt.getEquipmentType() + "\" " + "name=\"" + equipEnt.getName() + "\">");
 	nbrOfBlanks++;
 
 	// Description
 	if (equipEnt.getDescription()!=null)
 	    {
-		System.err.println(blanks.substring(0,nbrOfBlanks) + "<Description>" + equipEnt.getDescription() + "</Description>");
+		java.lang.System.err.println(blanks.substring(0,nbrOfBlanks) + "<Description>" + equipEnt.getDescription() + "</Description>");
 	    }
 	// States
 	StatesType states = equipEnt.getStates();
-	System.err.println(blanks.substring(0,nbrOfBlanks) + "<States>");
+	java.lang.System.err.println(blanks.substring(0,nbrOfBlanks) + "<States>");
 	// Nbr of States
 	List stateList = states.getState();
 	nbrOfBlanks++;
 	for (Iterator stateIter = stateList.iterator();stateIter.hasNext();)
 	    {
-		// Eftersom det finns en state-klass borde detta simpletype vara ett State, men det är en String
-		// I version 2.0 av JAXB kommer man ha möjlighet att sätta mapSimpleTypeDef="true" som förhoppningsvis 
-		// råder bot på detta
+		// Since there is a class State this XML simpletype should be represented by a State object,
+		// but it is a String.
+		// In JABX version 2.0 there will be an opportunity to set mapSimpleTypeDef="true" and that will 
+		// hopefully sort this out
 		//State currentState = (State) stateIter.next();
 		String currentState = (String) stateIter.next();
-		//System.err.println(blanks.substring(0,nbrOfBlanks) + "<State>" + currentState.getValue() + "</State>");
-		System.err.println(blanks.substring(0,nbrOfBlanks) + "<State>" + currentState + "</State>");
+		//java.lang.System.err.println(blanks.substring(0,nbrOfBlanks) + "<State>" + currentState.getValue() + "</State>");
+		java.lang.System.err.println(blanks.substring(0,nbrOfBlanks) + "<State>" + currentState + "</State>");
 	    }
 	nbrOfBlanks--;
-	System.err.println(blanks.substring(0,nbrOfBlanks) + "</States>");
+	java.lang.System.err.println(blanks.substring(0,nbrOfBlanks) + "</States>");
 
 	// Elements
 	if (equipEnt.getElements()!=null)
@@ -328,12 +328,85 @@ public class Loader
 	    }
 
 	nbrOfBlanks--;
-	System.err.println(blanks.substring(0,nbrOfBlanks) + "</EquipmentEntity>");
+	java.lang.System.err.println(blanks.substring(0,nbrOfBlanks) + "</EquipmentEntity>");
+
+	//Create an ObjectFactory to get instances of the interfaces
+	org.supremica.functionblocks.xsd.libraryelement.ObjectFactory objFactory = new  org.supremica.functionblocks.xsd.libraryelement.ObjectFactory();
+	// Create the corresponding FB
+	try
+	    {
+		FBTypeType FB = objFactory.createFBType();
+		FB.setName(equipEnt.getEquipmentType() + " " + equipEnt.getName());
+		// Check if it is a sensor EquipmentEntity
+		if (equipEnt.getEquipmentType().compareTo("Sensor") == 0)
+		    {
+			java.lang.System.err.println("Detta är en sensor!");
+			InterfaceListType interfaceList = new InterfaceListImpl();
+			EventInputsType eventInputs = new EventInputsImpl();
+			EventType eventREQ = new EventImpl();
+			eventREQ.setName("REQ");
+			// there is no setEvent method in EventInputs but the getEvent method returns the actual list
+			eventInputs.getEvent().add(eventREQ);
+			interfaceList.setEventInputs(eventInputs);
+			FB.setInterfaceList(interfaceList);		   
+			
+			// The FB needs to have versionInfo
+			VersionInfoType versionInfo = new VersionInfoImpl();
+			versionInfo.setOrganization("Chalmers");
+			versionInfo.setVersion("0.1");
+			versionInfo.setAuthor("Oscar Ljungkrantz");
+			versionInfo.setDate((new java.util.Date()).toLocaleString());
+			// there is no setVersionInfo method in FBType but the getVersionInfo method returns the actual list
+			FB.getVersionInfo().add(versionInfo);
+			// Create an xml-file of the FB
+			createFBXMLFile((FBType) FB);
+		    }
+	    }
+	catch (JAXBException je)
+	    {
+		je.printStackTrace();
+	    }
+
+    }
+
+    private void createFBXMLFile(Object o)
+    {
+	try
+	    {
+	JAXBContext jaxbContext = JAXBContext.newInstance("org.supremica.functionblocks.xsd.libraryelement");
+
+	Marshaller marshaller = jaxbContext.createMarshaller();
+
+	//You can tell the Marshaller to format the resulting XML data with line breaks and indentation. The following statement turns this output format property on -- line breaks and indentation will appear in the output format: 
+	marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT,new Boolean(true));
+ 
+	marshaller.marshal(o,new FileOutputStream("jaxbOutput.xml"));
+
+	//Validation is not performed as part of the marshalling operation. In other words, unlike the case for unmarshalling, there is no setValidating method for marshalling. Instead, when marshalling data, you use the Validator class that is a part of the binding framework to validate a content tree against a schema. For example: 
+
+	Validator validator = jaxbContext.createValidator();
+	validator.validate(o);
+	    }
+	catch(PropertyException pe)
+	    {
+		java.lang.System.err.println("Couldn´t set the desired marshal properties!");
+		pe.printStackTrace();
+	    }
+	catch(JAXBException je)
+	    {
+		java.lang.System.err.println("JAXBException caught!");
+		je.printStackTrace();
+	    }
+	catch(FileNotFoundException fe)
+	    {
+		java.lang.System.err.println("Couldn´t create the file!");
+		fe.printStackTrace();
+	    }
     }
 
     private void buildElements(ElementsType elements)
     {
-	System.err.println(blanks.substring(0,nbrOfBlanks) + "<Elements>");
+	java.lang.System.err.println(blanks.substring(0,nbrOfBlanks) + "<Elements>");
 
 	// Nbr of Elements
 	List elementList = elements.getElement();
@@ -344,22 +417,22 @@ public class Loader
 		buildElement(currentElement);
 	    }
 	nbrOfBlanks--;
-	System.err.println(blanks.substring(0,nbrOfBlanks) + "</Elements>");
+	java.lang.System.err.println(blanks.substring(0,nbrOfBlanks) + "</Elements>");
     }
 
     private void buildElement(ElementType element)
     {
-	System.err.println(blanks.substring(0,nbrOfBlanks) + "<Element elementType=\"" + element.getElementType() + "\" " + "name=\"" + element.getName() + "\">");
+	java.lang.System.err.println(blanks.substring(0,nbrOfBlanks) + "<Element elementType=\"" + element.getElementType() + "\" " + "name=\"" + element.getName() + "\">");
 	nbrOfBlanks++;
 
 	// Description
 	if (element.getDescription()!=null)
 	    {
-		System.err.println(blanks.substring(0,nbrOfBlanks) + "<Description>" + element.getDescription() + "</Description>");
+		java.lang.System.err.println(blanks.substring(0,nbrOfBlanks) + "<Description>" + element.getDescription() + "</Description>");
 	    }
 
 	nbrOfBlanks--;
-	System.err.println(blanks.substring(0,nbrOfBlanks) + "</Element>");
+	java.lang.System.err.println(blanks.substring(0,nbrOfBlanks) + "</Element>");
     }
 
     private File getFile(String path, String fileName)
@@ -374,7 +447,7 @@ public class Loader
 		    }
 		else
 		    {
-			System.err.println("The specified directory is invalid!");
+			java.lang.System.err.println("The specified directory is invalid!");
 			return null;
 		    }
 	    }
@@ -384,7 +457,7 @@ public class Loader
 	    }
 	else
 	    {
-		System.err.println("The file does not exist in the specified directory!");
+		java.lang.System.err.println("The file does not exist in the specified directory!");
 		return null;
 	    }
     }
