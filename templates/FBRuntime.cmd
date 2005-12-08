489a9c12efa22c160d9e68eeb0f75a87d746af04
Goran Cengic <cengic@gmail.com>
2005-12-08 14:05:04 +0000

    *** empty log message ***

--- templates/FBRuntime.cmd
+++ templates/FBRuntime.cmd
@@ -1,1 +1,1 @@
-@java -cp Supremica.jar;SupremicaLib.jar -enableassertions org.supremica.apps.FBRuntime  %1 %2 %3 %4 %5 %6 %7 %8 %9
+@java -cp Supremica.jar;SupremicaLib.jar -enableassertions org.supremica.functionblocks.model.FBRuntime  %1 %2 %3 %4 %5 %6 %7 %8 %9
