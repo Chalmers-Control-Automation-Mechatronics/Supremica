5dce0612f1dd22205ee35daf4fc0a03edf9fec02
Robi Malik <robi@cs.waikato.ac.nz>
2006-01-16 02:23:01 +0000

    ADD   New classes for command restructuring (forgotten to commit by Simon).

---
+++ src/net/sourceforge/waters/gui/command/CompoundCommand.java
@@ -0,0 +1,61 @@
+package net.sourceforge.waters.gui.command;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompoundCommand
+	implements Command
+{
+	private boolean mInProgress;
+	private List<Command> mCommands;
+	private String mDescription;
+	
+	public CompoundCommand()
+	{
+		mInProgress = true;
+		mCommands = new ArrayList();
+		mDescription = "Compound Command";
+	}
+	
+	public CompoundCommand(String name)
+	{
+		this();
+		mDescription = name;
+	}
+	
+	public boolean addCommand(Command c)
+	{
+		if (mInProgress)
+		{
+			mCommands.add(c);
+			return true;
+		}
+		return false;
+	}
+	
+	public void end()
+	{
+		mInProgress = false;
+	}
+	
+	public void execute()
+	{
+		for (Command c : mCommands)
+		{
+			c.execute();
+		}
+	}
+	
+	public void undo()
+	{
+		for (Command c : mCommands)
+		{
+			c.undo();
+		}
+	}
+	
+	public String getName()
+	{
+		return mDescription;
+	}
+}
---
+++ src/net/sourceforge/waters/gui/command/UndoableCommand.java
@@ -0,0 +1,45 @@
+package net.sourceforge.waters.gui.command;
+
+import javax.swing.undo.AbstractUndoableEdit;
+import javax.swing.undo.CannotRedoException;
+import javax.swing.undo.CannotUndoException;
+
+public class UndoableCommand
+	extends AbstractUndoableEdit
+{
+	private final Command mCommand;
+	
+	public UndoableCommand(Command command)
+	{
+		mCommand = command;
+	}
+	
+	/** 
+     * Redoes the Command
+     *
+     * @throws CannotRedoException if CanRedo returns false
+     */
+    
+    public void redo() throws CannotRedoException
+    {
+		super.redo();
+		mCommand.execute();
+    }
+
+    /** 
+     * Undoes the Command
+     *
+     * @throws CannotUndoException if CanUndo returns false
+     */    
+
+    public void undo() throws CannotUndoException
+    {
+		super.undo();
+		mCommand.undo();
+    }
+
+    public String getPresentationName()
+    {
+		return mCommand.getName();
+    }
+}
