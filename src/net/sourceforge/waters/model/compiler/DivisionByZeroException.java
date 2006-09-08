1bdfa2a627dfcc8ad1e66be9c3e586babb0a4b54
Robi Malik <robi@cs.waikato.ac.nz>
2006-09-08 07:19:58 +0000

    ADD   New exception to report division by zero when evaluating expressions   with new / and % operators.

---
+++ src/net/sourceforge/waters/model/compiler/DivisionByZeroException.java
@@ -0,0 +1,37 @@
+//# -*- indent-tabs-mode: nil  c-basic-offset: 2 -*-
+//###########################################################################
+//# PROJECT: Waters
+//# PACKAGE: net.sourceforge.waters.model.compiler
+//# CLASS:   DivisionByZeroException
+//###########################################################################
+//# $Id: DivisionByZeroException.java,v 1.1 2006-09-08 07:19:58 robi Exp $
+//###########################################################################
+
+package net.sourceforge.waters.model.compiler;
+
+import net.sourceforge.waters.model.base.Proxy;
+import net.sourceforge.waters.model.expr.EvalException;
+
+
+public class DivisionByZeroException extends EvalException {
+
+  //#########################################################################
+  //# Constructors
+  /**
+   * Constructs a new exception with default detail message.
+   */
+  public DivisionByZeroException()
+  {
+    super("Division by zero in expression!");
+  }
+
+  /**
+   * Constructs a new exception with default detail message.
+   * @param  expr     The subterm that causes the error.
+   */
+  public DivisionByZeroException(final Proxy expr)
+  {
+    super("Division by zero in expression!", expr);
+  }
+
+}
