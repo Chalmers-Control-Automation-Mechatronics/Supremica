dfef45c3d45319a16fbb2ce7a8779acadc601108
Knut Akesson <knut@chalmers.se>
2006-09-24 13:48:10 +0000

    *** empty log message ***

--- src/org/supremica/gui/ide/actions/AnalyzerStatisticsAction.java
+++ src/org/supremica/gui/ide/actions/AnalyzerStatisticsAction.java
@@ -6,6 +6,9 @@ import javax.swing.ImageIcon;
 import javax.swing.KeyStroke;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+import org.supremica.automata.Alphabet;
+import org.supremica.automata.Automata;
+import org.supremica.automata.Automaton;
 import org.supremica.gui.ide.ModuleContainer;
 import org.supremica.gui.ide.IDE;
 
@@ -44,5 +47,64 @@ public class AnalyzerStatisticsAction
      */
     public void doAction()
     {
+        int nbrOfAutomata = ide.getActiveModuleContainer().getVisualProject().nbrOfAutomata();
+        //gui.info("Number of automata: " + nbrOfAutomata);
+
+        Automata selectedAutomata = ide.getSelectedAutomata();
+        if (!selectedAutomata.sanityCheck(ide.getIDE(), 1, false, false, true, true))
+        {
+            return;
+        }
+
+        ide.getIDE().info("Number of selected automata: " + selectedAutomata.size() + " (" + nbrOfAutomata + ")");
+        ide.getIDE().info("Size of union alphabet: " + selectedAutomata.getUnionAlphabet().size());
+
+        for (Automaton currAutomaton : selectedAutomata)
+        {
+            StringBuffer statusStr = new StringBuffer();
+
+            statusStr.append("Status for automaton: " + currAutomaton.getName());
+
+            statusStr.append("\n\tnumber of states: " + currAutomaton.nbrOfStates());
+            statusStr.append("\n\tnumber of events: " + currAutomaton.nbrOfEvents());
+            statusStr.append("\n\tnumber of transitions: " + currAutomaton.nbrOfTransitions());
+            statusStr.append("\n\tnumber of accepting states: " + currAutomaton.nbrOfAcceptingStates());
+            //statusStr.append("\n\tNumber of mutually accepting states: " + currAutomaton.nbrOfMutuallyAcceptingStates());
+            statusStr.append("\n\tnumber of forbidden states: " + currAutomaton.nbrOfForbiddenStates());
+
+            int acceptingAndForbiddenStates = currAutomaton.nbrOfAcceptingAndForbiddenStates();
+            if (acceptingAndForbiddenStates > 0)
+            {
+                statusStr.append("\n\tnumber of accepting AND forbidden states: " + acceptingAndForbiddenStates);
+            }
+
+            if (currAutomaton.isDeterministic())
+            {
+                Alphabet redundantEvents = currAutomaton.getRedundantEvents();
+                if (redundantEvents.nbrOfEvents() > 0)
+                    statusStr.append("\n\talphabet of redundant events: " + redundantEvents);
+                statusStr.append("\n\tthe automaton is deterministic");
+            }
+
+            if ((currAutomaton.getComment() != null) && !currAutomaton.getComment().equals(""))
+            {
+                statusStr.append("\n\tcomment: \"" + currAutomaton.getComment() + "\"");
+            }
+
+            // logger.info(statusStr.toString());
+            ide.getIDE().info(statusStr.toString());
+        }
+
+        if (selectedAutomata.size() > 1)
+        {
+            double potentialNumberOfStates = 1.0;
+
+            for (Automaton currAutomaton : selectedAutomata)
+            {
+                potentialNumberOfStates = potentialNumberOfStates * currAutomaton.nbrOfStates();
+            }
+
+            ide.getIDE().info("Number of potential states: " + new Double(potentialNumberOfStates).longValue());
+        }
     }
 }
