ae0ec497d2413a4819fa0d847ef625a426eb811c
Knut Akesson <knut@chalmers.se>
2006-10-04 12:30:23 +0000

    *** empty log message ***

--- src/net/sourceforge/waters/gui/EditorEditVariableDialog.java
+++ src/net/sourceforge/waters/gui/EditorEditVariableDialog.java
@@ -43,23 +43,23 @@ implements ActionListener{
 	private ModuleSubjectFactory mFactory;
 	private SimpleComponentSubject mComponent;
 	private ModuleTree mTree;
-	
+
 	public EditorEditVariableDialog(VariableSubject variable, SimpleComponentSubject component,
 			ModuleTree tree) {
 		setModal(true);
 		setLocationRelativeTo(null);
-		
+
 		mVariable = variable;
 		mComponent = component;
 		mTree = tree;
-		
+
 		mFactory = ModuleSubjectFactory.getInstance();
 		GridBagConstraints con = new GridBagConstraints();
-		
+
 		//setup layout manager
 		layout = new GridBagLayout();
 		this.setLayout(layout);
-		
+
 		lName = new JLabel("Name:");
 		con.insets = new Insets(5,5,5,5);
 		con.gridx = 1;
@@ -68,7 +68,7 @@ implements ActionListener{
 		con.weighty = 1;
 		layout.setConstraints(lName, con);
 		add(lName);
-		
+
 		name = new JTextField();
 		name.setEditable(true);
 		name.setEnabled(true);
@@ -78,29 +78,29 @@ implements ActionListener{
 		con.weightx = 1;
 		layout.setConstraints(name, con);
 		add(name);
-		
+
 		lType = new JLabel("Type:");
 		con.gridx = 1;
 		con.gridy = 2;
 		con.weightx = 0;
 		layout.setConstraints(lType, con);
 		add(lType);
-		
-		String[] types = {"integer", "boolean"};
+
+		String[] types = {"boolean", "integer"};
 		typeSelector = new JComboBox(types);
 		con.gridx = 2;
 		con.weightx = 1;
 		layout.setConstraints(typeSelector, con);
 		add(typeSelector);
 		typeSelector.addActionListener(this);
-		
+
 		separator = new JSeparator(JSeparator.HORIZONTAL);
 		con.gridx = 1;
 		con.gridy = 3;
 		con.gridwidth = 2;
 		layout.setConstraints(separator, con);
 		add(separator);
-		
+
 		//setup editor panel for integer variables
 		createIntegerPanel();
 		con.gridx = 1;
@@ -110,12 +110,12 @@ implements ActionListener{
 		layout.setConstraints(integerPanel, con);
 		integerPanel.setEnabled(true);
 		add(integerPanel);
-		
+
 		//setup editor panel for boolean variables
 		createBooleanPanel();
 		layout.setConstraints(booleanPanel, con);
 		add(booleanPanel);
-		
+
 		if(variable == null) {
 			setTitle("New variable");
 			//modify dialog according to variable type
@@ -135,12 +135,12 @@ implements ActionListener{
 			selectTypePanel();
 			name.setText(variable.getName());
 		}
-		
+
 		//Button panel
 		FlowLayout buttonLayout = new FlowLayout(FlowLayout.RIGHT, 5, 5);
 		JPanel buttonPanel = new JPanel();
 		buttonPanel.setLayout(buttonLayout);
-		
+
 		con.gridx = 2;
 		con.gridy = 5;
 		con.weightx = 0;
@@ -150,7 +150,7 @@ implements ActionListener{
 		layout.setConstraints(buttonPanel, con);
 		buttonPanel.setAlignmentX(JPanel.RIGHT_ALIGNMENT);
 		this.add(buttonPanel);
-		
+
 		//ok button
 		okButton = new JButton();
 		okButton.setText("Ok");
@@ -158,9 +158,9 @@ implements ActionListener{
 		okButton.setActionCommand("OK");
 		okButton.addActionListener(this);
 		buttonPanel.add(okButton);
-		
+
 		this.getRootPane().setDefaultButton(okButton);
-		
+
 		//cancel button
 		cancelButton = new JButton();
 		cancelButton.setText("Cancel");
@@ -168,12 +168,12 @@ implements ActionListener{
 		cancelButton.setActionCommand("Cancel");
 		cancelButton.addActionListener(this);
 		buttonPanel.add(cancelButton);
-		
+
 		pack();
 		this.setResizable(false);
 		setVisible(true);
 	}
-	
+
 	private void selectTypePanel() {
 		//modify dialog according to variable type
 		if(typeSelector.getSelectedItem().equals("integer")) {
@@ -185,25 +185,25 @@ implements ActionListener{
 		}
 		pack();
 	}
-	
+
 	private void createBooleanPanel() {
 		booleanPanel = new JPanel();
 		GridBagLayout layout = new GridBagLayout();
 		booleanPanel.setLayout(layout);
 		GridBagConstraints con = new GridBagConstraints();
-		
+
 		JLabel lInitial, lMarked;
-		
+
 		lInitial = new JLabel("Initial value:");
 		lMarked = new JLabel("Marked value:");
-		
+
 		con.insets = new Insets(3,3,3,3);
 		con.gridx = 1;
 		con.gridy = 1;
 		con.anchor = GridBagConstraints.WEST;
 		layout.setConstraints(lInitial, con);
 		booleanPanel.add(lInitial);
-		
+
 		con.gridy = 2;
 		layout.setConstraints(lMarked, con);
 		booleanPanel.add(lMarked);
@@ -217,12 +217,12 @@ implements ActionListener{
 		booleanInitial = new JComboBox(booleanValues);
 		layout.setConstraints(booleanInitial, con);
 		booleanPanel.add(booleanInitial);
-		
+
 		con.gridy = 2;
 		booleanMarked = new JComboBox(booleanValuesExt);
 		layout.setConstraints(booleanMarked, con);
 		booleanPanel.add(booleanMarked);
-		
+
 		if(mVariable != null && mVariable.getType() instanceof SimpleIdentifierProxy) {
 			//set initial value
 			if(((BooleanConstantSubject) mVariable.getInitialValue()).isValue()) {
@@ -244,34 +244,34 @@ implements ActionListener{
 			booleanInitial.setSelectedItem(Boolean.toString(DEFAULTBOOLEANINITIAL));
 		}
 	}
-	
+
 	private void createIntegerPanel() {
 		integerPanel = new JPanel();
 		GridBagLayout layout = new GridBagLayout();
 		integerPanel.setLayout(layout);
 		GridBagConstraints con = new GridBagConstraints();
-		
+
 		JLabel lRange, lInitial, lMarked;
-		
+
 		lRange = new JLabel("Range:");
 		lInitial = new JLabel("Initial value:");
 		lMarked = new JLabel("Marked value:");
-		
+
 		con.insets = new Insets(3,3,3,3);
 		con.anchor = GridBagConstraints.WEST;
 		con.gridx = 1;
 		con.gridy = 1;
 		layout.setConstraints(lRange, con);
 		integerPanel.add(lRange);
-		
+
 		con.gridy = 2;
 		layout.setConstraints(lInitial, con);
 		integerPanel.add(lInitial);
-		
+
 		con.gridy = 3;
 		layout.setConstraints(lMarked, con);
 		integerPanel.add(lMarked);
-		
+
 		con.anchor = GridBagConstraints.CENTER;
 		con.gridx = 2;
 		con.gridy = 1;
@@ -282,33 +282,33 @@ implements ActionListener{
 		rangeLower.setEnabled(true);
 		layout.setConstraints(rangeLower, con);
 		integerPanel.add(rangeLower);
-		
+
 		con.gridx = 4;
 		rangeUpper = new JTextField();
 		rangeUpper.setEnabled(true);
 		rangeUpper.setEditable(true);
 		layout.setConstraints(rangeUpper, con);
 		integerPanel.add(rangeUpper);
-		
+
 		con.fill = GridBagConstraints.NONE;
 		con.gridx = 3;
 		con.weightx = 0;
 		JLabel to = new JLabel("-");
 		layout.setConstraints(to, con);
 		integerPanel.add(to);
-		
+
 		con.gridy = 2;
 		con.gridx = 2;
 		con.fill = GridBagConstraints.HORIZONTAL;
 		integerInitial = new JTextField();
 		layout.setConstraints(integerInitial, con);
 		integerPanel.add(integerInitial);
-		
+
 		con.gridy = 3;
 		integerMarked = new JTextField();
 		layout.setConstraints(integerMarked, con);
 		integerPanel.add(integerMarked);
-		
+
 		if(mVariable != null && mVariable.getType() instanceof BinaryExpressionProxy) {
 			//set range values
 			BinaryExpressionProxy range = (BinaryExpressionProxy) mVariable.getType();
@@ -316,37 +316,46 @@ implements ActionListener{
 					((IntConstantProxy) range.getLeft()).getValue()));
 			rangeUpper.setText(Integer.toString(
 					((IntConstantProxy) range.getRight()).getValue()));
-			
+
 			//set initial value
 			integerInitial.setText(Integer.toString(
 					((IntConstantProxy) mVariable.getInitialValue()).getValue()));
-			
+
 			//set marked value
 			if(mVariable.getMarkedValue() != null) {
 				integerMarked.setText(Integer.toString(
 						((IntConstantProxy) mVariable.getMarkedValue()).getValue()));
-				
+
 			}
 		} else {
 			//enter default values
 			//set range values
 			rangeLower.setText(Integer.toString(DEFAULTLOWERINT));
 			rangeUpper.setText(Integer.toString(DEFAULTUPPERINT));
-			
+
 			//set initial value
 			integerInitial.setText(Integer.toString(DEFAULTINITIALINT));
-			
+
 		}
 	}
-	
+
 	public static void showDialog(VariableSubject variable, SimpleComponentSubject component,
 			ModuleTree tree) {
 		new EditorEditVariableDialog(variable, component, tree);
 	}
-	
+
 	public void actionPerformed(ActionEvent e) {
 		if(e.getActionCommand().equals("OK")) {
 			//do some validity checking
+			if (name == null || name.getText() == null || name.getText().trim().equals(""))
+			{
+				JOptionPane.showMessageDialog(this,
+						"Variable name missing",
+						"Input error",
+						JOptionPane.ERROR_MESSAGE);
+				return;
+			}
+
 			int intLower=0, intUpper=0, intInitial=0, intMarked=0;
 			if(typeSelector.getSelectedItem().equals("integer")) {
 				try {
@@ -403,7 +412,7 @@ implements ActionListener{
 					mFactory.createIntConstantProxy(intLower),
 					mFactory.createIntConstantProxy(intUpper))
 					: mFactory.createSimpleIdentifierProxy("boolean");
-					
+
 		    SimpleExpressionSubject initial, marked;
 		    if(type instanceof BinaryExpressionProxy) {
 		    	initial = mFactory.createIntConstantProxy(intInitial);
@@ -423,9 +432,22 @@ implements ActionListener{
 		    	System.err.println("EditorEditVariableDialog: Unsupported variable type");
 		    	return;
 		    }
-		    
-		    if(mVariable == null) {	
+
+		    if(mVariable == null) {
 		    	//create new variable
+		    	// Check that a variable with the given name does not already exists
+		    	for (VariableSubject currVariable : mComponent.getVariablesModifiable())
+		    	{
+					if (currVariable.getName().equalsIgnoreCase(name.getText()))
+					{
+						JOptionPane.showMessageDialog(this,
+								"Variable " + name.getText() + " already exists",
+								"Input error",
+								JOptionPane.ERROR_MESSAGE);
+						return;
+					}
+				}
+
 		    	mVariable = new VariableSubject(name.getText(), type, initial, marked);
 		    	mComponent.getVariablesModifiable().add(mVariable);
 		    	mTree.addVariable(mVariable);
