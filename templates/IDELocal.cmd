e0451d6d262e5733edb30a298647bb270bc64a12
Tord Andersson <unknown@anywhere.com>
2007-02-21 08:33:24 +0000

    *** empty log message ***

--- templates/IDELocal.cmd
+++ templates/IDELocal.cmd
@@ -1,1 +1,1 @@
-@java -cp ..\build;..\lib\unjared\;..\images;..\examples -enableassertions org.supremica.gui.ide.IDE -p SupremicaProperties.cfg %1 %2 %3 %4 %5 %6 %7 %8 %9
+@java -cp ..\build;..\lib\unjared\;..\images;..\examples;..\animations;..\userinterfaces;..\runtime\softplc\build -enableassertions org.supremica.gui.ide.IDE -p SupremicaProperties.cfg %1 %2 %3 %4 %5 %6 %7 %8 %9
