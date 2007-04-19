07b5efb773445756d08b60ec0bbfebad331659cb
Goran Cengic <cengic@gmail.com>
2007-04-19 14:02:09 +0000

    *** empty log message ***

--- src/org/supremica/external/iec61499fb2efa/ModelMaker.java
+++ src/org/supremica/external/iec61499fb2efa/ModelMaker.java
@@ -326,6 +326,11 @@ class ModelMaker
 
 	private void makeInstanceQueue()
 	{
+		ExtendedAutomaton instanceQueue = new ExtendedAutomaton("instanceQueue", automata);
+		
+		// get the maximum number of FB instances in queue at the same time
+		
+		
 		
 	}
 	
@@ -335,20 +340,19 @@ class ModelMaker
 		// load IEC 61499 application
 		loadSystem(systemFileName);
 
+ 		automata = new ExtendedAutomata(theSystem.getName());
 
-		// test automata classes
-		automata = new ExtendedAutomata(theSystem.getName());
-
-		ExtendedAutomaton test = new ExtendedAutomaton("test", automata);
+// 		// test automata classes
+// 		ExtendedAutomaton test = new ExtendedAutomaton("test", automata);
 		
-		test.addState("s0", true);
-		test.addState("s1");
+// 		test.addState("s0", true);
+// 		test.addState("s1");
 
-		test.addIntegerVariable("var1", 0, 5, 0, null);
+// 		test.addIntegerVariable("var1", 0, 5, 0, null);
 
-		test.addTransition("s0","s1","e1;e2;","var1 == 1","var1  = 4;");
+// 		test.addTransition("s0","s1","e1;e2;","var1 == 1","var1  = 4;");
 
-		automata.addAutomaton(test);
+// 		automata.addAutomaton(test);
 
 		// make instance queue model
 		makeInstanceQueue();
