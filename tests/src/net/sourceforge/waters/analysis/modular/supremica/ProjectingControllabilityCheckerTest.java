a81edcf936160bb2479a3e5234fc2eebea04a77a
Robi Malik <robi@cs.waikato.ac.nz>
2007-05-24 20:23:03 +0000

    BUG FIX   Move ProjectingControllabilityCheckerTest to   net.sourceforge.waters.analysis.modular.supremica   and restore original MaxCommonEventsHeuristicTest.

---
+++ tests/src/net/sourceforge/waters/analysis/modular/supremica/ProjectingControllabilityCheckerTest.java
@@ -0,0 +1,55 @@
+//# -*- indent-tabs-mode: nil  c-basic-offset: 2 -*-
+//###########################################################################
+//# PROJECT: Waters
+//# PACKAGE: net.sourceforge.waters.analysis.monolithic
+//# CLASS:   ProjectingControllabilityCheckerTest
+//###########################################################################
+//# $Id: ProjectingControllabilityCheckerTest.java,v 1.1 2007-05-24 20:23:03 robi Exp $
+//###########################################################################
+
+package net.sourceforge.waters.analysis.modular.supremica;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import net.sourceforge.waters.analysis.modular.HeuristicType;
+import net.sourceforge.waters.analysis.modular.MaxCommonEventsHeuristic;
+import net.sourceforge.waters.cpp.analysis.NativeControllabilityChecker;
+import net.sourceforge.waters.model.analysis.
+  AbstractLargeControllabilityCheckerTest;
+import net.sourceforge.waters.model.analysis.
+  AbstractControllabilityCheckerTest;
+import net.sourceforge.waters.model.analysis.ControllabilityChecker;
+import net.sourceforge.waters.model.des.ProductDESProxyFactory;
+
+
+public class ProjectingControllabilityCheckerTest
+  extends AbstractLargeControllabilityCheckerTest
+{
+
+  //#########################################################################
+  //# Entry points in junit.framework.TestCase
+  public static Test suite() {
+    TestSuite testSuite =
+      new TestSuite(ProjectingControllabilityCheckerTest.class);
+    return testSuite;
+  }
+
+  public static void main(final String[] args)
+  {
+    junit.textui.TestRunner.run(suite());
+  }
+
+  //#########################################################################
+  //# Overrides for abstract base class
+  //# net.sourceforge.waters.analysis.AbstractModelVerifierTest
+  protected ControllabilityChecker createModelVerifier
+    (final ProductDESProxyFactory factory)
+  {
+    return
+      new ProjectingControllabilityChecker
+            (null, factory,
+             new NativeControllabilityChecker(factory),
+             new MaxCommonEventsHeuristic(HeuristicType.PREFERREALPLANT), false);
+  }
+}
