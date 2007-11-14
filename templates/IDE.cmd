96f9a5d58c82ebfd7c69fe59385816f8f39fdc24
Avenir Kobetski <kavenir@hotmail.com>
2007-11-14 17:12:13 +0000

    *** empty log message ***

--- templates/IDE.cmd
+++ templates/IDE.cmd
@@ -1,1 +1,7 @@
-@java -cp Supremica.jar -enableassertions org.supremica.gui.ide.IDE -p SupremicaProperties.cfg %1 %2 %3 %4 %5 %6 %7 %8 %9
+@echo off
+
+set supremicaDir = .
+if exist %SUPREMICA_HOME% set supremicaDir=%SUPREMICA_HOME%/dist
+
+@java -cp %supremicaDir%/Supremica.jar -enableassertions org.supremica.gui.ide.IDE -p %supremicaDir%/SupremicaProperties.cfg %1 %2 %3 %4 %5 %6 %7 %8 %9
+
