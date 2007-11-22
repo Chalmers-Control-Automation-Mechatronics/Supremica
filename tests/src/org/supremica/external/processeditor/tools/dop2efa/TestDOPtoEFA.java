db4d668191e255fc7d2066019d55486a33e777cc
Dan Millares <unknown@anywhere.com>
2007-11-22 13:39:52 +0000

    Some basic tests

---
+++ tests/src/org/supremica/external/processeditor/tools/dop2efa/TestDOPtoEFA.java
@@ -0,0 +1,362 @@
+/**
+ * class to test DOP to EFA 
+ */
+package org.supremica.external.processeditor.tools.dop2efa;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.supremica.testhelpers.*;
+import java.util.List;
+import java.util.LinkedList;
+import java.io.File;
+
+import org.supremica.external.processeditor.tools.dop2efa.*;
+import org.supremica.manufacturingTables.xsd.processeditor.*;
+
+public class TestDOPtoEFA extends TestCase{
+	
+	private ObjectFactory factory = new ObjectFactory();
+	private File tmpFile = null;
+    
+    public TestDOPtoEFA(String name){
+        super(name);
+    }
+    
+    /**
+     * Sets up the test fixture.
+     * Called before every test case method.
+     */
+    protected void setUp(){
+    	try{
+    		tmpFile = File.createTempFile("tmp", "tmp");
+    	}catch(Exception e){
+    		e.printStackTrace();
+    	}	
+    }
+    
+    /**
+     * Tears down the test fixture.
+     * Called after every test case method.
+     */
+    protected void tearDown(){
+    	tmpFile.delete();
+    }
+    
+    /**
+     * Assembles and returns a test suite
+     * for all the test methods of this test case.
+     */
+    public static Test suite(){
+        TestSuite suite = new TestSuite(TestDOPtoEFA.class);
+        return suite;
+    }
+    
+    
+    public void testBuildModuleFromFourDifferentNodeTypesROP(){
+    	
+    	Module module = null;
+    	List<ROP> ropList = new LinkedList<ROP>();
+    	String moduleName = "test";
+    	boolean block = false;
+    	
+    	//Add ROPs with four different relation types
+    	ropList.add(buildSequenceROP());
+    	ropList.add(buildAlternativeROP());
+    	ropList.add(buildParallelROP());
+    	ropList.add(buildArbitraryROP());
+    	
+    	try{
+    		module = DOPtoEFA.buildModuleFromROP(ropList, moduleName, block);
+    		module.writeToFile(tmpFile);
+    	}catch(Exception e){
+    		e.printStackTrace();
+    		assertTrue(false);
+    	}
+    }
+    
+    public void testBuildModuleWithROPsContainingPreconditions(){
+    	
+    	Module module = null;
+    	List<ROP> ropList = new LinkedList<ROP>();
+    	String moduleName = "test";
+    	boolean block = false;
+    	
+    	ROP seq = buildSequenceROP();
+    	ROP alt = buildAlternativeROP();
+    	ROP par = buildParallelROP();
+    	ROP arb = buildArbitraryROP();
+    	
+    	Activity a = factory.createActivity();
+    	
+    	a.setOperation("PreCon");
+    	a.setPrecondition(buildPrecondition());
+    	
+    	seq.getRelation().getActivityRelationGroup().add(1,a);
+    	alt.getRelation().getActivityRelationGroup().add(1,a);
+    	par.getRelation().getActivityRelationGroup().add(1,a);
+    	arb.getRelation().getActivityRelationGroup().add(1,a);
+    	
+    	ropList.add(seq);
+    	ropList.add(alt);
+    	ropList.add(par);
+    	ropList.add(arb);
+    	
+    	try{
+    		module = DOPtoEFA.buildModuleFromROP(ropList, moduleName, block);
+    		module.writeToFile(tmpFile);
+    	}catch(Exception e){
+    		e.printStackTrace();
+    		assertTrue(false);
+    	}
+    }
+    
+public void testBuildModuleWithROPsContainingResources(){
+    	
+    	Module module = null;
+    	List<ROP> ropList = new LinkedList<ROP>();
+    	String moduleName = "test";
+    	boolean block = false;
+    	
+    	ROP seq = buildSequenceROP();
+    	ROP alt = buildAlternativeROP();
+    	ROP par = buildParallelROP();
+    	ROP arb = buildArbitraryROP();
+    	
+    	Activity a = factory.createActivity();
+    	
+    	Properties prop = factory.createProperties();
+    	prop.getAttribute().add(buildResourceAttribute("Zon1", true, true));
+    	prop.getAttribute().add(buildResourceAttribute("Zon2", false, true));
+    	prop.getAttribute().add(buildResourceAttribute("Zon3", true, false));
+    	
+    	a.setOperation("ResourceBocking");
+    	a.setProperties(prop);
+    	
+    	seq.getRelation().getActivityRelationGroup().add(1,a);
+    	alt.getRelation().getActivityRelationGroup().add(1,a);
+    	par.getRelation().getActivityRelationGroup().add(1,a);
+    	arb.getRelation().getActivityRelationGroup().add(1,a);
+    	
+    	ropList.add(seq);
+    	ropList.add(alt);
+    	ropList.add(par);
+    	ropList.add(arb);
+    	
+    	try{
+    		module = DOPtoEFA.buildModuleFromROP(ropList, moduleName, block);
+    		module.writeToFile(tmpFile);
+    	}catch(Exception e){
+    		e.printStackTrace();
+    		assertTrue(false);
+    	}
+    }
+    
+    /* Sequence nodes should be collapsed */
+    public void testCollapseRelationTreeSequence(){
+    	
+    	/* build test relation */
+    	Relation relation = buildRelationWithSameType(RelationType.SEQUENCE);
+    	
+        /* call function */
+        relation = DOPtoEFA.collapseRelationTree(relation);
+        	
+        /* All seq schould be gone */
+        assertEquals(0, relation.getActivityRelationGroup().size());
+    }
+    
+    /* Alternative nodes should be collapsed */
+    public void testCollapseRelationTreeAlternative(){
+    	
+    	/* build test relation */
+    	Relation relation = buildRelationWithSameType(RelationType.ALTERNATIVE);
+    	
+        /* call function */
+        relation = DOPtoEFA.collapseRelationTree(relation);
+        	
+        /* All seq schould be gone */
+        assertEquals(0, relation.getActivityRelationGroup().size());
+    }
+    
+    /* Parallel nodes should be collapsed */
+    public void testCollapseRelationTreeParallel(){
+    	Relation relation = buildRelationWithSameType(RelationType.PARALLEL);
+    	relation = DOPtoEFA.collapseRelationTree(relation);
+    	assertEquals(0, relation.getActivityRelationGroup().size());
+    }
+
+    /* Arbitrary order nodes should NOT be collapsed */
+    public void testCollapseRelationTreeArbitrary(){
+    	
+    	int nodes = 0;
+    	Relation relation = buildRelationWithSameType(RelationType.ARBITRARY);
+    	
+    	nodes = relation.getActivityRelationGroup().size();
+    	
+    	relation = DOPtoEFA.collapseRelationTree(relation);
+    	
+    	/* No arbitrary should be gone */
+    	assertEquals(nodes, relation.getActivityRelationGroup().size());
+    }
+    
+    
+    
+    /**
+     *	Help function 
+     */
+    private Relation buildRelationWithSameType(RelationType type){
+    	
+    	/* create relation */
+    	Relation r1 = factory.createRelation();
+    	Relation r2 = factory.createRelation();
+    	Relation r3 = factory.createRelation();
+    	Relation r4 = factory.createRelation();
+    	
+    	/* set same type */
+    	r1.setType(type);
+    	r2.setType(type);
+    	r3.setType(type);
+    	r4.setType(type);
+    	
+    	/* same relation in relation */
+    	r3.getActivityRelationGroup().add(r4);
+    	r2.getActivityRelationGroup().add(r3);
+    	r1.getActivityRelationGroup().add(r2);
+    	
+    	return r1;
+    }
+    
+    /**
+     * Builds a ROP with one sequence containing three activities.
+     * @return ROP
+     */
+    private ROP buildSequenceROP(){
+    	ROP rop = factory.createROP();
+    	Relation r = buildRelationOfType(RelationType.SEQUENCE);
+    	
+    	rop.setMachine(r.getType().toString());
+    	rop.setComment("Test ROP " + r.getType().toString());
+    	rop.setId("1");
+    	rop.setType(ROPType.ROP);
+    	rop.setRelation(r);
+    	
+    	return rop;
+    }
+    
+    /**
+     * Builds a ROP with one alternative containing three activities.
+     * @return ROP
+     */
+    private ROP buildAlternativeROP(){
+    	ROP rop = factory.createROP();
+    	Relation r = buildRelationOfType(RelationType.ALTERNATIVE);
+    	
+    	rop.setMachine(r.getType().toString());
+    	rop.setComment("Test ROP " + r.getType().toString());
+    	rop.setId("2");
+    	rop.setType(ROPType.ROP);
+    	rop.setRelation(r);
+    	
+    	return rop;
+    }
+    
+    /**
+     * Builds a ROP with one parallel containing three activities.
+     * @return ROP
+     */
+    private ROP buildParallelROP(){
+    	ROP rop = factory.createROP();
+    	Relation r = buildRelationOfType(RelationType.PARALLEL);
+    	
+    	rop.setMachine(r.getType().toString());
+    	rop.setComment("Test ROP " + r.getType().toString());
+    	rop.setId("2");
+    	rop.setType(ROPType.ROP);
+    	rop.setRelation(r);
+    	
+    	return rop;
+    }
+    
+    /**
+     * Builds a ROP with one arbitrary order containing three activities.
+     * @return ROP
+     */
+    private ROP buildArbitraryROP(){
+    	ROP rop = factory.createROP();
+    	Relation r = buildRelationOfType(RelationType.ARBITRARY);
+    	
+    	rop.setMachine(r.getType().toString());
+    	rop.setComment("Test ROP " + r.getType().toString());
+    	rop.setId("2");
+    	rop.setType(ROPType.ROP);
+    	rop.setRelation(r);
+    	
+    	return rop;
+    }
+    
+    
+    /**
+     * Builds a relation containing three activities.
+     * @return relation with three activities of type in data.
+     */
+    private Relation buildRelationOfType(RelationType type){
+    	Relation relation = factory.createRelation();
+    	
+    	Activity op1 = factory.createActivity();
+    	Activity op2 = factory.createActivity();
+    	Activity op3 = factory.createActivity();
+    	
+    	relation.setType(type);
+    	relation.getActivityRelationGroup().add(op1);
+    	relation.getActivityRelationGroup().add(op2);
+    	relation.getActivityRelationGroup().add(op3);
+    	
+    	op1.setOperation("Op1");
+    	op2.setOperation("Op2");
+    	op3.setOperation("Op3");
+    
+    	return relation;
+    }
+    
+    private Precondition buildPrecondition(){
+    	
+    	Precondition precon = factory.createPrecondition();
+    	
+    	OperationReferenceType opRefType1 = factory.createOperationReferenceType();
+    	OperationReferenceType opRefType2 = factory.createOperationReferenceType();
+    	OperationReferenceType opRefType3 = factory.createOperationReferenceType();
+    	
+    	opRefType1.setMachine("machine1");
+    	opRefType1.setOperation("Op1");
+    	
+    	opRefType2.setMachine("machine2");
+    	opRefType2.setOperation("Op2");
+    	
+    	opRefType3.setMachine("machine3");
+    	opRefType3.setOperation("Op3");
+    	
+    	precon.getPredecessor().add(opRefType1);
+    	precon.getPredecessor().add(opRefType2);
+    	precon.getPredecessor().add(opRefType3);
+    	
+    	return precon;
+    }
+    
+    private Attribute buildResourceAttribute(String name, boolean book, boolean unbook){
+    	
+    	Attribute att = factory.createAttribute();
+    	UpperIndicator ui = factory.createUpperIndicator();
+    	LowerIndicator li = factory.createLowerIndicator();
+    	
+    	ui.setIndicatorValue(book);
+    	li.setIndicatorValue(unbook);
+    	
+    	att.setAttributeValue(name);
+    	att.setUpperIndicator(ui);
+    	att.setLowerIndicator(li);
+    	
+    	return att;
+    }
+}
+
---
+++ tests/src/org/supremica/external/processeditor/tools/dop2efa/TestModule.java
@@ -0,0 +1,136 @@
+/**
+ * class to test DOP to EFA 
+ */
+package org.supremica.external.processeditor.tools.dop2efa;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.supremica.testhelpers.*;
+import java.util.List;
+import java.util.Random;
+
+import org.supremica.external.processeditor.tools.dop2efa.Module;
+import org.supremica.manufacturingTables.xsd.processeditor.*;
+
+public class TestModule extends TestCase{
+	
+	private Module module;
+    
+    public TestModule(String name){
+        super(name);
+    }
+    
+    /**
+     * Sets up the test fixture.
+     * Called before every test case method.
+     */
+    protected void setUp(){
+    	module = new Module("Test", false);
+    }
+    
+    /**
+     * Tears down the test fixture.
+     * Called after every test case method.
+     */
+    protected void tearDown(){
+    	module = null;
+    }
+    
+    /**
+     * Assembles and returns a test suite
+     * for all the test methods of this test case.
+     */
+    public static Test suite(){
+        TestSuite suite = new TestSuite(TestModule.class);
+        return suite;
+    }
+    
+    
+    public void testAddEvent(){
+    	
+    	List<String> eventList = null;
+    	String event = "testevent";
+    	
+    	/*
+    	 *	Add one event
+    	 */
+    	module.addEvent(event);
+    	
+    	
+    	/*
+    	 * Event should have same name and be
+    	 * added to list
+    	 */
+    	eventList = module.getEvents();
+    	
+    	assertEquals(1, eventList.size());
+    	assertEquals(event, eventList.get(0));
+    	
+    	/*
+    	 * Add same event again
+    	 */
+    	module.addEvent(event);
+    	
+    	/*
+    	 *	An event should not be
+    	 *	added if it already exist
+    	 */
+    	eventList = module.getEvents();
+    	assertEquals(1, eventList.size());
+    	
+    	/*
+    	 * Add another event
+    	 */
+    	module.addEvent(event + "2");
+    	
+    	/*
+    	 *	this event should be added
+    	 */
+    	eventList = module.getEvents();
+    	assertEquals(2, eventList.size());
+	}
+    
+    public void testArbitraryOrederIntegerMaxValue(){
+    	//random integer with max value 100
+    	Random rnd = new Random();
+    	int upperBound = rnd.nextInt(100);
+    	
+    	//variable name
+    	String varName = "Zon1";
+    	
+    	//test arbitrary order integer
+    	varName = module.newArbitraryOrderNodeInteger(upperBound);
+    	assertEquals(upperBound, module.getMaxValueResourceInteger(varName));
+    }
+ 
+    public void testParallelIntegerMaxValue(){
+    	
+    	//random integer with max value 100
+    	Random rnd = new Random();
+    	int upperBound = rnd.nextInt(100);
+    	
+    	//variable name
+    	String varName = "Zon1";
+    	
+    	//test parallel integer
+    	varName = module.newParrallelNodeInteger(upperBound);
+    	assertEquals(upperBound, module.getMaxValueResourceInteger(varName));
+    }
+    
+    public void testResourceIntegerMaxValue(){
+    	
+    	//random integer with max value 100
+    	Random rnd = new Random();
+    	int upperBound = rnd.nextInt(100);
+    	
+    	//variable name
+    	String varName = "Zon1";
+    	
+    	//test resource integer
+    	module.newResourceInteger(varName, upperBound);
+    	assertEquals(upperBound, module.getMaxValueResourceInteger(varName));
+    }
+}
+
