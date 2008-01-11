d5e2c4f229732a952bdb90352ff97dbe0faad0c8
Tord Andersson <unknown@anywhere.com>
2008-01-11 12:50:35 +0000

    Updated code to work with changes to implementation of EFA variables, and other changes to the IDE, and also groovy version change.

--- src/net/sourceforge/waters/subject/module/builder/Util.groovy
+++ src/net/sourceforge/waters/subject/module/builder/Util.groovy
@@ -19,6 +19,7 @@ import net.sourceforge.waters.xsd.base.ComponentKind
 import net.sourceforge.waters.xsd.base.EventKind
 import net.sourceforge.waters.subject.module.EdgeSubject
 import net.sourceforge.waters.model.expr.Operator
+import org.supremica.gui.ide.ModuleContainer
 import org.supremica.gui.ide.IDE
 import org.supremica.gui.InterfaceManager
 import org.supremica.automata.IO.ProjectBuildFromWaters
@@ -29,7 +30,8 @@ import net.sourceforge.waters.model.marshaller.DocumentManager
 class Util {
 	private static final factory = ModuleSubjectFactory.instance
 	private static final parser = new ExpressionParser(factory.instance, CompilerOperatorTable.instance)
-
+	private static final marshaller = new JAXBModuleMarshaller(factory, CompilerOperatorTable.instance)
+	
 	public static void assertGeneratedModuleEqualsManual(ModuleProxy generatedModule, ModuleProxy manualModule) {
 		def generated, manual
 		assert ProxyTools.isEqualListByContents(generated = new ArrayList(generatedModule.eventDeclList).sort{it.name}, manual = new ArrayList(manualModule.eventDeclList).sort{it.name}), "\ngenerated:$generated\nmanual   :$manual\n"
@@ -39,17 +41,17 @@ class Util {
 		assert (generated = generatedComponents.graph.collect{it.nodes.name}) == (manual = manualComponents.graph.collect{it.nodes.name}), "\ngenerated:$generated\nmanual   :$manual\n"
 		assert (generated = generatedComponents.kind) == (manual = manualComponents.kind), "\ngenerated:$generated\nmanual   :$manual\n"
 		assert (generated = generatedComponents.graph.nodes.collect{[it.name, it.propositions.eventList.name]}) == (manual = manualComponents.graph.nodes.collect{[it.name, it.propositions.eventList.name]}), "\nstate marking\ngenerated:$generated\nmanual   :$manual\n"
-		assert ProxyTools.isEqualListByContents(generated = generatedComponents.graph.nodes, manual = manualComponents.graph.nodes), "\ngenerated:$generated\nmanual   :$manual\n"
+		assert ProxyTools.isEqualListByContents(generated = generatedComponents.graph.nodes.flatten(), manual = manualComponents.graph.nodes.flatten()), "\ngenerated:$generated\nmanual   :$manual\n"
 		assert (generated = generatedComponents.graph.collect{it.edges.collect{it.labelBlock.eventList*.toString().sort()}}) == (manual = manualComponents.graph.collect{it.edges.collect{it.labelBlock.eventList*.toString().sort()}}), "\nlabelBlock\ngenerated:$generated\nmanual   :$manual\n"
 		assert (generated = generatedComponents.graph.collect{it.edges.source.toString()}) == (manual = manualComponents.graph.collect{it.edges.source.toString()}), "\nSource node\ngenerated:$generated\nmanual   :$manual\n"
 		assert (generated = generatedComponents.graph.collect{it.edges.target.toString()}) == (manual = manualComponents.graph.collect{it.edges.target.toString()}), "\nTarget node\ngenerated:$generated\nmanual   :$manual\n"
-		assert ProxyTools.isEqualListByContents(generated = generatedComponents.graph.edges.guardActionBlock.findAll{it}.guards, manual = manualComponents.graph.edges.guardActionBlock.findAll{it}.guards), "\nGuards\ngenerated:${generated}\nmanual   :${manual}\n"
-		assert ProxyTools.isEqualListByContents(generated = generatedComponents.graph.edges.guardActionBlock.findAll{it}.actions, manual = manualComponents.graph.edges.guardActionBlock.findAll{it}.actions), "\nActions\ngenerated:$generated\nmanual   :$manual\n"
-		assert ProxyTools.isEqualListByContents(generated = generatedComponents.graph.edges.guardActionBlock, manual = manualComponents.graph.edges.guardActionBlock), "\ngenerated:$generated\nmanual   :$manual\n"
+		assert ProxyTools.isEqualListByContents(generated = generatedComponents.graph.edges.guardActionBlock.flatten().findAll{it}.guards.flatten(), manual = manualComponents.graph.edges.guardActionBlock.flatten().findAll{it}.guards.flatten()), "\nGuards\ngenerated:${generated}\nmanual   :${manual}\n"
+		assert ProxyTools.isEqualListByContents(generated = generatedComponents.graph.edges.guardActionBlock.flatten().findAll{it}.actions.flatten(), manual = manualComponents.graph.edges.guardActionBlock.flatten().findAll{it}.actions.flatten()), "\nActions\ngenerated:$generated\nmanual   :$manual\n"
+		assert ProxyTools.isEqualListByContents(generated = generatedComponents.graph.edges.guardActionBlock.flatten(), manual = manualComponents.graph.edges.guardActionBlock.flatten()), "\ngenerated:$generated\nmanual   :$manual\n"
 		//assert ProxyTools.isEqualListByContents(generated = generatedComponents.graph.edges, manual = manualComponents.graph.edges), "\ngenerated:$generated\nmanual   :$manual\n"
 		assert (generated = generatedComponents.graph.deterministic) == (manual = manualComponents.graph.deterministic), "\ngenerated:$generated\nmanual   :$manual\n"
 		//assert ProxyTools.isEqualListByContents(generated = generatedComponents.graph, manual = manualComponents.graph), "\ngenerated:$generated\nmanual   :$manual\n"
-		assert ProxyTools.isEqualListByContents(generated = generatedComponents.variables.sort{it.name}, manual = manualComponents.variables.sort{it.name}), "\ngenerated:$generated\nmanual   :$manual\n"
+		assert ProxyTools.isEqualListByContents(generated = generatedModule.componentList.grep(VariableComponentProxy.class).sort{it.name}, manual = manualModule.componentList.grep(VariableComponentProxy.class).sort{it.name}), "\ngenerated:$generated\nmanual   :$manual\n"
 		//assert ProxyTools.isEqualListByContents(generated = generatedModule.componentList.findAll{true}.sort{it.name}, manual = manualModule.componentList.findAll{true}.sort{it.name}), "\ngenerated:${generated}\nmanual   :${manual}\n"
 		assert ProxyTools.isEqualListByContents(generated = new ArrayList(generatedModule.eventAliasList.expression.eventList).sort{it.name}, manual = new ArrayList(manualModule.eventAliasList.expression.eventList).sort{it.name}), "\ngenerated:$generated\nmanual   :$manual\n"
 		assert (generated = generatedModule.eventAliasList.name.sort()) == (manual = manualModule.eventAliasList.name.sort()), "\ngenerated:$generated\nmanual   :$manual\n"
@@ -66,14 +68,16 @@ class Util {
     	saveModuleToFile(module, [filename] as File)
 	}
  */
-
+ 	public static void saveModuleToFile(ModuleProxy module) {
+ 		saveModuleToFile(module, "./${module.name}${marshaller.defaultExtension}")
+	}
+ 
 	public static void saveModuleToFile(ModuleProxy module, String filename) {
-            assert false
+		saveModuleToFile(module, [filename] as File)
 	}
 	
 	public static void saveModuleToFile(ModuleProxy module, File file) {
-		def marshaller = new JAXBModuleMarshaller(factory, CompilerOperatorTable.instance)
-    	marshaller.marshal(module, file)
+		marshaller.marshal(module, file)
 	}
 	
 	private static IDE ide
@@ -82,7 +86,10 @@ class Util {
 		InterfaceManager.instance.initLookAndFeel();
 		if (!ide) ide = new IDE()
 		ide.visible = true
-		ide.installContainer(module)
+		//ide.documentManager.newDocument(module)
+		ModuleContainer moduleContainer = [ide, module]
+		moduleContainer.addStandardPropositions()
+		ide.documentContainerManager.addContainer([ide, module] as ModuleContainer)
 	}
 	public static boolean verifyNonblocking(ModuleProxy module) {
 		def supremicaProjBuilderFromWatersModule = new ProjectBuildFromWaters(new DocumentManager());
