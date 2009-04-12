15215e0cd6391154ec90f2417529c095dea43ca4
Dan Millares <unknown@anywhere.com>
2009-04-12 09:44:52 +0000

    save COP to file

--- src/org/supremica/external/processeditor/tools/copextractor/RelationExtractionWorker.java
+++ src/org/supremica/external/processeditor/tools/copextractor/RelationExtractionWorker.java
@@ -1,11 +1,13 @@
 package org.supremica.external.processeditor.tools.copextractor;
 
+import java.io.File;
 import java.util.List;
 
 import javax.swing.SwingWorker;
 
 import org.supremica.external.avocades.COPBuilder;
 import org.supremica.external.processeditor.SOCGraphContainer;
+import org.supremica.external.processeditor.xml.Loader;
 import org.supremica.manufacturingTables.xsd.processeditor.ROP;
 
 class RelationExtractionWorker
@@ -15,9 +17,12 @@ class RelationExtractionWorker
 
 	private COPBuilder builder;
 	private SOCGraphContainer container;
+	private Loader loader;
 
 	//constructors
 	RelationExtractionWorker(COPBuilder builder, SOCGraphContainer container){
+		this.loader = new Loader();
+		
 		this.builder = builder;
 		this.container = container;
 	}
@@ -57,21 +62,48 @@ class RelationExtractionWorker
 		}
 
 
+		//Sanity check
 		if(null == copList || 0 == copList.size() ){
 			System.out.println( "No COP:s" );
 			return;
 		}
 		
 		if ( null != container ){
-
-			for(ROP rop : copList){
-				container.insertResource(rop, null);
-			}
-			
-			container.setVisible(true);
-
+			saveToFolder(copList);
+			openInProcessEditor(copList);
 		} else {
 			System.out.println( "No SOC" );
 		}
 	}
+	
+	private void openInProcessEditor(List<ROP> list){
+		
+		//Sanity check
+		if ( null == container || null == list ){
+			return;
+		}
+		
+		for(ROP rop : list){
+			container.insertResource(rop, null);
+		}
+		
+		container.setVisible(true);
+	}
+	
+	
+	private void saveToFolder(List<ROP> ropList){
+		File file = null;
+		String fileName = "";
+		String PATH = "";
+		
+		for(int i = 0; i < ropList.size(); i++){	
+    		fileName = "rop_" + i + ".xml";
+    		file = new File(PATH + fileName);
+    		loader.save(ropList.get(i), file);
+    	}
+	}
+	
+	
+	
+	
 }
