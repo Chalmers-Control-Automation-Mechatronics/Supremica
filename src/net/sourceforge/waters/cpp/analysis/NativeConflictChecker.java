9eedf1ae2139eb45b40ac709599c80ced3c72730
Robi Malik <robi@cs.waikato.ac.nz>
2009-05-19 04:57:30 +0000

    BUG FIX   Add forgotten files for NativeConflictChecker.

---
+++ src/net/sourceforge/waters/cpp/analysis/NativeConflictChecker.java
@@ -0,0 +1,114 @@
+//# -*- indent-tabs-mode: nil  c-basic-offset: 2 -*-
+//###########################################################################
+//# PROJECT: Waters
+//# PACKAGE: net.sourceforge.waters.cpp.analysis
+//# CLASS:   NativeConflictChecker
+//###########################################################################
+//# $Id$
+//###########################################################################
+
+package net.sourceforge.waters.cpp.analysis;
+
+import net.sourceforge.waters.model.analysis.AbstractConflictChecker;
+import net.sourceforge.waters.model.analysis.ConflictChecker;
+import net.sourceforge.waters.model.analysis.ConflictKindTranslator;
+import net.sourceforge.waters.model.analysis.KindTranslator;
+import net.sourceforge.waters.model.analysis.VerificationResult;
+import net.sourceforge.waters.model.des.ConflictTraceProxy;
+import net.sourceforge.waters.model.des.EventProxy;
+import net.sourceforge.waters.model.des.ProductDESProxy;
+import net.sourceforge.waters.model.des.ProductDESProxyFactory;
+
+
+/**
+ * @author Robi Malik
+ */
+
+public class NativeConflictChecker
+  extends NativeModelVerifier
+  implements ConflictChecker
+{
+
+  //#########################################################################
+  //# Constructors
+  public NativeConflictChecker(final ProductDESProxyFactory factory)
+  {
+    this(null, factory);
+  }
+
+  public NativeConflictChecker(final ProductDESProxy model,
+                               final ProductDESProxyFactory factory)
+  {
+    this(model, null, factory);
+  }
+
+  public NativeConflictChecker(final ProductDESProxy model,
+                               final EventProxy marking,
+                               final ProductDESProxyFactory factory)
+  {
+    super(model, factory);
+    mMarking = marking;
+  }
+
+
+  //#########################################################################
+  //# Interface net.sourceforge.waters.model.analysis.ModelAnalyser
+  public void setModel(final ProductDESProxy model)
+  {
+    super.setModel(model);
+    mUsedMarking = null;
+  }
+
+
+  //#########################################################################
+  //# Interface net.sourceforge.waters.model.analysis.ConflictChecker
+  public void setMarkingProposition(EventProxy marking)
+  {
+    mMarking = marking;
+    mUsedMarking = null;
+    clearAnalysisResult();
+  }
+
+  public EventProxy getMarkingProposition()
+  {
+    return mMarking;
+  }
+
+  public ConflictTraceProxy getCounterExample()
+  {
+    return (ConflictTraceProxy) super.getCounterExample();
+  }
+
+
+  //#########################################################################
+  //# Auxiliary Methods
+  public EventProxy getUsedMarkingProposition()
+  {
+    if (mUsedMarking == null) {
+      if (mMarking == null) {
+        final ProductDESProxy model = getModel();
+        mUsedMarking = AbstractConflictChecker.getMarkingProposition(model);
+      } else {
+        mUsedMarking = mMarking;
+      }
+    }
+    return mUsedMarking;
+  }
+
+  public KindTranslator getKindTranslator()
+  {
+    return ConflictKindTranslator.getInstance();
+  }
+
+  
+  //#########################################################################
+  //# Native Methods
+  native VerificationResult runNativeAlgorithm();
+
+
+  //#########################################################################
+  //# Data Members
+  private EventProxy mMarking;
+  private EventProxy mUsedMarking;
+
+}
---
+++ src/net/sourceforge/waters/model/analysis/ConflictKindTranslator.java
@@ -0,0 +1,92 @@
+//# -*- indent-tabs-mode: nil  c-basic-offset: 2 -*-
+//###########################################################################
+//# PROJECT: Waters
+//# PACKAGE: net.sourceforge.waters.model.analysis
+//# CLASS:   ConflictKindTranslator
+//###########################################################################
+//# $Id$
+//###########################################################################
+
+package net.sourceforge.waters.model.analysis;
+
+import net.sourceforge.waters.model.des.AutomatonProxy;
+import net.sourceforge.waters.model.des.EventProxy;
+import net.sourceforge.waters.xsd.base.ComponentKind;
+import net.sourceforge.waters.xsd.base.EventKind;
+
+
+/**
+ * <P>A kind translator used for conflict checking.
+ * This translator remaps all plants and specifications as plants,
+ * and all controllable and uncontrollable events as uncontrollable.
+ * The use of kind translators is optional in conflict checking,
+ * but the unification of types may make things easier for some
+ * implementations.</P>
+ *
+ * @author Robi Malik
+ */
+
+public class ConflictKindTranslator implements KindTranslator
+{
+
+  //#########################################################################
+  //# Singleton Pattern
+  public static ConflictKindTranslator getInstance()
+  {
+    return SingletonHolder.theInstance;
+  }
+
+  private static class SingletonHolder {
+    private static final ConflictKindTranslator theInstance =
+      new ConflictKindTranslator();
+  }
+
+  private ConflictKindTranslator()
+  {
+  }
+
+
+  //#########################################################################
+  //# Interface net.sourceforge.waters.model.analysis.KindTranslator
+  /**
+   * Returns the component kind of the given automaton in a controllability
+   * check.
+   * @return {@link ComponentKind#PLANT}, if the given automaton is
+   *         a plant, or {@link ComponentKind#SPEC} if the
+   *         given automaton is a spec or supervisor.
+   */
+  public ComponentKind getComponentKind(final AutomatonProxy aut)
+  {
+    final ComponentKind kind = aut.getKind();
+    switch (kind) {
+    case PLANT:
+    case SPEC:
+      return ComponentKind.PLANT;
+    default:
+      return kind;
+    }
+  }
+
+  /**
+   * Returns the event kind of the given event in a language
+   * inclusion check.
+   * @return {@link EventKind#UNCONTROLLABLE}.
+   */
+  public EventKind getEventKind(final EventProxy event)
+  {
+    final EventKind kind = event.getKind();
+    switch (kind) {
+    case CONTROLLABLE:
+    case UNCONTROLLABLE:
+      return EventKind.UNCONTROLLABLE;
+    default:
+      return kind;
+    }
+  }
+
+
+  //#########################################################################
+  //# Static Class Variables
+  private static ConflictKindTranslator theInstance;
+
+}
