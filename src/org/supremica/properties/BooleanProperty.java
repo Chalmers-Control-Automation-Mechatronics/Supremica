5f2cd8046fdac7aefbf7e1fe3dca9bc90ac0cb42
Robi Malik <robi@cs.waikato.ac.nz>
2009-12-04 08:38:37 +0000

    BUG FIX   Do not fire property change event unless property has really changed.

--- src/org/supremica/properties/BooleanProperty.java
+++ src/org/supremica/properties/BooleanProperty.java
@@ -62,7 +62,7 @@ package org.supremica.properties;
 public class BooleanProperty
     extends Property
 {
-    
+
     //#######################################################################
     //# Constructors
     public BooleanProperty(final PropertyType type,
@@ -72,7 +72,7 @@ public class BooleanProperty
     {
         this(type, key, value, comment, false);
     }
-    
+
     public BooleanProperty(final PropertyType type,
         final String key,
         final boolean value,
@@ -83,52 +83,54 @@ public class BooleanProperty
         mDefaultValue = value;
         mValue = value;
     }
-    
-    
+
+
     //#######################################################################
     //# Overrides for Abstract Base Class Property
     public void set(final boolean value)
     {
+      if (mValue != value) {
         checkMutable();
-        final String oldvalue = getAsString();
         mValue = value;
+        final String oldvalue = getAsString();
         firePropertyChanged(oldvalue);
+      }
     }
-    
+
     public String getAsString()
     {
         return Boolean.toString(mValue);
     }
-    
+
     public boolean currentValueDifferentFromDefaultValue()
     {
         return mDefaultValue != mValue;
     }
-    
-    
+
+
     //#######################################################################
     //# Specific Access
     public boolean isTrue()
     {
         return mValue;
     }
-    
+
     public boolean get()
     {
         return isTrue();
     }
-    
+
     public void set(final String value)
     {
         final boolean boolval = Boolean.parseBoolean(value);
         set(boolval);
     }
-    
-    
+
+
     //#######################################################################
     //# Data Members
     private final boolean mDefaultValue;
-    
+
     private boolean mValue;
-    
+
 }
