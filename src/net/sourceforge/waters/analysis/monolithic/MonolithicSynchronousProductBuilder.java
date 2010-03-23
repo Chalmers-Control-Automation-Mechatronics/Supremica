b59193f2c594de91b241dca72ce3ed674192bd58
Robi Malik <robi@cs.waikato.ac.nz>
2010-03-23 20:37:55 +0000

    ADD   Add hiding support to MonolithicSynchronousProductBuilder.

--- src/net/sourceforge/waters/analysis/monolithic/MonolithicSynchronousProductBuilder.java
+++ src/net/sourceforge/waters/analysis/monolithic/MonolithicSynchronousProductBuilder.java
@@ -1,6 +1,8 @@
 package net.sourceforge.waters.analysis.monolithic;
 
+import gnu.trove.THashSet;
 import gnu.trove.TIntArrayList;
+import gnu.trove.TIntHashSet;
 import gnu.trove.TObjectIntHashMap;
 
 import java.util.AbstractMap;
@@ -10,6 +12,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
@@ -20,6 +23,7 @@ import net.sourceforge.waters.model.analysis.AnalysisException;
 import net.sourceforge.waters.model.analysis.OverflowException;
 import net.sourceforge.waters.model.analysis.SynchronousProductBuilder;
 import net.sourceforge.waters.model.analysis.SynchronousProductStateMap;
+import net.sourceforge.waters.model.analysis.VerificationResult;
 import net.sourceforge.waters.model.base.NamedProxy;
 import net.sourceforge.waters.model.base.ProxyVisitor;
 import net.sourceforge.waters.model.base.VisitorException;
@@ -36,8 +40,7 @@ import net.sourceforge.waters.xsd.base.EventKind;
 
 /**
  * A simple monolithic implementation of the synchronous product algorithm.
- * This implementation supports nondeterministic automata and hiding
- * (the latter not yet implemented).
+ * This implementation supports nondeterministic automata and hiding.
  * States are stored in integer arrays without compression, so it is not
  * recommended to use this implementation to compose a large number of
  * automata.
@@ -71,7 +74,11 @@ public class MonolithicSynchronousProductBuilder
   public void addMask(final Collection<EventProxy> hidden,
                       final EventProxy replacement)
   {
-    // TODO Auto-generated method stub
+    if (mMaskingPairs == null) {
+      mMaskingPairs = new LinkedList<MaskingPair>();
+    }
+    final MaskingPair pair = new MaskingPair(hidden, replacement);
+    mMaskingPairs.add(pair);
   }
 
   public Collection<EventProxy> getPropositions()
@@ -100,16 +107,17 @@ public class MonolithicSynchronousProductBuilder
   {
     setUp();
 
+    final int tableSize = Math.min(getNodeLimit(), MAX_TABLE_SIZE);
+    mStates = new IntArrayMap(tableSize);
     mStateTuples = new ArrayList<int[]>();
-    mStates = new IntArrayMap(500000);
-    mSynchronisedTransitions = new ArrayList<int[]>();
+    mTransitionBuffer = new TIntArrayList();
     mNumStates = 0;
     mUnvisited = new ArrayDeque<int[]>(100);
     permutations(mNumAutomata, null, -1, -1);
     mNumInitialStates = mNumStates;
     while (!mUnvisited.isEmpty()) {
-      final int[] cs = mUnvisited.remove();
-      explore(cs);
+      final int[] tuple = mUnvisited.remove();
+      explore(tuple);
     }
 
     final AutomatonProxy aut = createAutomaton();
@@ -119,7 +127,8 @@ public class MonolithicSynchronousProductBuilder
 
 
   //#########################################################################
-  //# Auxiliary Methods
+  //# Overrides for Base Class
+  //# net.sourceforge.waters.model.analysis.AbstractModelAnalyser
   protected void setUp()
   {
     super.setUp();
@@ -129,30 +138,55 @@ public class MonolithicSynchronousProductBuilder
     final Collection<AutomatonProxy> automata = model.getAutomata();
     mNumEvents = events.size();
     mNumAutomata = automata.size();
-    mEvents = new ArrayList<EventProxy>(events);
 
+    mEvents = new ArrayList<EventProxy>(mNumEvents);
     TObjectIntHashMap<EventProxy> eventToIndex =
-      new TObjectIntHashMap<EventProxy>(mNumEvents);
-    int e = 0;
-    for (final EventProxy event : mEvents) {
-      eventToIndex.put(event, e++);
-    }
+      new TObjectIntHashMap<EventProxy>(mNumInputEvents);
     if (mUsedPropositions == null) {
       mCurrentPropositions = new ArrayList<EventProxy>();
-      for (final EventProxy event : mEvents) {
-        if (event.getKind() == EventKind.PROPOSITION) {
-          mCurrentPropositions.add(event);
-        }
-      }
     } else {
       mCurrentPropositions = mUsedPropositions;
+    }
+    int e = 0;
+    for (final EventProxy event : events) {
+      if (event.getKind() == EventKind.PROPOSITION) {
+        if (mUsedPropositions == null) {
+          mCurrentPropositions.add(event);
+        }
+      } else {
+        mEvents.add(event);
+        eventToIndex.put(event, e++);
+      }
+    }
+    mNumInputEvents = mNumEvents = mEvents.size();
+    if (mMaskingPairs != null) {
+      mProjectionMask = new int[mNumInputEvents];
+      Arrays.fill(mProjectionMask, -1);
+      for (final MaskingPair pair : mMaskingPairs) {
+        final EventProxy replacement = pair.getReplacement();
+        if (eventToIndex.containsKey(replacement)) {
+          e = eventToIndex.get(replacement);
+        } else {
+          e = mNumEvents++;
+          eventToIndex.put(replacement, e);
+          mEvents.add(replacement);
+        }
+        for (final EventProxy hidden : pair.getHiddenEvents()) {
+          final int h = eventToIndex.get(hidden);
+          mProjectionMask[h] = e;
+        }
+      }
+      mCurrentSuccessors = new TIntHashSet[mNumEvents];
+      for (e = 0; e < mNumEvents; e++) {
+        mCurrentSuccessors[e] = new TIntHashSet();
+      }
     }
     final int numProps = mCurrentPropositions.size();
     mOriginalStates = new StateProxy[mNumAutomata][];
     mAllMarkings = new HashMap<List<EventProxy>,List<EventProxy>>();
     mStateMarkings = new List<?>[mNumAutomata][];
     // transitions indexed first by automaton then by event then by source state
-    mTransitions = new int[mNumAutomata][mNumEvents][][];
+    mTransitions = new int[mNumAutomata][mNumInputEvents][][];
     mNDTuple = new int[mNumAutomata][];
 
     int a = 0;
@@ -198,7 +232,7 @@ public class MonolithicSynchronousProductBuilder
       }
       mNDTuple[a] = initials.toNativeArray();
       final TIntArrayList[][] autTransitionLists =
-        new TIntArrayList[mNumEvents][numStates];
+        new TIntArrayList[mNumInputEvents][numStates];
       for (final TransitionProxy trans : aut.getTransitions()) {
         final int event = eventToIndex.get(trans.getEvent());
         final int source = stateToIndex.get(trans.getSource());
@@ -224,9 +258,9 @@ public class MonolithicSynchronousProductBuilder
     }
     eventToIndex = null;
 
-    mEventAutomata = new int[mNumEvents][];
+    mEventAutomata = new int[mNumInputEvents][];
     final List<IntDouble> list = new ArrayList<IntDouble>(mNumAutomata);
-    for (e = 0; e < mNumEvents; e++) {
+    for (e = 0; e < mNumInputEvents; e++) {
       for (a = 0; a < mNumAutomata; a++) {
         if (mTransitions[a][e] != null) {
           final int numStates = mTransitions[a][e].length;
@@ -252,6 +286,36 @@ public class MonolithicSynchronousProductBuilder
     }
   }
 
+  protected void tearDown()
+  {
+    mEvents = null;
+    mCurrentPropositions = null;
+    mProjectionMask = null;
+    mOriginalStates = null;
+    mAllMarkings = null;
+    mStateMarkings = null;
+    mTransitions = null;
+    mEventAutomata = null;
+
+    mStates = null;
+    mStateTuples = null;
+    mUnvisited = null;
+    mTransitionBuffer = null;
+
+    mNDTuple = null;
+    mTargetTuple = null;
+    mCurrentSuccessors = null;
+
+    super.tearDown();
+  }
+
+  protected void addStatistics(final VerificationResult result)
+  {
+    result.setNumberOfAutomata(mNumAutomata);
+    result.setNumberOfStates(mNumStates);
+    result.setNumberOfTransitions(mTransitionBuffer.size() / 3);
+  }
+
 
   //#########################################################################
   //# Auxiliary Methods
@@ -262,8 +326,13 @@ public class MonolithicSynchronousProductBuilder
     for (int a = 0; a < mNumAutomata; a++) {
       mNDTuple[a] = null;
     }
+    if (mCurrentSuccessors != null) {
+      for (int e = 0; e < mNumEvents; e++) {
+        mCurrentSuccessors[e].clear();
+      }
+    }
     events:
-    for (int e = 0; e < mNumEvents; e++) {
+    for (int e = 0; e < mNumInputEvents; e++) {
       for (final int a : mEventAutomata[e]) {
         if (mTransitions[a][e] != null) {
           final int[] succ = mTransitions[a][e][sourceTuple[a]];
@@ -320,14 +389,34 @@ public class MonolithicSynchronousProductBuilder
       mUnvisited.offer(newTuple);
       mStateTuples.add(newTuple);
     }
-    // only add a transition if not adding in an initial state
-    if (!isInitial) {
-      mSynchronisedTransitions.add(new int[] {source, event, target});
+    // Only add a transition if not adding in an initial state,
+    // and avoid duplicates.
+    if (!isInitial && mCurrentSuccessors[event].add(target)) {
+      mTransitionBuffer.add(source);
+      mTransitionBuffer.add(event);
+      mTransitionBuffer.add(target);
     }
   }
 
   private AutomatonProxy createAutomaton()
   {
+    final int numEvents = mNumEvents + mCurrentPropositions.size();
+    final Collection<EventProxy> events = new ArrayList<EventProxy>(numEvents);
+    if (mMaskingPairs == null) {
+      events.addAll(mEvents);
+    } else {
+      final THashSet<EventProxy> hidden = new THashSet<EventProxy>(mNumEvents);
+      for (final MaskingPair pair : mMaskingPairs) {
+        hidden.addAll(pair.getHiddenEvents());
+      }
+      for (final EventProxy event : mEvents) {
+        if (!hidden.contains(event)) {
+          events.add(event);
+        }
+      }
+    }
+    events.addAll(mCurrentPropositions);
+
     final int numProps = mCurrentPropositions.size();
     final List<StateProxy> states = new ArrayList<StateProxy>(mNumStates);
     for (int code = 0; code < mNumStates; code++) {
@@ -351,12 +440,17 @@ public class MonolithicSynchronousProductBuilder
     }
 
     final ProductDESProxyFactory factory = getFactory();
+    final int bufferSize = mTransitionBuffer.size();
     final ArrayList<TransitionProxy> transitions =
-      new ArrayList<TransitionProxy>(mSynchronisedTransitions.size());
-    for (final int[] trans : mSynchronisedTransitions) {
-      final StateProxy source = states.get(trans[0]);
-      final StateProxy target = states.get(trans[2]);
-      final EventProxy event = mEvents.get(trans[1]);
+      new ArrayList<TransitionProxy>(bufferSize / 3);
+    int t = 0;
+    while (t < bufferSize) {
+      int code = mTransitionBuffer.get(t++);
+      final StateProxy source = states.get(code);
+      code = mTransitionBuffer.get(t++);
+      final EventProxy event = mEvents.get(code);
+      code = mTransitionBuffer.get(t++);
+      final StateProxy target = states.get(code);
       transitions.add(factory.createTransitionProxy(source, event, target));
     }
 
@@ -372,11 +466,12 @@ public class MonolithicSynchronousProductBuilder
       }
       buffer.append(aut.getName());
     }
+    buffer.append('}');
     final String name = buffer.toString();
     final ComponentKind kind = ComponentKind.PLANT;
 
     return factory.createAutomatonProxy
-      (name, kind, mEvents, states, transitions);
+      (name, kind, events, states, transitions);
   }
 
   private List<EventProxy> getUniqueMarking(final List<EventProxy> marking)
@@ -398,10 +493,41 @@ public class MonolithicSynchronousProductBuilder
 
 
   //#########################################################################
+  //# Inner Class MaskingPair
+  private static class MaskingPair
+  {
+    //#######################################################################
+    //# Constructor
+    private MaskingPair(final Collection<EventProxy> hidden,
+                        final EventProxy replacement)
+    {
+      mHiddenEvents = hidden;
+      mReplacement = replacement;
+    }
+
+    //#######################################################################
+    //# Simple Access
+    private Collection<EventProxy> getHiddenEvents()
+    {
+      return mHiddenEvents;
+    }
+
+    private EventProxy getReplacement()
+    {
+      return mReplacement;
+    }
+
+    //#######################################################################
+    //# Data Members
+    private final Collection<EventProxy> mHiddenEvents;
+    private final EventProxy mReplacement;
+  }
+
+
+  //#########################################################################
   //# Inner Class MemStateMap
   private static class MemStateMap implements SynchronousProductStateMap
   {
-
     //#######################################################################
     //# Constructor
     private MemStateMap(final Collection<AutomatonProxy> automata)
@@ -453,7 +579,6 @@ public class MonolithicSynchronousProductBuilder
     //# Data Members
     private final List<AutomatonProxy> mInputAutomata;
     private final StateProxy[][] mStateLists;
-
   }
 
 
@@ -464,12 +589,11 @@ public class MonolithicSynchronousProductBuilder
    */
   private static class MemStateProxy implements StateProxy
   {
-
     //#######################################################################
     //# Constructor
-    public MemStateProxy(final int name, final int[] stateTuple,
-                         final Collection<EventProxy> props,
-                         final boolean isInitial)
+    private MemStateProxy(final int name, final int[] stateTuple,
+                          final Collection<EventProxy> props,
+                          final boolean isInitial)
     {
       mName = name;
       mStateTuple = stateTuple;
@@ -555,9 +679,7 @@ public class MonolithicSynchronousProductBuilder
   //# Inner Class IntArrayMap
   private static class IntArrayMap extends AbstractMap<int[],Integer>
   {
-    final Map<IntArray,Integer> mMap;
-
-    public IntArrayMap(final int num)
+    private IntArrayMap(final int num)
     {
       mMap = new HashMap<IntArray,Integer>(num);
     }
@@ -595,6 +717,8 @@ public class MonolithicSynchronousProductBuilder
     {
       return mMap.values();
     }
+
+    private final Map<IntArray,Integer> mMap;
   }
 
 
@@ -664,11 +788,14 @@ public class MonolithicSynchronousProductBuilder
   //#########################################################################
   //# Data Members
   private Collection<EventProxy> mUsedPropositions;
+  private Collection<MaskingPair> mMaskingPairs;
 
   private int mNumAutomata;
+  private int mNumInputEvents;
   private int mNumEvents;
   private List<EventProxy> mEvents;
   private Collection<EventProxy> mCurrentPropositions;
+  private int[] mProjectionMask;
   private StateProxy[][] mOriginalStates;
   private Map<List<EventProxy>,List<EventProxy>> mAllMarkings;
   private List<?>[][] mStateMarkings;
@@ -680,9 +807,15 @@ public class MonolithicSynchronousProductBuilder
   private Map<int[],Integer> mStates;
   private List<int[]> mStateTuples;
   private Queue<int[]> mUnvisited;
-  private List<int[]> mSynchronisedTransitions;
+  private TIntArrayList mTransitionBuffer;
 
   private int[][] mNDTuple;
   private int[] mTargetTuple;
+  private TIntHashSet[] mCurrentSuccessors;
+
+
+  //#########################################################################
+  //# Class Constants
+  private static final int MAX_TABLE_SIZE = 500000;
 
 }
