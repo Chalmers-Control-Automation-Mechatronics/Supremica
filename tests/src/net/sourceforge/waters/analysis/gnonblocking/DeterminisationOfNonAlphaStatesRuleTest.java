593c843aeef1bd6d766eef45b2fbb7895027445e
rmf18 <unknown@anywhere.com>
2010-05-04 01:54:08 +0000

    Added a test case for DeterminisationOfNonAlphaStatesRule with multiple states marked with alpha, some merging possible.

--- tests/src/net/sourceforge/waters/analysis/gnonblocking/DeterminisationOfNonAlphaStatesRuleTest.java
+++ tests/src/net/sourceforge/waters/analysis/gnonblocking/DeterminisationOfNonAlphaStatesRuleTest.java
@@ -226,6 +226,22 @@ public class DeterminisationOfNonAlphaStatesRuleTest extends
     runAbstractionRule(group, subdir, name);
   }
 
+  public void test_determinisation_21() throws Exception
+  {
+    final String group = "tests";
+    final String subdir = "abstraction";
+    final String name = "determinisation_21.wmod";
+    runAbstractionRule(group, subdir, name);
+  }
+
+  public void test_determinisation_22() throws Exception
+  {
+    final String group = "tests";
+    final String subdir = "abstraction";
+    final String name = "determinisation_22.wmod";
+    runAbstractionRule(group, subdir, name);
+  }
+
   /**
    * A test to see whether a single abstraction rule object can perform multiple
    * abstractions in sequence.
