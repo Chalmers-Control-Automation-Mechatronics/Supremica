a21c7ce81ba71cad17059108e5384b7ca7bd547a
rmf18 <unknown@anywhere.com>
2010-05-12 22:06:56 +0000

    Added remaining statistic getter/setters for the AbstractionRuleStatitics class

--- src/net/sourceforge/waters/analysis/gnonblocking/AbstractionRuleStatistics.java
+++ src/net/sourceforge/waters/analysis/gnonblocking/AbstractionRuleStatistics.java
@@ -5,7 +5,10 @@ import java.util.Formatter;
 
 
 /**
- * Contains statistics about the application of an abstraction rule.
+ * Contains statistics about the application of an abstraction rule to one
+ * model. Intended use is that a rule will be applied multiple times during the
+ * composition of a model, and this will hold accumulative data of each
+ * application of the rule.
  *
  * @author Rachel Francis
  */
@@ -14,23 +17,36 @@ public class AbstractionRuleStatistics
   // #########################################################################
   // # Constructors
 
+  public AbstractionRuleStatistics(final String ruleName)
+  {
+    mName = ruleName;
+    mRunTime = 0;
+    mAppliedCount = 0;
+    mReductionCount = 0;
+    mInputStates = 0;
+    mOutputStates = 0;
+    mInputTransitions = 0;
+    mOutputTransitions = 0;
+    mUnchangedStates = 0;
+    mUnchangedTransitions = 0;
+  }
+
   // #########################################################################
   // # Simple Access Methods
   /**
-   * Gets the time taken to run the abstraction rule.
+   * Gets the name of the abstraction rule these statistics are for.
+   */
+  public String getRuleName()
+  {
+    return mName;
+  }
+
+  /**
+   * Gets the accumulative time taken to run the abstraction rule.
    */
   public long getRunTime()
   {
     return mRunTime;
-  }
-
-  /**
-   * Gets the number of times this abstraction rule reduced the size of the
-   * model it was applied to.
-   */
-  public int getResult()
-  {
-    return mReductionCount;
   }
 
   /**
@@ -41,32 +57,188 @@ public class AbstractionRuleStatistics
     return mAppliedCount;
   }
 
+  /**
+   * Gets the number of times this abstraction rule reduced the size of the
+   * model it was applied to.
+   */
+  public int getReductionCount()
+  {
+    return mReductionCount;
+  }
+
+  /**
+   * Gets the sum of the number of input states for this rule when a reduction
+   * occurred.
+   */
+  public int getInputStatesSum()
+  {
+    return mInputStates;
+  }
+
+  /**
+   * Gets the sum of the number of output states for this rule when a reduction
+   * occurred.
+   */
+  public int getOutputStatesSum()
+  {
+    return mOutputStates;
+  }
+
+  /**
+   * Gets the sum of the number of input transitions for this rule when a
+   * reduction occurred.
+   */
+  public int getInputTransitionsSum()
+  {
+    return mInputTransitions;
+  }
+
+  /**
+   * Gets the sum of the number of output transitions for this rule when a
+   * reduction occurred.
+   */
+  public int getOutputTransitionsSum()
+  {
+    return mOutputTransitions;
+  }
+
+  /**
+   * Gets the sum of the number of input states for this rule when a reduction
+   * didn't occur (therefore, output count = this input count).
+   */
+  public int getUnchangedStatesSum()
+  {
+    return mUnchangedStates;
+  }
+
+  /**
+   * Gets the sum of the number of input transitions for this rule when a
+   * reduction didn't occur (therefore, output count = this input count).
+   */
+  public int getUnchangedTransitionsSum()
+  {
+    return mUnchangedTransitions;
+  }
+
+  /**
+   * Gets the sum of the total number of input states for this rule. This
+   * includes input for when a reduction did occur and when a reduction didn't
+   * occur.
+   */
+  public int getTotalInputStates()
+  {
+    return mInputStates + mUnchangedStates;
+  }
+
+  /**
+   * Gets the sum of the total number of input transitions for this rule. This
+   * includes input for when a reduction did occur and when a reduction didn't
+   * occur.
+   */
+  public int getTotalInputTransitions()
+  {
+    return mInputTransitions + mUnchangedTransitions;
+  }
+
+  /**
+   * Gets the sum of the total number of output states for this rule. This
+   * includes output for when a reduction did occur and when a reduction didn't
+   * occur.
+   */
+  public int getTotalOutputStates()
+  {
+    return mOutputStates + mUnchangedStates;
+  }
+
+  /**
+   * Gets the sum of the total number of output transitions for this rule. This
+   * includes output for when a reduction did occur and when a reduction didn't
+   * occur.
+   */
+  public int getTotalOutputTransitions()
+  {
+    return mOutputTransitions + mUnchangedTransitions;
+  }
+
   // #########################################################################
   // # Providing Statistics
   /**
-   * Specifies the number of times this abstraction rule reduced the size of the
+   * Adds to the number of times this abstraction rule reduced the size of the
    * model it was applied to.
    */
-  public void setResult(final int reduced)
+  public void addToReductionCount(final int reduced)
   {
-    mReductionCount = reduced;
+    mReductionCount += reduced;
   }
 
   /**
-   * Specifies the time taken for the abstraction rule to run.
+   * Adds to the time taken for the abstraction rule to run.
    */
-  public void setRunTime(final long runTime)
+  public void addToRunTime(final long runTime)
   {
-    mRunTime = runTime;
+    mRunTime += runTime;
   }
 
   /**
-   * Specifies the number of times this abstraction rule is applied to the
-   * model.
+   * Adds to the number of times this abstraction rule is applied to the model.
    */
-  public void setApplicationCount(final int count)
+  public void addToApplicationCount(final int count)
   {
-    mAppliedCount = count;
+    mAppliedCount += count;
+  }
+
+  /**
+   * Adds to the sum of the number of input states for this rule when a
+   * reduction occurred.
+   */
+  public void addToInputStates(final int countToAdd)
+  {
+    mInputStates += countToAdd;
+  }
+
+  /**
+   * Adds to the sum of the number of output states for this rule when a
+   * reduction occurred.
+   */
+  public void addToOutputStates(final int countToAdd)
+  {
+    mOutputStates += countToAdd;
+  }
+
+  /**
+   * Adds to the sum of the number of input transitions for this rule when a
+   * reduction occurred.
+   */
+  public void addToInputTransitions(final int countToAdd)
+  {
+    mInputTransitions += countToAdd;
+  }
+
+  /**
+   * Adds to the sum of the number of output transitions for this rule when a
+   * reduction occurred.
+   */
+  public void addToOutputTransitions(final int countToAdd)
+  {
+    mOutputTransitions += countToAdd;
+  }
+
+  /**
+   * Adds to the sum of the number of input states for this rule when a
+   * reduction didn't occur.
+   */
+  public void addToUnchangedStates(final int countToAdd)
+  {
+    mUnchangedStates += countToAdd;
+  }
+
+  /**
+   * Adds to the sum of the number of input transitions for this rule when a
+   * reduction didn't occur.
+   */
+  public void addToUnchangedTransitions(final int countToAdd)
+  {
+    mUnchangedTransitions += countToAdd;
   }
 
   // #########################################################################
@@ -75,13 +247,42 @@ public class AbstractionRuleStatistics
   {
     @SuppressWarnings("unused")
     final Formatter formatter = new Formatter(stream);
-    // TODO:add stats to print
+    stream.println("Name of rule: " + mName);
+    stream.println("Total run time: " + mRunTime);
+    stream.println("Total number of times applied: " + mAppliedCount);
+    stream.println("Total number of times a reduction occurred: "
+        + mReductionCount);
+    final double probability =
+        (double) mReductionCount / (double) mAppliedCount;
+    stream.println("Probability of a reduction occurring: " + probability);
+    stream.println("Total number of input states: " + getTotalInputStates());
+    stream.println("Total number of input transitions: "
+        + getTotalInputTransitions());
+    stream.println("Total number of output states: " + getTotalOutputStates());
+    stream.println("Total number of output transitions: "
+        + getTotalOutputTransitions());
+    stream.println("Sum of input states with a reduction: " + mInputStates);
+    stream.println("Sum of input transitions with a reduction: "
+        + mInputTransitions);
+    stream.println("Sum of output states with a reduction: " + mOutputStates);
+    stream.println("Sum of output transitions with a reduction: "
+        + mOutputTransitions);
+    stream
+        .println("Sum of input states with no reduction: " + mUnchangedStates);
+    stream.println("Sum of input transitions with no reduction: "
+        + mUnchangedTransitions);
   }
 
   // #########################################################################
   // # Data Members
+  private final String mName;
   private long mRunTime;
   private int mAppliedCount;
   private int mReductionCount;
-
+  private int mInputStates;
+  private int mOutputStates;
+  private int mInputTransitions;
+  private int mOutputTransitions;
+  private int mUnchangedStates;
+  private int mUnchangedTransitions;
 }
