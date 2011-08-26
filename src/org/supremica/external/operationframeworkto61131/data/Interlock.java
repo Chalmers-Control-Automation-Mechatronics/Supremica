26a48e3b1cf8ae0a4fb30655f4ec6900962f755d
Robi Malik <robi@cs.waikato.ac.nz>
2011-08-26 17:14:00 +0000

    REMOVE   Delete non-compiling packages org.supremica.external.operationframeworkto61131 and   org.supremica.external.robotCoordination.

--- src/org/supremica/external/operationframeworkto61131/data/Interlock.java
+++
@@ -1,9 +0,0 @@
-package org.supremica.external.operationframeworkto61131.data;
-/**
- * @author LC
- *
- */
-public class Interlock {
-
-//	Only use the class as a type name
-}
--- src/org/supremica/external/robotCoordination/Status.java
+++
@@ -1,7 +0,0 @@
-package org.supremica.external.robotCoordination;
-
-public class Status
-{
-	boolean occupied;
-	boolean checked;
-}
