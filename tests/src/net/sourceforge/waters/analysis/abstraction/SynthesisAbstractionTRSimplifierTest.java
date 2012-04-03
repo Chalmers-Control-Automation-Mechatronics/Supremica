ee8c3b4d4cd0a8e848803d3eb1f61c8bbfba1c26
Robi Malik <robi@waikato.ac.nz>
2012-04-04 10:04:29 +1200

    ADD. More synthesis abstraction tests.

--- tests/src/net/sourceforge/waters/analysis/abstraction/SynthesisAbstractionTRSimplifierTest.java
+++ tests/src/net/sourceforge/waters/analysis/abstraction/SynthesisAbstractionTRSimplifierTest.java
@@ -311,6 +311,14 @@ public class SynthesisAbstractionTRSimplifierTest
     runTransitionRelationSimplifier(group, subdir, name);
   }
 
+  public void test_synthesisAbstraction_21() throws Exception
+  {
+    final String group = "tests";
+    final String subdir = "abstraction";
+    final String name = "synthesisAbstraction_21.wmod";
+    runTransitionRelationSimplifier(group, subdir, name);
+  }
+
 
   /**
    * A test to see whether a single abstraction rule object can perform multiple
