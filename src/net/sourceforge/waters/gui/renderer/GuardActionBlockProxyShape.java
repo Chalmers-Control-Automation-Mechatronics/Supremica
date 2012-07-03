305713d82c8f013e3600d04b32bed40a6707c694
Carly Hona <carly.7@hotmail.com>
2012-07-03 16:15:40 +1200

    ADD. Completed drag functionality. Shift-drag enables vertical/horizontal move only, and Control-drag disables snapping to grid during move.

--- src/net/sourceforge/waters/gui/renderer/GuardActionBlockProxyShape.java
+++ src/net/sourceforge/waters/gui/renderer/GuardActionBlockProxyShape.java
@@ -10,24 +10,24 @@ import net.sourceforge.waters.model.module.GuardActionBlockProxy;
 public class GuardActionBlockProxyShape
     extends AbstractProxyShape
 {
-    public GuardActionBlockProxyShape(GuardActionBlockProxy block, RoundRectangle2D bounds)
+    public GuardActionBlockProxyShape(final GuardActionBlockProxy block, final RoundRectangle2D bounds)
     {
         super(block);
         mBlock = block;
         mBounds = bounds;
     }
-    
+
     public GuardActionBlockProxy getProxy()
     {
         return (GuardActionBlockProxy)super.getProxy();
     }
-    
+
     public RoundRectangle2D getShape()
     {
         return mBounds;
     }
-    
-    public void draw(Graphics2D g, RenderingInformation status)
+
+    public void draw(final Graphics2D g, final RenderingInformation status)
     {
         g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         if (status.isFocused())
@@ -36,16 +36,14 @@ public class GuardActionBlockProxyShape
             g.setStroke(SHADOWSTROKE);
             g.fill(getShape());
         }
-	/*
-        g.setColor(EditorColor.GUARDCOLOR);
-        g.setStroke(BASICSTROKE);
-        int offset = g.getFontMetrics().getHeight();
-        g.drawString(mBlock.getGuards().toString(),
-                     (int) mBounds.getX(), (int) mBounds.getY() + offset);
-	*/
+        if (status.isSelected()) {
+          g.setColor(status.getColor());
+          g.setStroke(BASICSTROKE);
+          g.draw(getShape());
+        }
     }
-    
-    private RoundRectangle2D mBounds;
+
+    private final RoundRectangle2D mBounds;
     GuardActionBlockProxy mBlock;
     public static final int DEFAULTARCW = 8;
     public static final int DEFAULTARCH = 8;
