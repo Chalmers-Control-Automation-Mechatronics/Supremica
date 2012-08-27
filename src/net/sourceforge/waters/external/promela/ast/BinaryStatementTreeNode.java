0caad22f0c5451bee59b7144b11e3e7b566f9063
doommaggot <ejd9@waikato.ac.nz>
2012-08-28 11:21:17 +1200

    Added in variable assignment rule to the grammar in the promela importer

---
+++ src/net/sourceforge/waters/external/promela/ast/BinaryStatementTreeNode.java
@@ -0,0 +1,28 @@
+package net.sourceforge.waters.external.promela.ast;
+
+
+import net.sourceforge.waters.external.promela.PromelaVisitor;
+
+import org.antlr.runtime.CommonToken;
+
+public class BinaryStatementTreeNode extends PromelaTree
+{
+  final private String mOperator;
+
+  public BinaryStatementTreeNode(final int token, final String operator)
+  {
+    super(new CommonToken(token));
+    mOperator = operator;
+  }
+
+  public Object acceptVisitor(final PromelaVisitor visitor)
+  {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public String getOperator()
+  {
+    return mOperator;
+  }
+}
