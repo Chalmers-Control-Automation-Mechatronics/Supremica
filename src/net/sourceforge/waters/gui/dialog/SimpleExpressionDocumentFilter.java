aa53028a8f1f16b1c5cbedcf4b8dfca22da2b2ca
Robi Malik <robi@waikato.ac.nz>
2013-05-03 22:53:36 +1200

    MODIFY. Comment panel no longer accepts invalid XML characters.

--- src/net/sourceforge/waters/gui/dialog/SimpleExpressionDocumentFilter.java
+++ src/net/sourceforge/waters/gui/dialog/SimpleExpressionDocumentFilter.java
@@ -1,6 +1,6 @@
 //# -*- indent-tabs-mode: nil  c-basic-offset: 2 -*-
 //###########################################################################
-//# PROJECT: Waters/Supremica GUI
+//# PROJECT: Waters GUI
 //# PACKAGE: net.sourceforge.waters.gui.dialog
 //# CLASS:   SimpleExpressionDocumentFilter
 //###########################################################################
@@ -10,19 +10,22 @@
 
 package net.sourceforge.waters.gui.dialog;
 
-import javax.swing.text.AttributeSet;
-import javax.swing.text.BadLocationException;
 import javax.swing.text.DocumentFilter;
 
+import net.sourceforge.waters.gui.util.CharacterDocumentFilter;
 import net.sourceforge.waters.model.expr.ExpressionParser;
 
 
 /**
+ * A {@link DocumentFilter} implementation that can be linked to
+ * Swing text fields in order to restrict the set of possible
+ * characters to those allowed in Waters expressions.
+ *
  * @author Robi Malik
  */
 
 class SimpleExpressionDocumentFilter
-  extends DocumentFilter
+  extends CharacterDocumentFilter
 {
 
   //#########################################################################
@@ -34,63 +37,15 @@ class SimpleExpressionDocumentFilter
 
 
   //#########################################################################
-  //# Overrides for class javax.swing.DocumentFilter
-  public void insertString(final DocumentFilter.FilterBypass bypass,
-			   final int offset,
-			   final String text,
-			   final AttributeSet attribs)
-    throws BadLocationException
-  {
-    final String filtered = filter(text);
-    if (filtered != null) {
-      super.insertString(bypass, offset, filtered, attribs);
-    }
-  }
-
-  public void replace(final DocumentFilter.FilterBypass bypass,
-		      final int offset,
-		      final int length,
-		      final String text,
-		      final AttributeSet attribs)
-    throws BadLocationException
-  {
-    final String filtered = filter(text);
-    if (filtered != null) {
-      super.replace(bypass, offset, length, filtered, attribs);
-    }
-  }
-
-
-  //#########################################################################
-  //# Auxiliary Methods
-  String filter(final String text)
-  {
-    if (text == null) {
-      return null;
-    } else {
-      final int len = text.length();
-      final StringBuffer buffer = new StringBuffer(len);
-      for (int i = 0; i < len; i++) {
-	final char ch = text.charAt(i);
-	if (isAllowedCharacter(ch)) {
-	  buffer.append(ch);
-	}
-      }
-      if (buffer.length() == 0) {
-	return null;
-      } else {
-	return buffer.toString();
-      }
-    }
-  }
-
-  boolean isAllowedCharacter(final char ch)
+  //# Overrides for net.sourceforge.waters.gui.CharacterDocumentFilter
+  @Override
+  protected boolean isAllowedCharacter(final char ch)
   {
     return mExpressionParser.isExpressionCharacter(ch);
   }
 
 
-  //#######################################################################
+  //#########################################################################
   //# Data Members
   private final ExpressionParser mExpressionParser;
 
