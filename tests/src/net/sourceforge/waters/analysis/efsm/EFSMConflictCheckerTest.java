a7a1ff39a1b402d87caf6ea0c7891f96b40d38f4
Robi Malik <robi@waikato.ac.nz>
2013-06-05 07:38:30 +1200

    MODIFY. Add failing prime sieve to EFSM conflict checker test suite.

--- tests/src/net/sourceforge/waters/analysis/efsm/EFSMConflictCheckerTest.java
+++ tests/src/net/sourceforge/waters/analysis/efsm/EFSMConflictCheckerTest.java
@@ -182,6 +182,12 @@ public class EFSMConflictCheckerTest
     checkConflict("efa", "prime_sieve4", true);
   }
 
+  public void testPrimeSieve4fail()
+    throws IOException, WatersException
+  {
+    checkConflict("efa", "prime_sieve4b", false);
+  }
+
   public void testPrimeSieve5()
     throws IOException, WatersException
   {
