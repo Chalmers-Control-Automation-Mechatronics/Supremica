1ddab71e169202953895d14fb1ccb293c6218ca4
Robi Malik <robi@waikato.ac.nz>
2013-08-29 19:10:51 +1200

    MODIFY. Refine TRPartition class and add documentation.

--- src/net/sourceforge/waters/analysis/tr/TRPartition.java
+++ src/net/sourceforge/waters/analysis/tr/TRPartition.java
@@ -1,6 +1,6 @@
 //# -*- indent-tabs-mode: nil  c-basic-offset: 2 -*-
 //###########################################################################
-//# PROJECT: Waters/Supremica GUI
+//# PROJECT: Waters Analysis
 //# PACKAGE: net.sourceforge.waters.analysis.tr
 //# CLASS:   TRPartition
 //###########################################################################
@@ -9,39 +9,192 @@
 
 package net.sourceforge.waters.analysis.tr;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 /**
+ * <P>A partition to represent equivalent states of a {@link
+ * ListBufferTransitionRelation}.</P>
+ *
+ * <P>A partition consists of a list of equivalence classes, each representing
+ * a set of equivalent states. States in the same equivalence class are
+ * equivalent and may be merged.</P>
+ *
+ * <P>This class represents states as integer primitives. There are two
+ * standard ways to represent and access a partition.</P>
+ * <DL>
+ * <DT>Class List.</DT>
+ * <DD>A list of integer arrays (<CODE>{@link List}&lt;int[]&gt;</CODE>),
+ * each of which contains the states in an equivalence class. The state
+ * code of an equivalence class is given by its position in the list.
+ * The list may contain <CODE>null</CODE> entries to represent unused
+ * or bad equivalence class numbers.</DD>
+ * <DT>State-to-Class Map.</DD>
+ * <DD>An integer array that assigns to each state the number of its
+ * equivalence class (which is the position of the class in the above
+ * class list). Two states are equivalent of their array entries contain
+ * the same equivalence class number. The array may contain negative
+ * entries as class numbers to represent bad states that are not merged
+ * into any equivalence class but removed.</DD>
+ * </DL>
+ * <P>This class supports both of the above partition representation.
+ * The user can initialise the partition by providing data in either
+ * form, and access information using both forms. The missing index
+ * structure is computed automatically when requested the first time.</P>
+ *
  * @author Robi Malik, Sahar Mohajerani
  */
+
 public class TRPartition
 {
-  public TRPartition(final List<int[]> partition, final int size)
+
+  //#########################################################################
+  //# Constructors
+  /**
+   * Creates a partition using data in class-list form.
+   * @param classes      List of equivalence classes.
+   * @param numStates    The total number of states to be partitioned.
+   *                     The largest state number mentioned in the equivalence
+   *                     classes must be less than the number of states.
+   */
+  public TRPartition(final List<int[]> classes, final int numStates)
   {
-    mPartition = partition;
-    mSize = size;
+    mNumberOfStates = numStates;
+    mNumberOfClasses = classes.size();
+    mClasses = classes;
     mStateToClass = null;
   }
 
-  public List<int[]> getPartition()
+  /**
+   * Creates a partition using data in state-to-class map form.
+   * @param stateToClass Array assigning equivalence class numbers to states.
+   * @param numClasses   The total number of equivalence classes.
+   *                     The largest class number mentioned in the map
+   *                     must be less than the number of classes.
+   */
+  public TRPartition(final int[] stateToClass, final int numClasses)
   {
-    return mPartition;
+    mNumberOfStates = stateToClass.length;
+    mNumberOfClasses = numClasses;
+    mClasses = null;
+    mStateToClass = stateToClass;
   }
 
-  public int getSize()
+
+  //#########################################################################
+  //# Simple Access
+  /**
+   * Gets the number of states partitioned by this partitioned.
+   */
+  public int getNumberOfStates()
   {
-    return mSize;
+    return mNumberOfStates;
   }
 
+  /**
+   * Gets the number of equivalence classes in this partition.
+   */
+  public int getNumberOfClasses()
+  {
+    return mNumberOfClasses;
+  }
+
+  /**
+   * Gets the list of equivalence classes.
+   * @return A list of integer arrays, each of which contains the states in
+   *         an equivalence class. The state code of an equivalence class is
+   *         given by its position in the list. The list may contain
+   *         <CODE>null</CODE> entries to represent unused or bad equivalence
+   *         class numbers.
+   */
+  public List<int[]> getClasses()
+  {
+    setUpClasses();
+    return mClasses;
+  }
+
+  /**
+   * Gets the state-to-class map representing this partition.
+   * @return An integer array that assigns to each state the number of its
+   *         equivalence class. The array may contain negative entries to
+   *         represent bad states that are not merged into any equivalence
+   *         class but removed.
+   */
   public int[] getStateToClass()
   {
+    setUpStateToClass();
+    return mStateToClass;
+  }
+
+  /**
+   * Gets the states in the given equivalence class.
+   * @param  clazz    The number of the equivalence class to be checked.
+   * @return An array containing the state codes in this equivalence class,
+   *         or <CODE>null</CODE> indicating an empty or bad class.
+   */
+  public int[] getStates(final int clazz)
+  {
+    setUpClasses();
+    return mClasses.get(clazz);
+  }
+
+  /**
+   * Gets the equivalence class number of the given state.
+   * @param  state    The number of the state to be looked up.
+   * @return The number of the state's equivalence class. A negative
+   *         number may be returned to indicate a bad state that is not
+   *         merged into any class.
+   */
+  public int getClassCode(final int state)
+  {
+    setUpStateToClass();
+    return mStateToClass[state];
+  }
+
+
+  //#########################################################################
+  //# Auxiliary Methods
+  private void setUpClasses()
+  {
+    if (mClasses == null) {
+      final int[] counts = new int[mNumberOfClasses];
+      for (int s = 0; s < mNumberOfStates; s++) {
+        final int c = mStateToClass[s];
+        if (c >= 0) {
+          counts[c]++;
+        }
+      }
+      mClasses = new ArrayList<int[]>(mNumberOfClasses);
+      for (int c = 0; c < mNumberOfClasses; c++) {
+        final int count = counts[c];
+        if (count > 0) {
+          final int[] clazz = new int[count];
+          mClasses.add(clazz);
+          counts[c] = 0;
+        } else {
+          mClasses.add(null);
+        }
+      }
+      for (int s = 0; s < mNumberOfStates; s++) {
+        final int c = mStateToClass[s];
+        if (c >= 0) {
+          final int index = counts[c]++;
+          final int[] clazz = mClasses.get(c);
+          clazz[index] = s;
+        }
+      }
+    }
+  }
+
+  private void setUpStateToClass()
+  {
     if (mStateToClass == null) {
-      mStateToClass = new int[mSize];
+      mStateToClass = new int[mNumberOfStates];
       Arrays.fill(mStateToClass, -1);
-      for (int i = 0; i < mPartition.size(); i++)
+      for (int i = 0; i < mClasses.size(); i++)
       {
-        final int[] clazz = mPartition.get(i);
+        final int[] clazz = mClasses.get(i);
         if (clazz != null) {
           for (final int s : clazz) {
             mStateToClass[s] = i;
@@ -49,13 +202,14 @@ public class TRPartition
         }
       }
     }
-    return mStateToClass;
   }
+
 
   //#########################################################################
   //# Data Members
-
-  private final List<int[]> mPartition;
-  private final int mSize;
+  private final int mNumberOfStates;
+  private final int mNumberOfClasses;
+  private List<int[]> mClasses;
   private int[] mStateToClass;
+
 }
