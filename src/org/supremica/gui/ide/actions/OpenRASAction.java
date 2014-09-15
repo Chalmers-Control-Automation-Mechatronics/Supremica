9eb0a441813a4020c896e7adc39ace5f43dc786d
Robi Malik <robi@waikato.ac.nz>
2014-09-15 19:42:06 +1200

    MODIFY. Open RAS only sets module after compile and uses new icon.

--- src/org/supremica/gui/ide/actions/OpenRASAction.java
+++ src/org/supremica/gui/ide/actions/OpenRASAction.java
@@ -2,19 +2,22 @@ package org.supremica.gui.ide.actions;
 
 import java.awt.event.ActionEvent;
 import java.io.File;
+
 import javax.swing.Action;
-import javax.swing.ImageIcon;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+
+import net.sourceforge.waters.gui.util.IconLoader;
 import net.sourceforge.waters.subject.module.ModuleSubject;
 import net.sourceforge.waters.subject.module.ModuleSubjectFactory;
+
 import org.supremica.automata.FlowerEFABuilder;
 import org.supremica.gui.ide.DocumentContainerManager;
 import org.supremica.gui.ide.IDE;
 
 /**
  * Create a EFA model for a Resource Allocation System (RAS)
- * 
+ *
  * @ author Sajed, Zhennan
  */
 
@@ -25,8 +28,7 @@ public class OpenRASAction extends net.sourceforge.waters.gui.actions.IDEAction 
         super(ide);
         putValue(Action.NAME, "Open RAS ...");
         putValue(Action.SHORT_DESCRIPTION, "Open/import a RAS module");
-        putValue(Action.SMALL_ICON,
-                new ImageIcon(IDE.class.getResource("/toolbarButtonGraphics/general/Open16.gif")));
+        putValue(Action.SMALL_ICON, IconLoader.ICON_TOOL_OPEN);
     }
 
     // # Interface java.awt.event.ActionListener
@@ -43,23 +45,19 @@ public class OpenRASAction extends net.sourceforge.waters.gui.actions.IDEAction 
         final int choice = chooser.showOpenDialog(frame);
         // Load the files ...
         if (choice == JFileChooser.APPROVE_OPTION) {
-           
             final File selectedRAS = chooser.getSelectedFile();
             final String rasName = selectedRAS.getName();
-
-            final DocumentContainerManager cmanager =
-                    ide.getDocumentContainerManager();
-
-            ModuleSubject module = ModuleSubjectFactory.getInstance()
+            final ModuleSubject module = ModuleSubjectFactory.getInstance()
                     .createModuleProxy(rasName, null);
-
-            cmanager.newContainer(module);
-
             FlowerEFABuilder flbuilder = null;
             flbuilder = new FlowerEFABuilder(selectedRAS, module);
             flbuilder.buildEFA();
+            final DocumentContainerManager cmanager =
+              ide.getDocumentContainerManager();
+            cmanager.newContainer(module);
         }
     }
+
     // #########################################################################
     // # Class Constants
     private static final long serialVersionUID = 1L;
