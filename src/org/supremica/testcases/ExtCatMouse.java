baaefb93fde84ab78ab915ccc3593d35270ed617
Robi Malik <robi@waikato.ac.nz>
2017-08-11 08:34:22 -0300

    BUG FIX. Eliminate Eclipse (Oxygen) compiler warning.
    Declaring MODEL_ID as final gives a warning because only one of the
    branches in the if-statement is ever taken and the rest is redundant
    code. If this variable is intended to be changed, it should probably be
    made a parameter to the class constructor.

--- src/org/supremica/testcases/ExtCatMouse.java
+++ src/org/supremica/testcases/ExtCatMouse.java
@@ -23,17 +23,17 @@ class Level extends Room
 //    private static Logger logger = LoggerFactory.createLogger(ExtCatMouse.class);
     private final Automata level;
     final String LEVEL_NAME = "L";
-    
+
     public Level()
     {
         level = new Automata();
     }
-    
-    public Level(int id, int num, int num_levels) throws Exception
+
+    public Level(final int id, final int num, final int num_levels) throws Exception
     {
        this();
        this.id = id;
-     
+
        for (int i = 0; i < CatMouse.NUMBER_OF_ROOMS; ++i)
        {
            final Room room = new Room(i,num);
@@ -41,22 +41,22 @@ class Level extends Room
            level.addAutomaton(roomAuto);
        }
     }
-    
+
     public Automata buildLevel()
 		throws Exception
     {
         final Automata sm = new Automata(level);
         final Automaton[] r = new Automaton[CatMouse.NUMBER_OF_ROOMS];
-        
+
         for(int i = 0; i < CatMouse.NUMBER_OF_ROOMS; i++)
         {
             r[i] = sm.getAutomatonAt(i);
             sm.renameAutomaton(r[i], LEVEL_NAME + id + NAME_SEP + r[i].getName());
         }
-        
+
         return sm;
     }
-    
+
     public Automata getAutomata()
     {
         return level;
@@ -64,28 +64,28 @@ class Level extends Room
 }
 
 public class ExtCatMouse
-{    
+{
     final Project project = new Project("Extended Cat Mouse");
     final Automata theAutomata = new Automata();
     final int number_of_levels, number_of_cats;
-    
-    final int MODEL_ID = 4;
-    
+
+    static int MODEL_ID = 4;
+
 	public ExtCatMouse(final int num, final int num_levels)
 		throws Exception
 	{
 		this(num, num_levels, false);
 	}
-	
-    public ExtCatMouse(int num, int num_levels, final boolean use_selfloops)
+
+    public ExtCatMouse(final int num, final int num_levels, final boolean use_selfloops)
         throws Exception
     {
         // Add comment
         project.setComment("Consider the cat and mouse problem. Assume this five rooms maze is just the first level of a tower composed by n identical levels. A controllable bidirectional passageway connects room j of level 5*i+j to room j of  5*i+j+1 (for i = 0, 1, 2   ..., and   j = 1, 2, 3, 4, 5). The first level is only connected with the second, the last level is only connected with the last-but-one. There are initially k cats in room 1 of the first level and k mice in room 5 of the last level.");
-        
+
         number_of_levels = num_levels;
         number_of_cats = num;
-       
+
         if(MODEL_ID == 1)
         {
             for (int i = 0; i < num; ++i)
@@ -115,7 +115,7 @@ public class ExtCatMouse
                 }
             }
         }
-        
+
         //THE NEW MODEL
         else if(MODEL_ID == 2)
         {
@@ -128,13 +128,13 @@ public class ExtCatMouse
                 {
                     cb[i][j] = new CatBuffer(num,number_of_levels,i+1,j+1);
                     mb[i][j] = new MouseBuffer(num,number_of_levels,i+1,j+1);
-                    
+
                     project.addAutomaton(cb[i][j].getAutomaton());
-                    theAutomata.addAutomaton(cb[i][j].getAutomaton());                                       
-                    
+                    theAutomata.addAutomaton(cb[i][j].getAutomaton());
+
                     project.addAutomaton(mb[i][j].getAutomaton());
                     theAutomata.addAutomaton(mb[i][j].getAutomaton());
-                    
+
                     spec[i][j] = new RoomSpec(i+1,j+1,number_of_cats, num_levels, cb[i][j].getFEvents(),cb[i][j].getBEvents(),mb[i][j].getFEvents(),mb[i][j].getBEvents());                    project.addAutomaton(spec[i][j].getAutomaton());
                     theAutomata.addAutomaton(spec[i][j].getAutomaton());
                 }
@@ -149,12 +149,12 @@ public class ExtCatMouse
                 for (int j = 0; j < 5; ++j)
                 {
                     ecb[i][j] = new ExtCatBuffer(num,number_of_levels,i+1,j+1);
-                    
+
                     project.addAutomaton(ecb[i][j].getAutomaton());
                     theAutomata.addAutomaton(ecb[i][j].getAutomaton());
-                    
+
                     emb[i][j] = new ExtMouseBuffer(num,number_of_levels,i+1,j+1);
-                    
+
                     project.addAutomaton(emb[i][j].getAutomaton());
                     theAutomata.addAutomaton(emb[i][j].getAutomaton());
                 }
@@ -171,26 +171,26 @@ public class ExtCatMouse
                 {
                     cb[i][j] = new CatBuffer(num,number_of_levels,i+1,j+1);
                     mb[i][j] = new MouseBuffer(num,number_of_levels,i+1,j+1);
-                    
-                    spec[i][j] = new ExtRoomSpec(i+1,j+1,number_of_cats, num_levels, cb[i][j].getFEvents(),cb[i][j].getBEvents(),mb[i][j].getFEvents(),mb[i][j].getBEvents());                    
+
+                    spec[i][j] = new ExtRoomSpec(i+1,j+1,number_of_cats, num_levels, cb[i][j].getFEvents(),cb[i][j].getBEvents(),mb[i][j].getFEvents(),mb[i][j].getBEvents());
                     project.addAutomaton(spec[i][j].getAutomaton());
                     theAutomata.addAutomaton(spec[i][j].getAutomaton());
                 }
             }
-        }     
+        }
     }
-    
+
     public Automata getAutomata()
     {
         return theAutomata;
     }
-    
+
     public Project getProject()
     {
         return project;
     }
 	// For debugging only
-	public static void main(String[] args)
+	public static void main(final String[] args)
 	{
 		try
 		{
@@ -206,5 +206,5 @@ public class ExtCatMouse
 		{
 			excp.printStackTrace();
 		}
-	}    
+	}
 }
