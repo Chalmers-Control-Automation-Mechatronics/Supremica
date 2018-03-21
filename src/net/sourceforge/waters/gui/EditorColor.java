Chalmers-Control-Automation-Mechatronics/Supremica

commit 427751f9a7d1704545a0ab6e1bb70fa42f22e30d

ADD. Options for black&white layout, and to suppress state names.
The layout mode option in the options now includes BlackAndWhite, which disables all colours in graphs. Another option to suppress state names is available as gui.editor.hideStateNames in configuration files, but not editable. These options are useful for scripted EPS generation, not recommended for use with the GUI.

src/net/sourceforge/waters/gui/EditorColor.java
@@ -86,7 +86,7 @@ public class EditorColor
     if (o instanceof GroupNodeProxy) {
       return DEFAULTCOLOR_NODEGROUP;
     } else if (o instanceof LabelGeometryProxy) {
-      return DEFAULTCOLOR_LABEL;
+      return NODE_LABEL_COLOR;
     } else {
       return DEFAULTCOLOR;
     }
@@ -145,10 +145,10 @@ public class EditorColor
   /** The colour of guard expressions. */
   public static final Color GUARDCOLOR = Color.CYAN.darker().darker();
 
-  /** The color of guard expressions. */
+  /** The colour of guard expressions. */
   public static final Color ADDEDGUARDCOLOR = new Color(114,68,174);
 
-  /** The color of action expressions. */
+  /** The colour of action expressions. */
   public static final Color ACTIONCOLOR = Color.RED.darker().darker();
 
   /** The colour of the drag-select area. */
@@ -158,7 +158,7 @@ public class EditorColor
   public static final Color DEFAULTCOLOR = Color.BLACK;
 
   /** The default colour of node labels. */
-  public static final Color DEFAULTCOLOR_LABEL = Color.GREEN.darker().darker();
+  public static final Color NODE_LABEL_COLOR = Color.GREEN.darker().darker();
 
   /** The default colour of group nodes. */
   public static final Color DEFAULTCOLOR_NODEGROUP = new Color(64,64,64);

src/net/sourceforge/waters/gui/renderer/ColorGroup.java
@@ -31,70 +31,49 @@
 //# exception.
 //###########################################################################
 
+
 package net.sourceforge.waters.gui.renderer;
 
-import java.awt.Font;
-
 import net.sourceforge.waters.gui.EditorColor;
-import net.sourceforge.waters.gui.ModuleContext;
-import net.sourceforge.waters.gui.util.PropositionIcon.ColorInfo;
-import net.sourceforge.waters.model.module.GraphProxy;
-import net.sourceforge.waters.model.module.IdentifierProxy;
-import net.sourceforge.waters.model.module.SimpleNodeProxy;
-import net.sourceforge.waters.subject.base.ModelChangeEvent;
-import net.sourceforge.waters.xsd.base.EventKind;
 
 
 /**
- * An implementation of the {@link RenderingContext} interface based on
- * a module context. This implementation obtains event fonts and proposition
- * colours using the best guess of a module context, while suppressing all
- * highlighting.
+ * Enumeration of coloured items in a rendered graph.
+ *
+ * These are mapped to actual colour by the {@link RenderingContext}.
+ * The default colours are defined in class {@link EditorColor}.
  *
  * @author Robi Malik
  */
 
-public class ModuleRenderingContext
-  extends DefaultRenderingContext
+enum ColorGroup
 {
-
-  //#########################################################################
-  //# Constructors
-  public ModuleRenderingContext(final ModuleContext context)
-  {
-    mModuleContext = context;
-  }
-
-
-  //#########################################################################
-  //# Interface net.sourceforge.waters.gui.renderer.RenderingContext
-  @Override
-  public ColorInfo getMarkingColorInfo(final GraphProxy graph,
-                                       final SimpleNodeProxy node)
-  {
-    return mModuleContext.guessPropositionColors(graph, node);
-  }
-
-  @Override
-  public Font getFont(final IdentifierProxy ident)
-  {
-    if (mModuleContext.guessEventKind(ident) == EventKind.UNCONTROLLABLE) {
-      return EditorColor.UNCONTROLLABLE_FONT;
-    } else {
-      return EditorColor.DEFAULT_FONT;
-    }
-  }
-
-  @Override
-  public boolean causesPropositionStatusChange(final ModelChangeEvent event,
-                                               final GraphProxy graph)
-  {
-    return mModuleContext.causesPropositionStatusChange(event, graph);
-  }
-
-
-  //#########################################################################
-  //# Data Members
-  private final ModuleContext mModuleContext;
-
+  /**
+   * A regular graphical item such as a node or edge, but not a group node.
+   */
+  GRAPH_ITEM,
+  /**
+   * A group node.
+   */
+  GROUP_NODE,
+  /**
+   * A state name.
+   */
+  NODE_LABEL,
+  /**
+   * An event name or other element of a label block.
+   */
+  EVENT_LABEL,
+  /**
+   * A regular guard in a guard/action block.
+   */
+  NORMAL_GUARD,
+  /**
+   * A special guard added by seamless synthesis.
+   */
+  ADDED_GUARD,
+  /**
+   * An action in a guard/action block.
+   */
+  ACTION
 }

src/net/sourceforge/waters/gui/renderer/DefaultRenderingContext.java
@@ -101,7 +101,7 @@ public class DefaultRenderingContext
   }
 
   @Override
-  public ColorInfo getColorInfo(final GraphProxy graph,
+  public ColorInfo getMarkingColorInfo(final GraphProxy graph,
                                 final SimpleNodeProxy node)
   {
     final PlainEventListProxy props = node.getPropositions();

commit 5dc6ea5318b59a37d2b606edf69cacd60b2dbfe0

BUG FIX. Selected state names are shown in blue again.
This has been broken in 427751f9a7d1704545a0ab6e1bb70fa42f22e30d and should now be fixed.

src/net/sourceforge/waters/gui/EditorColor.java
@@ -36,11 +36,6 @@ package net.sourceforge.waters.gui;
 import java.awt.Color;
 import java.awt.Font;
 
-import net.sourceforge.waters.model.base.Proxy;
-import net.sourceforge.waters.model.module.GroupNodeProxy;
-import net.sourceforge.waters.model.module.LabelGeometryProxy;
-import net.sourceforge.waters.model.module.SimpleNodeProxy;
-
 
 /**
  * A collection of all the colours used in the editor.
@@ -54,65 +49,12 @@ public class EditorColor
   //#########################################################################
   //# Colour Selection
   /**
-   * Returns the appropriate colour for painting this object.
-   */
-  public static Color getColor(final Proxy o,
-                               final GraphPanel.DragOverStatus dragOver,
-                               final boolean selected,
-                               final boolean error,
-                               final boolean hasfocus)
-  {
-    // In order of importance
-    if (dragOver != GraphPanel.DragOverStatus.NOTDRAG) {
-      if (dragOver == GraphPanel.DragOverStatus.CANDROP) {
-        return CANDROPCOLOR;
-      } else if (dragOver == GraphPanel.DragOverStatus.CANTDROP) {
-        return CANTDROPCOLOR;
-      }
-    } else if (selected) {
-      if (hasfocus) {
-        return GRAPH_SELECTED_FOCUSSED;
-      } else {
-        return GRAPH_SELECTED_NOTFOCUSSED;
-      }
-    } else if (error) {
-      if (o instanceof SimpleNodeProxy) {
-        return ERRORCOLOR_NODE;
-      } else {
-        return ERRORCOLOR;
-      }
-    }
-    // Defaults
-    if (o instanceof GroupNodeProxy) {
-      return DEFAULTCOLOR_NODEGROUP;
-    } else if (o instanceof LabelGeometryProxy) {
-      return NODE_LABEL_COLOR;
-    } else {
-      return DEFAULTCOLOR;
-    }
-  }
-
-  /**
-   * Returns a lighter shade of the color of the object for drawing a "shadow".
-   */
-  public static Color getShadowColor
-    (final Proxy o,
-     final GraphPanel.DragOverStatus dragOver,
-     final boolean selected,
-     final boolean error,
-     final boolean hasfocus)
-  {
-    final Color color = getColor(o, dragOver, selected, error, hasfocus);
-    return shadow(color);
-  }
-
-  /**
    * Returns a transparent variant of the supplied colour. The
-   * alpha-value is changed to {@link #SHADOWALPHA}.
+   * alpha-value is changed to {@link #SHADOW_ALPHA}.
    */
   public static Color shadow(final Color color)
   {
-    return shadow(color, SHADOWALPHA);
+    return shadow(color, SHADOW_ALPHA);
   }
 
   /**
@@ -258,7 +200,7 @@ public class EditorColor
   //#########################################################################
   //# Private Class Constants
   /** The alpha value of the shadow-colours. */
-  private static final int SHADOWALPHA = 48;
+  private static final int SHADOW_ALPHA = 48;
 
 
 }

src/net/sourceforge/waters/gui/GraphEditorPanel.java
@@ -33,6 +33,9 @@
 
 package net.sourceforge.waters.gui;
 
+import gnu.trove.set.hash.THashSet;
+
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Font;
@@ -96,10 +99,12 @@ import net.sourceforge.waters.gui.language.ProxyNamer;
 import net.sourceforge.waters.gui.observer.EditorChangedEvent;
 import net.sourceforge.waters.gui.observer.Observer;
 import net.sourceforge.waters.gui.observer.SelectionChangedEvent;
+import net.sourceforge.waters.gui.renderer.ColorGroup;
 import net.sourceforge.waters.gui.renderer.GeneralShape;
 import net.sourceforge.waters.gui.renderer.GeometryAbsentException;
 import net.sourceforge.waters.gui.renderer.Handle;
 import net.sourceforge.waters.gui.renderer.Handle.HandleType;
+import net.sourceforge.waters.gui.renderer.LayoutMode;
 import net.sourceforge.waters.gui.renderer.MiscShape;
 import net.sourceforge.waters.gui.renderer.ModuleRenderingContext;
 import net.sourceforge.waters.gui.renderer.ProxyShape;
@@ -174,8 +179,6 @@ import org.supremica.gui.ide.IDE;
 import org.supremica.gui.ide.IDEToolBar;
 import org.supremica.gui.ide.ModuleContainer;
 import org.supremica.properties.Config;
-
-import gnu.trove.set.hash.THashSet;
 
 
 /**
@@ -1605,13 +1608,9 @@ public class GraphEditorPanel
    */
   private Handle getClickedHandle(final ProxySubject item, final Point point)
   {
-    if (isSelected(item)) {
-      final boolean handlesAreShown = getShapeProducer().getRenderingContext()
-            .getRenderingInformation(item).showHandles();
-      if (!handlesAreShown) {
-        return null;
-      }
-      final ProxyShape shape = getShapeProducer().getShape(item);
+    if (isSelected(item) && getCurrentSelection().size() == 1) {
+      final ProxyShapeProducer producer = getShapeProducer();
+      final ProxyShape shape = producer.getShape(item);
       return shape.getClickedHandle(point);
     } else {
       return null;
@@ -1780,41 +1779,47 @@ public class GraphEditorPanel
     //#######################################################################
     //# Interface net.sourceforge.waters.gui.renderer.RenderingContext
     @Override
-    public RenderingInformation getRenderingInformation(final Proxy proxy)
+    public RenderingInformation getRenderingInformation(final Proxy proxy,
+                                                        final ColorGroup group)
     {
       final ProxySubject item = (ProxySubject) proxy;
       final boolean focused = isRenderedFocused(item);
       final boolean selected = isRenderedSelected(item);
-      final boolean showHandles = getCurrentSelection().size() == 1;
-        //showHandles = mController.canBeSelected(item);
+      final DragOverStatus dragOver;
+      if (focused && mInternalDragAction != null) {
+        dragOver = mInternalDragAction.getExternalDragStatus();
+      } else {
+        dragOver = DragOverStatus.NOTDRAG;
+      }
+      final boolean hasFocus = isTrackedFocusOwner();
+      final ProxySubject orig = getOriginal(item);
+      final ModuleCompilationErrors errors =
+        getModuleContext().getCompilationErrors();
       final boolean overlap = isOverlap(item);
+      final boolean nestedError = mErrorVisitor.isRenderedError(orig);
+      final boolean error = overlap || nestedError;
+
+      final LayoutMode layout = Config.GUI_EDITOR_LAYOUT_MODE.get();
+      final Color color =
+        layout.getColor(group, dragOver, selected, error, hasFocus);
+      if (color == null) {
+        return null;
+      }
+      final Color shadow = EditorColor.shadow(color);
+      final boolean showHandles = getCurrentSelection().size() == 1;
+      //showHandles = mController.canBeSelected(item);
+      final boolean underlined = errors.isUnderlined(orig);
       int priority = getPriority(item);
       if (mDontDraw.contains(item)) {
         priority = -1;
       } else if (selected) {
         priority += 6;
       }
-      final DragOverStatus dragover;
-      if (focused && mInternalDragAction != null) {
-        dragover = mInternalDragAction.getExternalDragStatus();
-      } else {
-        dragover = DragOverStatus.NOTDRAG;
-      }
-      final boolean focussed = isTrackedFocusOwner();
-      final ProxySubject orig = getOriginal(item);
-      final ModuleCompilationErrors errors =
-        getModuleContext().getCompilationErrors();
-      final boolean underlined = errors.isUnderlined(orig);
-      final boolean nestedError = mErrorVisitor.isRenderedError(orig);
-      final boolean error = overlap || nestedError;
       return new RenderingInformation
-        (selected, showHandles, underlined, focused,
-         EditorColor.getColor(item, dragover, selected, error, focussed),
-         EditorColor.getShadowColor(item, dragover, selected, error, focussed),
-         priority);
+        (selected, showHandles, underlined, focused, color, shadow, priority);
     }
 
-    //#########################################################################
+    //#######################################################################
     //# Data Members
     private final ErrorVisitor mErrorVisitor;
   }

src/net/sourceforge/waters/gui/renderer/ColorGroup.java
@@ -46,12 +46,16 @@ import net.sourceforge.waters.gui.EditorColor;
  * @author Robi Malik
  */
 
-enum ColorGroup
+public enum ColorGroup
 {
   /**
-   * A regular graphical item such as a node or edge, but not a group node.
+   * A regular graphical item such an edge, but not a node.
    */
   GRAPH_ITEM,
+  /**
+   * A simple node.
+   */
+  SIMPLE_NODE,
   /**
    * A group node.
    */

src/net/sourceforge/waters/gui/renderer/DefaultRenderingContext.java
@@ -33,6 +33,7 @@
 
 package net.sourceforge.waters.gui.renderer;
 
+import java.awt.Color;
 import java.awt.Font;
 import java.util.List;
 
@@ -57,6 +58,8 @@ import net.sourceforge.waters.model.module.PlainEventListProxy;
 import net.sourceforge.waters.model.module.SimpleExpressionProxy;
 import net.sourceforge.waters.model.module.SimpleNodeProxy;
 import net.sourceforge.waters.subject.base.ModelChangeEvent;
+
+import org.supremica.properties.Config;
 
 
 /**
@@ -89,15 +92,19 @@ public class DefaultRenderingContext
   }
 
   @Override
-  public RenderingInformation getRenderingInformation(final Proxy proxy)
+  public RenderingInformation getRenderingInformation(final Proxy proxy,
+                                                      final ColorGroup group)
   {
+    final LayoutMode layout = Config.GUI_EDITOR_LAYOUT_MODE.get();
+    final Color color = layout.getColor
+      (group, GraphPanel.DragOverStatus.NOTDRAG, false, false, false);
+    if (color == null) {
+      return null;
+    }
+    final Color shadow = EditorColor.shadow(color);
+    final int priority = getPriority(proxy);
     return new RenderingInformation
-      (false, false, false, false,
-       EditorColor.getColor
-         (proxy, GraphPanel.DragOverStatus.NOTDRAG, false, false, true),
-       EditorColor.getShadowColor
-         (proxy, GraphPanel.DragOverStatus.NOTDRAG, false, false, true),
-       getPriority(proxy));
+      (false, false, false, false, color, shadow, priority);
   }
 
   @Override

src/net/sourceforge/waters/gui/renderer/LayoutMode.java
@@ -37,6 +37,7 @@ package net.sourceforge.waters.gui.renderer;
 import java.awt.Color;
 
 import net.sourceforge.waters.gui.EditorColor;
+import net.sourceforge.waters.gui.GraphPanel;
 
 import org.supremica.properties.Config;
 
@@ -64,7 +65,7 @@ public enum LayoutMode
    */
   BlackAndWhite {
     @Override
-    Color getColor(final ColorGroup group)
+    public Color getDefaultColor(final ColorGroup group)
     {
       switch (group) {
       case GROUP_NODE:
@@ -89,16 +90,61 @@ public enum LayoutMode
   //#########################################################################
   //# Methods
   /**
-   * Gets the colour to render items of the given type.
+   * Gets the colour to render items of the given type, if it has the
+   * indicated highlighting status.
+   * @param  group    The colour group identifying the type of object to be
+   *                  rendered.
+   * @param  dragOver The state of a drag&amp;drop operation affecting the
+   *                  rendering of the item.
+   * @param  selected Whether or not the item is selected.
+   * @param  error    Whether or not an error is reported for the item.
+   * @param  hasFocus Whether or not the panel displaying the item has
+   *                  the keyboard focus.
+   * @return The colour to be used, or <CODE>null</CODE> to suppress rendering
+   *         of the item.
+   */
+  public Color getColor(final ColorGroup group,
+                        final GraphPanel.DragOverStatus dragOver,
+                        final boolean selected,
+                        final boolean error,
+                        final boolean hasFocus)
+  {
+    // In order of importance
+    if (dragOver != GraphPanel.DragOverStatus.NOTDRAG) {
+      if (dragOver == GraphPanel.DragOverStatus.CANDROP) {
+        return EditorColor.CANDROPCOLOR;
+      } else if (dragOver == GraphPanel.DragOverStatus.CANTDROP) {
+        return EditorColor.CANTDROPCOLOR;
+      }
+    } else if (selected) {
+      if (hasFocus) {
+        return EditorColor.GRAPH_SELECTED_FOCUSSED;
+      } else {
+        return EditorColor.GRAPH_SELECTED_NOTFOCUSSED;
+      }
+    } else if (error) {
+      if (group == ColorGroup.SIMPLE_NODE) {
+        return EditorColor.ERRORCOLOR_NODE;
+      } else {
+        return EditorColor.ERRORCOLOR;
+      }
+    }
+    return getDefaultColor(group);
+  }
+
+  /**
+   * Gets the default colour to render items of the given type, if it does
+   * not have any special status such as selected or highlighted.
    * @param  group  The colour group identifying the type of object to be
    *                rendered.
    * @return The colour to be used, or <CODE>null</CODE> to suppress rendering
    *         of the item.
    */
-  Color getColor(final ColorGroup group)
+  public Color getDefaultColor(final ColorGroup group)
   {
     switch (group) {
     case GRAPH_ITEM:
+    case SIMPLE_NODE:
       return EditorColor.DEFAULTCOLOR;
     case GROUP_NODE:
       return EditorColor.DEFAULTCOLOR_NODEGROUP;

src/net/sourceforge/waters/gui/renderer/PrintRenderingContext.java
@@ -33,34 +33,15 @@
 
 package net.sourceforge.waters.gui.renderer;
 
-import net.sourceforge.waters.gui.EditorColor;
-import net.sourceforge.waters.gui.GraphPanel.DragOverStatus;
 import net.sourceforge.waters.gui.ModuleContext;
-import net.sourceforge.waters.model.base.Proxy;
-import net.sourceforge.waters.subject.base.ProxySubject;
+
 
 public class PrintRenderingContext extends ModuleRenderingContext
 {
 
-    public PrintRenderingContext(final ModuleContext context)
+  public PrintRenderingContext(final ModuleContext context)
   {
     super(context);
   }
-
-    //#######################################################################
-    //# Interface net.sourceforge.waters.gui.renderer.RenderingContext
-    @Override
-    public RenderingInformation getRenderingInformation(final Proxy proxy)
-    {
-      final ProxySubject item = (ProxySubject) proxy;
-      final int priority = getPriority(item);
-      return new RenderingInformation
-        (false, false, false, false,
-         EditorColor.getColor(item, DragOverStatus.NOTDRAG, false,
-                              false, false),
-         EditorColor.getShadowColor(item, DragOverStatus.NOTDRAG, false,
-                                    false, false),
-         priority);
-    }
 
 }

src/net/sourceforge/waters/gui/renderer/Renderer.java
@@ -33,24 +33,21 @@
 
 package net.sourceforge.waters.gui.renderer;
 
-import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.util.List;
 import java.util.PriorityQueue;
 
-import net.sourceforge.waters.gui.EditorColor;
 import net.sourceforge.waters.model.base.Proxy;
 import net.sourceforge.waters.model.module.BinaryExpressionProxy;
 import net.sourceforge.waters.model.module.EdgeProxy;
 import net.sourceforge.waters.model.module.ForeachProxy;
 import net.sourceforge.waters.model.module.GraphProxy;
+import net.sourceforge.waters.model.module.GroupNodeProxy;
 import net.sourceforge.waters.model.module.LabelBlockProxy;
 import net.sourceforge.waters.model.module.NodeProxy;
 import net.sourceforge.waters.model.module.SimpleExpressionProxy;
 import net.sourceforge.waters.model.module.SimpleNodeProxy;
-
-import org.supremica.properties.Config;
 
 
 public class Renderer
@@ -77,28 +74,30 @@ public class Renderer
     // Blocked events
     final LabelBlockProxy blocked = mGraph.getBlockedEvents();
     if (blocked != null) {
-      addToQueue(blocked);
+      addToQueue(blocked, ColorGroup.GRAPH_ITEM);
       listToRender(blocked.getEventIdentifierList());
     }
 
     // Nodes
     for (final NodeProxy proxy : mGraph.getNodes()) {
-      addToQueue(proxy);
       if (proxy instanceof SimpleNodeProxy) {
+        addToQueue(proxy, ColorGroup.SIMPLE_NODE);
         final SimpleNodeProxy simple = (SimpleNodeProxy) proxy;
         addToQueue(simple.getLabelGeometry(), ColorGroup.NODE_LABEL);
+      } else if (proxy instanceof GroupNodeProxy) {
+        addToQueue(proxy, ColorGroup.GROUP_NODE);
       }
     }
 
     // Edges
     for (final EdgeProxy edge : mGraph.getEdges()) {
-      addToQueue(edge);
-      addToQueue(edge.getLabelBlock());
+      addToQueue(edge, ColorGroup.GRAPH_ITEM);
+      addToQueue(edge.getLabelBlock(), ColorGroup.GRAPH_ITEM);
 
       listToRender(edge.getLabelBlock().getEventIdentifierList());
 
       if (edge.getGuardActionBlock() != null) {
-        addToQueue(edge.getGuardActionBlock());
+        addToQueue(edge.getGuardActionBlock(), ColorGroup.GRAPH_ITEM);
         for (final BinaryExpressionProxy action :
              edge.getGuardActionBlock().getActions()) {
           addToQueue(action, ColorGroup.ACTION);
@@ -158,43 +157,28 @@ public class Renderer
   private void listToRender(final List<Proxy> list)
   {
     for (final Proxy proxy : list) {
-      addToQueue(proxy);
+      addToQueue(proxy, ColorGroup.GRAPH_ITEM);
       if (proxy instanceof ForeachProxy){
         final ForeachProxy foreach = (ForeachProxy) proxy;
         listToRender(foreach.getBody());
-        addToQueue(foreach.getRange());
+        addToQueue(foreach.getRange(), ColorGroup.GRAPH_ITEM);
         if(foreach.getGuard() != null) {
-          addToQueue(foreach.getGuard());
+          addToQueue(foreach.getGuard(), ColorGroup.GRAPH_ITEM);
         }
       }
     }
   }
 
-  protected void addToQueue(final Proxy proxy)
-  {
-    final RenderingContext context =
-      mProxyShapeProducer.getRenderingContext();
-    final RenderingInformation info = context.getRenderingInformation(proxy);
-    final ProxyShape shape = mProxyShapeProducer.getShape(proxy);
-    mQueue.offer(new ShapeToRender(shape, info));
-  }
-
   protected void addToQueue(final Proxy proxy, final ColorGroup group)
   {
-    final LayoutMode layout = Config.GUI_EDITOR_LAYOUT_MODE.get();
-    final Color color = layout.getColor(group);
-    if (color == null) {
-      return;
-    }
-    final Color shadow = EditorColor.shadow(color);
     final RenderingContext context =
       mProxyShapeProducer.getRenderingContext();
-    final RenderingInformation info = context.getRenderingInformation(proxy);
-    final RenderingInformation fixed = new RenderingInformation(
-        info.isSelected(), info.showHandles(), info.isUnderlined(),
-        info.isFocused(), color, shadow, info.getPriority());
-    final ProxyShape shape = mProxyShapeProducer.getShape(proxy);
-    mQueue.offer(new ShapeToRender(shape, fixed));
+    final RenderingInformation info =
+      context.getRenderingInformation(proxy, group);
+    if (info != null) {
+      final ProxyShape shape = mProxyShapeProducer.getShape(proxy);
+      mQueue.offer(new ShapeToRender(shape, info));
+    }
   }
 
 

src/net/sourceforge/waters/gui/renderer/RenderingContext.java
@@ -63,8 +63,16 @@ public interface RenderingContext
    * The rendering information provides the colours and highlighting status
    * for an item to be displayed. This method is <I>not</I> required to
    * return the same result when called multiply with the same input.
+   * @param  proxy   The item to be displayed.
+   * @param  group   The colour group for the item. The colour group
+   *                 provides additional context, e.g., to determine whether
+   *                 an expression is rendered as an event label or as part
+   *                 of a guard/action block.
+   * @return The rendering context to be used,
+   *         or <CODE>null</CODE> to suppress rendering of the item.
    */
-  public RenderingInformation getRenderingInformation(Proxy proxy);
+  public RenderingInformation getRenderingInformation(Proxy proxy,
+                                                      ColorGroup group);
 
   /**
    * Gets colour information for the display of the given simple node in

src/net/sourceforge/waters/gui/renderer/RenderingInformation.java
@@ -38,19 +38,9 @@ import java.awt.Color;
 
 public class RenderingInformation
 {
-  private final boolean mIsSelected;
-  private final boolean mShowHandles;
-  private final Color mColor;
-  private final Color mShadowColor;
-  private final boolean mIsUnderlined;
-  private final boolean mIsFocused;
-  private final int mPriority;
 
-  /*
-   * public RenderingInformation(EditorObject o) { this(false, false, false,
-   * EditorObject.NOTDRAG, o); }
-   */
-
+  //#######################################################################
+  //# Constructor
   public RenderingInformation(final boolean isSelected,
                               final boolean showHandles,
                               final boolean isUnderlined,
@@ -66,6 +56,9 @@ public class RenderingInformation
     mPriority = priority;
   }
 
+
+  //#######################################################################
+  //# Simple Access
   public boolean isSelected()
   {
     return mIsSelected;
@@ -91,6 +84,11 @@ public class RenderingInformation
     return mColor;
   }
 
+  public void setColor(final Color color)
+  {
+    mColor = color;
+  }
+
   public Color getShadowColor()
   {
     return mShadowColor;
@@ -100,4 +98,16 @@ public class RenderingInformation
   {
     return mPriority;
   }
+
+
+  //#######################################################################
+  //# Data Members
+  private final boolean mIsSelected;
+  private final boolean mShowHandles;
+  private Color mColor;
+  private final Color mShadowColor;
+  private final boolean mIsUnderlined;
+  private final boolean mIsFocused;
+  private final int mPriority;
+
 }

src/net/sourceforge/waters/gui/simulator/AutomatonDisplayPane.java
@@ -55,8 +55,8 @@ import java.util.Map;
 
 import net.sourceforge.waters.gui.BackupGraphPanel;
 import net.sourceforge.waters.gui.EditorColor;
-import net.sourceforge.waters.gui.GraphPanel;
 import net.sourceforge.waters.gui.ModuleContext;
+import net.sourceforge.waters.gui.renderer.ColorGroup;
 import net.sourceforge.waters.gui.renderer.GeometryAbsentException;
 import net.sourceforge.waters.gui.renderer.ModuleRenderingContext;
 import net.sourceforge.waters.gui.renderer.ProxyShape;
@@ -582,7 +582,8 @@ public class AutomatonDisplayPane
     }
 
     @Override
-    public RenderingInformation getRenderingInformation(final Proxy proxy)
+    public RenderingInformation getRenderingInformation(final Proxy proxy,
+                                                        final ColorGroup group)
     {
       // The spring embedder modifies a copy of our graph. When it is running,
       // the items being displayed are not in our compiled graph ...
@@ -591,7 +592,7 @@ public class AutomatonDisplayPane
         // *** BUG? ***
         // Identifiers have no original, this may cause failure to
         // highlight them while spring embedding.
-        return super.getRenderingInformation(proxy);
+        return super.getRenderingInformation(proxy, group);
       } else {
         final RenderingStatus status;
         if (orig instanceof LabelGeometrySubject) {
@@ -602,13 +603,10 @@ public class AutomatonDisplayPane
           status = getRenderingStatus(orig);
         }
         if (status == null) {
-          final Color foreground = EditorColor.SIMULATION_INVALID;
-          final Color shadow =
-            EditorColor.getShadowColor(orig, GraphPanel.DragOverStatus.NOTDRAG,
-                                       false, false, false);
-          final int prio = getPriority(orig);
-          return new RenderingInformation
-            (false, false, false, false, foreground, shadow, prio);
+          final RenderingInformation info =
+            super.getRenderingInformation(orig, group);
+          info.setColor(EditorColor.SIMULATION_INVALID);
+          return info;
         } else {
           final boolean selected;
           if (orig == mFocusedItem) {
@@ -635,7 +633,7 @@ public class AutomatonDisplayPane
             return new RenderingInformation
               (false, false, false, true, foreground, shadow, prio);
           } else {
-            return super.getRenderingInformation(orig);
+            return super.getRenderingInformation(orig, group);
           }
         }
       }