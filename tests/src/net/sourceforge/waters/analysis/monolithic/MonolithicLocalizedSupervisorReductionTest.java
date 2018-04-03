6df5eefbf913b9420dbd664b0a3238956630b35e
Jordan Schroder <sjs53@students.waikato.ac.nz>
2018-04-03 12:02:53 +1200

    Forgot to stage this change

---
+++ tests/src/net/sourceforge/waters/analysis/monolithic/MonolithicLocalizedSupervisorReductionTest.java
@@ -0,0 +1,52 @@
+package net.sourceforge.waters.analysis.monolithic;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import net.sourceforge.waters.analysis.abstraction.LocalizedSupervisorReductionTRSimplifier;
+import net.sourceforge.waters.model.analysis.AbstractSupervisorSynthesizerTest;
+import net.sourceforge.waters.model.analysis.des.SupervisorSynthesizer;
+import net.sourceforge.waters.model.des.ProductDESProxyFactory;
+
+public class MonolithicLocalizedSupervisorReductionTest
+  extends AbstractSupervisorSynthesizerTest
+{
+
+  //#########################################################################
+  //# To be Provided by Subclasses
+  /**
+   * Creates an instance of the synthesiser under test. This method
+   * instantiates the class of the synthesiser tested by the particular
+   * subclass of this test, and configures it as needed.
+   * @param factory
+   *          The factory used by the synthesiser to create its output.
+   * @return An instance of the synthesiser.
+   */
+  @Override
+  protected SupervisorSynthesizer createSynthesizer
+    (final ProductDESProxyFactory factory)
+  {
+    final MonolithicSynthesizer synthesizer =
+      new MonolithicSynthesizer(factory);
+    synthesizer.setSupervisorReductionSimplifier(new LocalizedSupervisorReductionTRSimplifier());
+    synthesizer.setSupervisorReductionEnabled(true);
+    synthesizer.setSupervisorLocalizationEnabled(true);
+    return synthesizer;
+  }
+
+
+  //#########################################################################
+  //# Entry points in junit.framework.TestCase
+  public static Test suite()
+  {
+    final TestSuite testSuite =
+      new TestSuite(MonolithicLocalizedSupervisorReductionTest.class);
+    return testSuite;
+  }
+
+  public static void main(final String[] args)
+  {
+    junit.textui.TestRunner.run(suite());
+  }
+
+}
