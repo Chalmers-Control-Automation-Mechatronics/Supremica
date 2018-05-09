faa07c71c258ddd3354670ce31c097fa2a4268ea
Robi Malik <robi@waikato.ac.nz>
2018-05-09 12:42:43 +1200

    ADD. One more test for clique-based supervisor reduction.

--- tests/src/net/sourceforge/waters/analysis/abstraction/CliqueBasedSupervisorReductionTRSimplifierTest.java
+++ tests/src/net/sourceforge/waters/analysis/abstraction/CliqueBasedSupervisorReductionTRSimplifierTest.java
@@ -179,6 +179,14 @@ public class CliqueBasedSupervisorReductionTRSimplifierTest
     runTransitionRelationSimplifier(group, subdir, name);
   }
 
+  public void test_supred_02() throws Exception
+  {
+    final String group = "tests";
+    final String subdir = "abstraction";
+    final String name = "supred_02.wmod";
+    runTransitionRelationSimplifier(group, subdir, name);
+  }
+
   /**
    * A test to see whether a single transition relation simplifier
    * object can perform multiple abstractions in sequence.
@@ -186,9 +194,9 @@ public class CliqueBasedSupervisorReductionTRSimplifierTest
   public void testReentrant() throws Exception
   {
     test_supred_01();
+    test_supred_02();
     test_supred_01();
-    test_supred_01();
-    test_supred_01();
+    test_supred_02();
     test_supred_01();
   }
 }
