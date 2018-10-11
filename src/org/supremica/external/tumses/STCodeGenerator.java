0d802108ff34a86274557927cd1a278bf85125ec
Julien Provost <provost@ses.mw.tum.de>
2018-10-11 20:40:50 +0200

    FIX. * The filename is now used instead of the module's name. Fixed the problem when files where copy/paste from the windows explorer and not using "save as"      * The code has been refactored a bit: new function isLocalWmod(final ModuleSubject module) checks if the module is saved locally as a WMOD file

--- src/org/supremica/external/tumses/STCodeGenerator.java
+++ src/org/supremica/external/tumses/STCodeGenerator.java
@@ -3,7 +3,6 @@ package org.supremica.external.tumses;
 import java.io.File;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -20,51 +19,52 @@ import org.supremica.automata.algorithms.EditorSynthesizerOptions;
 import org.supremica.gui.ide.DocumentContainerManager;
 import org.supremica.gui.ide.actions.IDEActionInterface;
 
+/**
+ * @author Julien Provost
+ */
+
 public class STCodeGenerator
 {
   private static final long serialVersionUID = 1L;
   private final static Logger logger = LogManager.getLogger();
 
-
+  /**
+   * Generates, from a WMOD file, IEC 61131-3 ST code using TUM external STCodeConverter executable
+   * @param ide {@link IDEActionInterface}
+   * @param module {@link ModuleSubject}
+   * @param options {@link EditorSynthesizerOptions}
+   */
   public static void GenerateSTCode(final IDEActionInterface ide, final ModuleSubject module, final EditorSynthesizerOptions options) {
     logger.info("Generating PLC Code using TUM external toolbox...");
     tryLabel:
     try {
-      logger.debug("\tChecking the current model file");
-      final String modulename = module.getName(); // Module's name without extension
-      final String moduleextension = ".wmod"; // TODO: Check if that is really the case, e.g. *.xml modules return null to getName()
-      final String modulefullname = modulename + moduleextension;
-
-      // Retrieve the path to access the Wmod file to be passed to the converter
-      String modulePath;
-      URI moduleUri = module.getLocation(); // already returns null if the module (included in the JAR) is not a Wmod file
-      if (moduleUri == null) {
-        modulePath = null;
-      } else {
-        modulePath = moduleUri.getPath(); // returns null if in the JAR, returns the path otherwise
-      }
-
-      // modulePath is null if the module is included in the JAR -> Save the current module into a selected folder
+      logger.debug("\tChecking if the current model file is saved locally as a WMOD file...");
+      // Retrieve the path of the Wmod file to be passed to the converter
+      // Always save the module locally (Save as / Save). Otherwise, last changes may not be saved in the Wmod file
       final DocumentContainerManager manager = ide.getActiveDocumentContainer().getIDE().getDocumentContainerManager();
-      if (modulePath == null) { // modulePath is null if the module is included in the JAR
-        // Save as ...
-        logger.warn("The current model first needs to be saved as an external WMOD file. Please select the destination folder.");
-        manager.saveActiveContainerAs();
-
-        // Retrieve the new module's path (and name)
-        moduleUri = module.getLocation();
-        modulePath = moduleUri.getPath(); // retrieve the new module's path (and name)
-      } else {
+      if (isLocalWmod(module)) {
         manager.saveActiveContainer();
+      } else {
+        int count = 0;
+        final int maxcount = 5; // max 5 tries, to avoid an infinite loop
+        while (!isLocalWmod(module)) {
+          logger.warn("The current model first needs to be saved, locally, as a WMOD file. Please select the destination folder.");
+          count += 1;
+          if (count > maxcount) { // Abort after too many missed tries
+            logger.error("Failed to save the current model, locally, as a WMOD file. ST code generation aborted.");
+            break tryLabel;
+          }
+          manager.saveActiveContainerAs();
+        }
       }
-      logger.trace("modulename  : " + modulename);
-      logger.trace("moduleUri   : " + moduleUri);
-      logger.trace("modulePath  : " + modulePath);
+      // Retrieve the new module's file name and folder
+      String modFilePath = module.getFileLocation().getPath();
+      final String modFileFolder = module.getFileLocation().getParentFile().getPath();
 
-      // Select the Output path to be passed to the converter
-      String destFoldPath;
-      if (!options.getSavPLCCodeTUMBox()) { // adding the Output option
-        logger.warn("Selection of the PLC code destination. Please select the output folder.");
+      // If different, select the output path to be passed to the converter
+      String outFolder = null;
+      if (!options.getSavPLCCodeTUMBox()) {
+        logger.info("Selection of the PLC code destination. Please select the output folder.");
         final String dialogTitle;
         if (options.getTypePLCCodeTUM()=="standalone") {
           dialogTitle = "Select the destination folder";
@@ -76,45 +76,38 @@ public class STCodeGenerator
 
         final JFileChooser chooser = new JFileChooser();
         chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-        chooser.setCurrentDirectory(new File("/" + modulePath));
+        chooser.setCurrentDirectory(new File("/" + modFileFolder));
         chooser.setAcceptAllFileFilterUsed(false);
         chooser.setDialogTitle(dialogTitle);
         final int returnVal = chooser.showOpenDialog(ide.getFrame());
         if (returnVal == JFileChooser.APPROVE_OPTION) {
-          destFoldPath = chooser.getSelectedFile().getAbsolutePath();
-          destFoldPath = destFoldPath.replace("\\", "/");
+          outFolder = chooser.getSelectedFile().getAbsolutePath();
+          outFolder = outFolder.replace("\\", "/");
         } else {
-          logger.warn("\tSelection canceled! Saving in the module's folder:" + modulePath);
-          destFoldPath = modulePath.substring(0, modulePath.length() - modulefullname.length());
+          logger.warn("\tSelection canceled! Saving in the current module's folder:" + modFileFolder);
         }
-      } else {
-        destFoldPath = modulePath.substring(0, modulePath.length() - modulefullname.length());
       }
-      logger.trace("destFoldPath: " + destFoldPath);
+      if (outFolder == null) {
+        outFolder = modFileFolder;
+      }
 
-      // Check and clean up the paths
-      if (modulePath.startsWith("//")) {
-        logger.error("The external PLC code converter does not support network repositories!");
-        break tryLabel;
-      } else if (modulePath.startsWith("/")) {
-        modulePath = modulePath.substring(1); // remove the first /
+      // Clean up the paths
+      if (modFilePath.startsWith("/")) {
+        modFilePath = modFilePath.substring(1); // remove the first /
       }
-      if (destFoldPath.startsWith("//")) {
-        logger.error("The external PLC code converter does not support network repositories!");
-        break tryLabel;
-      } else if (destFoldPath.startsWith("/")) {
-        destFoldPath = destFoldPath.substring(1); // remove the first /
+      if (outFolder.startsWith("/")) {
+        outFolder = outFolder.substring(1); // remove the first /
       }
 
       // Building the command to pass to the external converter
       // examples:
-      // STCodeConverter.exe -tc -f modulePath -o destFoldPath outputfilename(without extension)
+      // STCodeConverter.exe -tc -f modFilePath -o outFolder pouFilename(without extension)
       final ArrayList<String> command = new ArrayList<>(Arrays.asList("STCodeConverter.exe"));
-      if (options.getTypePLCCodeTUM()=="standalone") { // Standalone (default) option, with filepath
-        final List<String> addargs = Arrays.asList("-f", modulePath);
+      if (options.getTypePLCCodeTUM()=="standalone") { // Standalone (default) option, with module's file path
+        final List<String> addargs = Arrays.asList("-f", modFilePath);
         command.addAll(addargs);
-      } else if (options.getTypePLCCodeTUM()=="TwinCAT") { // TwinCAT option, with filepath
-        final List<String> addargs = Arrays.asList("-tc", "-f", modulePath);
+      } else if (options.getTypePLCCodeTUM()=="TwinCAT") { // TwinCAT option, with module's file path
+        final List<String> addargs = Arrays.asList("-tc", "-f", modFilePath);
         command.addAll(addargs);
       } else { // Help option
         command.add("-h");
@@ -125,9 +118,9 @@ public class STCodeGenerator
       }
       // NOTA: by default for our Praktikum we write the supervisor code into the file "main_control.TcPOU"
       // TODO: Add this information to the documentation
-      if (options.getTypePLCCodeTUM()=="TwinCAT") { // TwinCAT option -> Specify the output file's path and name
+      if (options.getTypePLCCodeTUM()=="TwinCAT") { // TwinCAT option -> Specify the output file's folder and name
         final String pouFilename = options.getPouNameField();
-        final List<String> addargs = Arrays.asList("-o", destFoldPath, pouFilename);
+        final List<String> addargs = Arrays.asList("-o", outFolder, pouFilename);
         command.addAll(addargs);
       }
       logger.trace("\tCommand built: " + command);
@@ -165,13 +158,9 @@ public class STCodeGenerator
         }
       }
 
-      final int result = proc.exitValue(); // TODO: What is the difference between exitValue and waitFor?
-//      final int result = proc.waitFor();
+      final int result = proc.waitFor();
 
       if (result == 0) {
-        // TODO: Parse the outputstream to check if that was really successful, or if that was a successfully caught error!
-        //       -> Need to agree on the message to be written by the STCodeConverter.exe
-        //       -> See in a previous version (commit ac0f7708551b59d0c37a01bb77d0acc8c473ff69), e.g. if (str.contains("successfully")) ...
         final String msg = "ST code generation process completed!\r\n"
                          + "Check the console for more details.";
         logger.warn(msg);
@@ -194,6 +183,25 @@ public class STCodeGenerator
     }
     finally{
       // TODO: finally of Generating PLC Code using TUM external toolbox
+    }
+  }
+
+  /**
+   * Checks if the module is saved locally as a WMOD file
+   * @param  module {@link ModuleSubject}
+   * @return true if the module is saved locally and has the right extension
+   */
+  public static boolean isLocalWmod(final ModuleSubject module) {
+    if (module.getLocation() == null) { // already returns null if the module (included in the JAR) is not a Waters Module
+      return false;
+    } else if (module.getLocation().getPath() == null) { // returns null if in the JAR, returns the path otherwise
+      return false;
+    } else if (!module.getLocation().getPath().endsWith(".wmod")){ // returns null is the file extension is not .wmod
+      return false;
+    } else if (module.getLocation().getPath().startsWith("//")) { // Network repositories are not supported
+      return false;
+    } else {
+      return true;
     }
   }
 
