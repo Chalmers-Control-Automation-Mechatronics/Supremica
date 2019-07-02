bc71a09bab15a1ad5e6a30d70c17bb8ebc83b727
Robi Malik <robi@waikato.ac.nz>
2019-07-03 10:20:31 +1200

    MODIFY. Tooltips for conflict check action.

--- src/net/sourceforge/waters/gui/actions/AnalyzerConflictCheckAction.java
+++ src/net/sourceforge/waters/gui/actions/AnalyzerConflictCheckAction.java
@@ -1,3 +1,37 @@
+//# -*- indent-tabs-mode: nil  c-basic-offset: 2 -*-
+//###########################################################################
+//# Copyright (C) 2004-2019 Robi Malik
+//###########################################################################
+//# This file is part of Waters.
+//# Waters is free software: you can redistribute it and/or modify it under
+//# the terms of the GNU General Public License as published by the Free
+//# Software Foundation, either version 2 of the License, or (at your option)
+//# any later version.
+//# Waters is distributed in the hope that it will be useful, but WITHOUT ANY
+//# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+//# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+//# details.
+//# You should have received a copy of the GNU General Public License along
+//# with Waters. If not, see <http://www.gnu.org/licenses/>.
+//#
+//# Linking Waters statically or dynamically with other modules is making a
+//# combined work based on Waters. Thus, the terms and conditions of the GNU
+//# General Public License cover the whole combination.
+//# In addition, as a special exception, the copyright holders of Waters give
+//# you permission to combine Waters with code included in the standard
+//# release of Supremica under the Supremica Software License Agreement (or
+//# modified versions of such code, with unchanged license). You may copy and
+//# distribute such a system following the terms of the GNU GPL for Waters and
+//# the licenses of the other code concerned.
+//# Note that people who make modified versions of Waters are not obligated to
+//# grant this special exception for their modified versions; it is their
+//# choice whether to do so. The GNU General Public License gives permission
+//# to release a modified version without this exception; this exception also
+//# makes it possible to release a modified version which carries forward this
+//# exception.
+//###########################################################################
+
+
 package net.sourceforge.waters.gui.actions;
 
 import java.awt.event.ActionEvent;
@@ -10,11 +44,13 @@ import net.sourceforge.waters.gui.observer.EditorChangedEvent;
 
 import org.supremica.gui.ide.IDE;
 
+
 /**
- * The action to invoke the Conflict Checker dialog in the Waters analyser.
+ * The action to invoke the Conflict Check dialog in the Waters analyser.
  *
  * @author Brandon Bassett
  */
+
 public class AnalyzerConflictCheckAction extends WatersAnalyzerAction
 {
   //#########################################################################
@@ -29,15 +65,18 @@ public class AnalyzerConflictCheckAction extends WatersAnalyzerAction
     updateEnabledStatus();
   }
 
+
   //#########################################################################
   //# Interface java.awt.event.ActionListener
   @Override
   public void actionPerformed(final ActionEvent arg0)
   {
     final IDE ide = getIDE();
-    if (ide != null)
+    if (ide != null) {
       new AutomatonSynthesizerDialog(getAnalyzerPanel());           //To be replaced with conflict dialog
+    }
   }
+
 
   //#########################################################################
   //# Interface net.sourceforge.waters.gui.observer.Observer
@@ -49,6 +88,7 @@ public class AnalyzerConflictCheckAction extends WatersAnalyzerAction
     }
   }
 
+
   //#########################################################################
   //# Auxiliary Methods
   private void updateEnabledStatus()
@@ -56,20 +96,22 @@ public class AnalyzerConflictCheckAction extends WatersAnalyzerAction
     final AutomataTable table = getAnalyzerTable();
     if (table == null) {
       setEnabled(false);
-      putValue(Action.SHORT_DESCRIPTION, "Conflict Check 1");
+      putValue(Action.SHORT_DESCRIPTION,
+               "Check whether automata are nonconflicting");
     } else if (table.getSelectedRowCount() > 0) {
       setEnabled(true);
       putValue(Action.SHORT_DESCRIPTION,
-        "Conflict Check 2");
+               "Check whether the selected automata are nonconflicting");
     } else {
       setEnabled(table.getRowCount() > 0);
       putValue(Action.SHORT_DESCRIPTION,
-        "Conflict Check 3");
+               "Check whether all automata are nonconflicting");
     }
   }
 
+
   //#########################################################################
   //# Class Constants
-  private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 636028154288275788L;
 
 }
