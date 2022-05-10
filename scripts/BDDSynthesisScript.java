b484e85a2bdf5a396979edec533b505b02885d98
M Fabian <fabian@chalmers.se>
2022-05-10 22:08:22 +0200

    ADD. scripts folder with two example scripts

---
+++ scripts/BDDSynthesisScript.java
@@ -0,0 +1,82 @@
+// How to synthesize using the BDD synthesizer
+// The options are set in an instance of org\supremica\automata\algorithms\EditorSynthesizerOptions.java
+// The options seem to be remembered, so need only be set up once, can be done manually
+// Code below is extracted from src\org\supremica\gui\ide\actions\EditorSynthesizerAction.java
+// This code can be run by the Editor > Run script...
+// Use CreateOpenModule.java as template
+
+import java.util.Vector;
+
+import org.supremica.gui.ide.IDE;
+import org.supremica.gui.ide.actions.Actions;
+import org.supremica.automata.ExtendedAutomata;
+import org.supremica.automata.BDD.EFA.BDDExtendedSynthesizer;
+import org.supremica.gui.ide.actions.EditorSynthesizerAction;
+import org.supremica.automata.algorithms.EditorSynthesizerOptions;
+import net.sourceforge.waters.subject.module.EventDeclSubject;
+import net.sourceforge.waters.subject.module.ModuleSubject;
+import net.sourceforge.waters.model.base.EventKind;
+
+public class BDDSynthesisScript
+{
+    private final IDE ide;
+
+    public BDDSynthesisScript(final IDE ide)
+    {
+        this.ide = ide;
+
+		// Look up the editorSynthesizerAction
+		final Actions actions = ide.getActions();
+		EditorSynthesizerAction editorSynthesizerAction =
+				(EditorSynthesizerAction)actions.editorSynthesizerAction;
+
+        final ModuleSubject module =
+              ide.getActiveDocumentContainer().getEditorPanel().getModuleSubject();
+
+    	// get the stored or default options
+    	final EditorSynthesizerOptions options = new EditorSynthesizerOptions();
+
+        // collect controllable event names from the module
+        final Vector<String> controllableEventNames = new Vector<String>();
+        for (final EventDeclSubject sigmaS : module.getEventDeclListModifiable())
+        {
+            if (sigmaS.getKind() == EventKind.CONTROLLABLE)
+            {
+                controllableEventNames.add(sigmaS.getName());
+            }
+        }
+
+        final ExtendedAutomata exAutomata = new ExtendedAutomata(module);
+
+        BDDExtendedSynthesizer bddSynthesizer = null;
+        RuntimeException synthesisException = null;
+
+        try
+        {
+            bddSynthesizer = new BDDExtendedSynthesizer(exAutomata, options);
+            bddSynthesizer.synthesize(options);
+            bddSynthesizer.generateGuard(controllableEventNames, options);
+            editorSynthesizerAction.saveOrPrintGuards(bddSynthesizer, controllableEventNames,
+                                    options.getSaveInFile(), options.getPrintGuard(),
+                                    module.getName(), "R:/BDDsynthOutput.txt");
+
+            if (options.getAddGuards())
+                bddSynthesizer.addGuardsToAutomata();
+
+            // Cleanup...
+            bddSynthesizer.done();
+        }
+        catch (final RuntimeException e)
+        {
+            synthesisException = e;
+        }
+        finally
+        {
+            if (bddSynthesizer != null)
+                bddSynthesizer.done();
+
+            if (synthesisException != null)
+                throw synthesisException;
+        }
+    }
+}
---
+++ scripts/CreateOpenModule.java
@@ -0,0 +1,37 @@
+/**************** CreateOpenModule.java ********************/
+/* First try on java code to create or open a new module
+ * in Supremica. Meant to be run as external script
+**//***** And it works! ************************************/
+import java.io.File;
+import java.util.List;
+import org.supremica.gui.ide.DocumentContainerManager;
+import org.supremica.gui.ide.IDE;
+
+public class CreateOpenModule
+{
+	private final IDE ide;
+
+    public void createModule()
+    {
+        // To create a new module (from org\supremica\gui\ide\actions\NewAction.java)
+        // final IDE ide = getIDE();
+        final DocumentContainerManager manager = ide.getDocumentContainerManager();
+        final org.supremica.gui.ide.ModuleContainer container = manager.newModuleContainer();
+    }
+    public void openModule(java.io.File file)
+    {
+        // To open an existing *.wmod (from net.sourceforge.waters.gui.actions.IDEOpenAction.java)
+        // final IDE ide = getIDE();
+        final DocumentContainerManager cmanager =
+                  ide.getDocumentContainerManager();
+        cmanager.openContainer(file);
+    }
+
+	public CreateOpenModule(final IDE ide)
+    {
+		System.out.println("CreateOpenModule(final IDE ide), called");
+		this.ide = ide;
+		// this.createModule();
+		this.openModule(new File("Z:/Supremica/examples/waters/tests/synthesis/transferline_1.wmod"));
+	}
+}
