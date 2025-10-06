262f69f63dc98d89b8d92c92e8f713723b757e80
M Fabian <fabian@chalmers.se>
2025-10-06 22:35:38 +0200

    FIX. BDDSynthesisScript.lua to synthesize for all open modules

--- scripts/BDDSynthesisScript.lua
+++ scripts/BDDSynthesisScript.lua
@@ -7,9 +7,10 @@ local script, ide = ... -- grab the arguments passed from Java via LuaJ
 local Config = luaj.bindClass("org.supremica.properties.Config")
 local getFileName = dofile(Config.FILE_SCRIPT_PATH:getValue():getPath().."/getFileName.lua")
 
+-- Set up some helpers
 local actions = ide:getActions()
 local editorSynthesizerAction  = actions.editorSynthesizerAction
-local module = ide:getActiveDocumentContainer():getEditorPanel():getModuleSubject()
+local manager = ide:getDocumentContainerManager() 
 
 -- bindClass is like Java's import
 local EventKind = luaj.bindClass("net.sourceforge.waters.model.base.EventKind")
@@ -18,36 +19,51 @@ local SynthesisAlgorithm = luaj.bindClass("org.supremica.automata.algorithms.Syn
 
 -- Set the options
 local options = luaj.newInstance("org.supremica.automata.algorithms.EditorSynthesizerOptions")
-options:setSaveInFile(true)
-options:setPrintGuard(true)
+options:setSaveInFile(false)
+options:setPrintGuard(false)
 options:setSynthesisType(SynthesisType.NONBLOCKING_CONTROLLABLE)
-options:setSynthesisAlgorithm(SynthesisAlgorithm.PARTITIONBDD)
+options:setSynthesisAlgorithm(SynthesisAlgorithm.MONOLITHICBDD) 
+-- options:setSynthesisAlgorithm(SynthesisAlgorithm.PARTITIONBDD)
 options:setExpressionType(options.ExpressionType.ADAPTIVE)
 options:setAddGuards(false)
 
--- collect the controllable events
-local controllableEvents = luaj.newInstance("java.util.Vector")
-local eventDeclList = module:getEventDeclListModifiable()
-for i = 1, eventDeclList:size() do
-  local event = eventDeclList:get(i-1)
-  if event:getKind() == EventKind.CONTROLLABLE then
-    controllableEvents:add(event:getName())
-  end
+-- For all open modules, perform synthesis
+local recent = manager:getRecent() -- java.util.List<DocumentContainer> of all open modules
+for i = 1, recent:size() do
+	local container = recent:get(i-1)
+	local name = container:getName()
+	if name ~= "New Module" then 
+		print("**** Synthesizing for: "..name)
+		local module = container:getEditorPanel():getModuleSubject()
+
+		-- collect the controllable events, only them can we assign guards to
+		local controllableEvents = luaj.newInstance("java.util.Vector")
+		local eventDeclList = module:getEventDeclListModifiable()
+		for i = 1, eventDeclList:size() do
+		  local event = eventDeclList:get(i-1)
+		  if event:getKind() == EventKind.CONTROLLABLE then
+			controllableEvents:add(event:getName())
+		  end
+		end
+
+		local exAutomata = luaj.newInstance("org.supremica.automata.ExtendedAutomata", module)
+		local bddSynthesizer = luaj.newInstance("org.supremica.automata.BDD.EFA.BDDExtendedSynthesizer", exAutomata, options)
+		bddSynthesizer:synthesize(options)
+		bddSynthesizer:generateGuard(controllableEvents, options)
+
+		local saveFile = luaj.newInstance("java.io.File", getFileName("BDDoutput.txt"))
+		editorSynthesizerAction:saveOrPrintGuards(bddSynthesizer, controllableEvents,
+											options:getSaveInFile(), options:getPrintGuard(),
+											saveFile)
+
+		print("Synthesis took "..bddSynthesizer:getSynthesisTimer():toString())
+		print("Guards generated in "..bddSynthesizer:getGuardTimer():toString())
+		
+		if options:getAddGuards() then
+		  bddSynthesizer:addGuardsToAutomata()
+		end
+
+		-- Cleanup...
+		bddSynthesizer:done()
+	end
 end
-
-local exAutomata = luaj.newInstance("org.supremica.automata.ExtendedAutomata", module)
-local bddSynthesizer = luaj.newInstance("org.supremica.automata.BDD.EFA.BDDExtendedSynthesizer", exAutomata, options)
-bddSynthesizer:synthesize(options)
-bddSynthesizer:generateGuard(controllableEvents, options)
-
-local saveFile = luaj.newInstance("java.io.File", getFileName("BDDoutput.txt"))
-editorSynthesizerAction:saveOrPrintGuards(bddSynthesizer, controllableEvents,
-                                    options:getSaveInFile(), options:getPrintGuard(),
-                                    saveFile)
-                                  
-if options:getAddGuards() then
-  bddSynthesizer:addGuardsToAutomata()
-end
-
--- Cleanup...
-bddSynthesizer:done()
